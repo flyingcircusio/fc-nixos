flyingcircusio/fc-nixos

commit 10d709a8d9057f7c100098394011f2ccd6e1f469

Remove /var/setuid-wrappers at boot time

nixos/platform/default.nix
@@ -65,9 +65,10 @@ with lib;
       # of fc-data to avoid killing the seeded ENC upon boot.
       "d /etc/current-config"  # used by various FC roles
       "d /srv"
-      "z /srv 0755 root root"
       "d /tmp 1777 root root 3d"
       "d /var/tmp 1777 root root 7d"
+      # remove old (pre-16.09) setuid wrappers first reboot after upgrade
+      "R! /var/setuid-wrappers"
     ];
 
     time.timeZone =

commit b87dea03dd0c5c2e2140d0b52f8839737049f92b

Fix permissions of /srv
Accidentally broke that in 10d709a8d.

nixos/platform/default.nix
@@ -64,7 +64,7 @@ with lib;
       # d instead of r to a) respect the age rule and b) allow exclusion
       # of fc-data to avoid killing the seeded ENC upon boot.
       "d /etc/current-config"  # used by various FC roles
-      "d /srv"
+      "d /srv 0755"
       "d /tmp 1777 root root 3d"
       "d /var/tmp 1777 root root 7d"
       # remove old (pre-16.09) setuid wrappers first reboot after upgrade