77cc31f6892fc16d8d385f98240c06b1e2ca54d1
Tobias Stenzel <ts@flyingcircus.io>
2019-06-03 15:15:03 +0200

    port external_net role and VxLAN client
    * OpenVPN and VXLAN gateway activated by external_net role
    * VxLAN client activates itself if a gateway is present
    * only allow some secure data and control channel ciphers
    * OpenVPN clients 2.3 should work, but cipher must be set in the client config
    * using clients > 2.4 is recommended

---
+++ nixos/roles/external_net/default.nix
@@ -0,0 +1,95 @@
+# Managed external network (OpenVPN/DHP/...) gateway
+{ lib, config, pkgs, ... }:
+
+with builtins;
+
+let
+  fclib = config.fclib;
+  cfg = config.flyingcircus;
+  parameters = lib.attrByPath [ "enc" "parameters" ] {} cfg;
+
+  # attrset of those fe addresses that have a reverse
+  feReverses =
+    # fake attrset from listenAddresses: { "addr" = null; ... }
+    let feAddrSet =
+      lib.genAttrs (fclib.listenAddresses "ethfe") (x: null);
+    in
+    intersectAttrs feAddrSet (lib.attrByPath [ "reverses" ] {} parameters);
+
+  # pick a suitable DNS name for client config
+  domain = config.networking.domain;
+  location = lib.attrByPath [ "parameters" "location" ] "standalone" cfg.enc;
+  defaultFrontendName =
+    if feReverses != {}
+    then fclib.normalizeDomain domain (head (attrValues feReverses))
+    else
+      if location != null
+      then "${config.networking.hostName}.fe.${location}.${domain}"
+      else "localhost";
+
+in
+{
+  imports = [
+    ./openvpn.nix
+    ./vxlan.nix
+  ];
+
+  options = {
+    flyingcircus.roles.external_net = {
+
+      enable = lib.mkEnableOption { };
+
+      vxlan4 = lib.mkOption {
+        type = lib.types.str;
+        default = "10.102.99.0/24";
+        description = ''
+          IPv4 network range for VxLAN external network. Must be changed on all
+          nodes within a RG for end-to-end routing. So it is best to leave it
+          alone.
+        '';
+      };
+
+      vxlan6 = lib.mkOption {
+        type = lib.types.str;
+        default = "fd3e:65c4:fc10:fc::/64";
+        description = ''
+          IPv6 network range for VxLAN external network. Must be changed on all
+          nodes within a RG for end-to-end routing. So it is best to leave it
+          alone.
+        '';
+      };
+
+      frontendName = lib.mkOption {
+        type = lib.types.str;
+        default = defaultFrontendName;
+        description = ''
+          DNS host name for the external network gateway. This is also the name
+          of the OpenVPN client configuration. The default is derived from the
+          FE address' reverse name.
+        '';
+      };
+
+    };
+  };
+
+  config = lib.mkIf cfg.roles.external_net.enable {
+    # does not interact well with old-style policy routing
+    flyingcircus.network.policyRouting.enable = lib.mkForce false;
+
+    flyingcircus.roles.openvpn.enable = true;
+
+    flyingcircus.roles.vxlan.gateway = true;
+
+    boot.kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
+      "net.ipv6.conf.default.forwarding" = 1;
+    };
+
+    environment.systemPackages = [ pkgs.mosh ];
+
+    # we need 53/udp in any case, both openvpn and vxlan use dnsmasq
+    # 60000-60003 is for mosh
+    networking.firewall.allowedUDPPorts = [ 53 60000 60001 60002 60003 ];
+  };
+}
---
+++ nixos/services/vxlan-client.nix
@@ -0,0 +1,73 @@
+# Access external networks like VxLAN tunnels to Kamp DHP.
+# This service should be included unconditionally from platform/default.nix. It
+# activates itself if an appropriate ENC service is present and the host isn't a VxLAN gateway.
+
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.flyingcircus;
+  gw = lib.findFirst
+    (s: s.service == "external_net-gateway") null cfg.enc_services;
+  fqdn = "${cfg.enc.name}.${config.networking.domain}";
+in
+
+{
+  options = {
+    flyingcircus.roles.external_net_client = {
+      enable = lib.mkOption {
+        description = "Access external networks via external_net gateway";
+        type = lib.types.bool;
+        default = (gw != null) && (!cfg.roles.vxlan.gateway);
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.roles.external_net_client.enable (
+    let
+      gwHost = gw.address;
+      extnet = cfg.roles.external_net;
+    in
+    {
+      # does not interact well with old-style policy routing
+      flyingcircus.network.policyRouting.enable = lib.mkForce false;
+
+      systemd.services."network-external-routing" = rec {
+        description = "Custom routing rules for external networks";
+        after = [ "network-routing-ethsrv.service" "firewall.service" ];
+        requires = after;
+        wantedBy = [ "network.target" ];
+        bindsTo = [ "sys-subsystem-net-devices-ethsrv.device" ];
+        path = [ pkgs.gawk pkgs.iproute pkgs.glibc pkgs.iptables ];
+
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = pkgs.writeScript "network-external-routing-start" ''
+            #! ${pkgs.stdenv.shell} -e
+            echo "Adding routes via external network gateway ${gwHost}"
+            gw4=$(getent ahostsv4 ${gwHost} | awk 'NR==1 {print $1}')
+            gw6=$(getent ahostsv6 ${gwHost} | awk 'NR==1 {print $1}')
+            ip -4 route add ${extnet.vxlan4} via $gw4 dev ethsrv
+            ip -6 route add ${extnet.vxlan6} via $gw6 dev ethsrv
+            iptables -I nixos-fw 3 -i ethsrv -s ${extnet.vxlan4} \
+              -j nixos-fw-accept
+            ip6tables -I nixos-fw 3 -i ethsrv -s ${extnet.vxlan6} \
+              -j nixos-fw-accept
+          '';
+          ExecStop = pkgs.writeScript "network-external-routing-stop" ''
+            #! ${pkgs.stdenv.shell}
+            echo "Removing routes via external network gateway ${gwHost}"
+            gw4=$(getent ahostsv4 ${gwHost} | awk 'NR==1 {print $1}')
+            gw6=$(getent ahostsv6 ${gwHost} | awk 'NR==1 {print $1}')
+            ip -4 route del ${extnet.vxlan4} via $gw4 dev ethsrv
+            ip -6 route del ${extnet.vxlan6} via $gw6 dev ethsrv
+            iptables -D nixos-fw -i ethsrv -s ${extnet.vxlan4} \
+              -j nixos-fw-accept
+            ip6tables -D nixos-fw -i ethsrv -s ${extnet.vxlan6} \
+              -j nixos-fw-accept
+          '';
+          RemainAfterExit = true;
+        };
+      };
+    }
+  );
+}
