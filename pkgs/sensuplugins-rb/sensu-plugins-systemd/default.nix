a89f10db4d96e982c8548b4f79f64f3792b4c29b
Tobias Stenzel <ts@flyingcircus.io>
2019-07-18 11:17:26 +0200

    Fix systemd sensu check
    * vendor plugin code, fetching it from git didn't work with bundlerSensuPlugin
    * suppress deprecation warnings
    * update gem deps

--- pkgs/sensuplugins-rb/sensu-plugins-systemd/default.nix
+++ pkgs/sensuplugins-rb/sensu-plugins-systemd/default.nix
@@ -1,6 +1,30 @@
-{ bundlerSensuPlugin }:
+{ bundlerEnv, stdenv, ruby }:
 
-bundlerSensuPlugin {
-  pname = "sensu-plugins-systemd";
-  exes = [ "check-failed-units.rb" ];
-}
+let 
+  gems = bundlerEnv {
+    gemdir = ./.;
+    name = "sensu-plugin-systemd-gems";
+  };
+
+in 
+  stdenv.mkDerivation {
+    name = "sensu-plugin-systemd";
+    version = "0.1";
+
+    src = ./check-failed-units.rb;
+
+    phases = [ "installPhase" "fixupPhase" ];
+    buildInputs = [ gems ];
+    propagatedBuildInputs = [ ruby ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      prog=$out/bin/check-failed-units.rb
+      wrapped=$out/bin/.check-failed-units.rb-wrapped
+      cp $src $wrapped
+
+      echo /usr/bin/env -i HOME=/tmp ${gems}/bin/bundle exec $wrapped '"$@"' > $prog
+      chmod 755 $prog
+    '';
+
+  }
--- pkgs/sensuplugins-rb/sensu-plugins-systemd/gemset.nix
+++ pkgs/sensuplugins-rb/sensu-plugins-systemd/gemset.nix
@@ -1,7 +1,9 @@
 {
   json = {
+    groups = ["default"];
+    platforms = [];
     source = {
-      remotes = ["http://rubygems.org"];
+      remotes = ["https://rubygems.org"];
       sha256 = "0sx97bm9by389rbzv8r1f43h06xcz8vwi3h5jv074gvparql7lcx";
       type = "gem";
     };
@@ -12,10 +14,10 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "050mjilikw7am0g7yw5qpsz6y7aj98f3x3c5zcwnwkyldn5ri5jc";
+      sha256 = "0647msh7kp7lzyf6m72g6snpirvhimjm22qb8xgv9pdhbcrmcccp";
       type = "gem";
     };
-    version = "2.0.6";
+    version = "1.7.0";
   };
   ruby-dbus = {
     groups = ["default"];
@@ -33,23 +35,10 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1k8mkkwb70z2j5lq457y7lsh5hr8gzd53sjbavpqpfgy6g4bxrg8";
+      sha256 = "1hibm8q4dl5bp949h21zjc3d2ss26mg4sl5svy7gl7bz59k9dg06";
       type = "gem";
     };
-    version = "1.2.0";
-  };
-  sensu-plugins-systemd = {
-    dependencies = ["sensu-plugin" "systemd-bindings"];
-    groups = ["default"];
-    platforms = [];
-    source = {
-      fetchSubmodules = false;
-      rev = "be972959c5f6cdc989b1122db72a4b10a1ecce77";
-      sha256 = "0n1jbzs4ls4gmci8zc92nm3mi1n69w2i37k28zlfwxazbm1gyy0z";
-      type = "git";
-      url = "https://github.com/nyxcharon/sensu-plugins-systemd.git";
-    };
-    version = "0.0.1";
+    version = "4.0.0";
   };
   systemd-bindings = {
     dependencies = ["ruby-dbus"];
@@ -61,5 +50,15 @@
       type = "gem";
     };
     version = "0.0.1.1";
+  };
+  warning = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0psdaiy38ky4gqifgd5c034s8gng0sxr7p8h0cqwc5s43126wlxa";
+      type = "gem";
+    };
+    version = "0.10.1";
   };
 }
