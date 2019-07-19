e5fa8a23aec760322d580d4d9450a86b544bf6a4
Tobias Stenzel <ts@flyingcircus.io>
2019-07-19 10:25:18 +0200

    Use default rust platform 1.32 for our tools

--- pkgs/fc/default.nix
+++ pkgs/fc/default.nix
@@ -5,24 +5,18 @@
 
   agent = callPackage ./agent {};
   box = callPackage ./box {
-    rustPlatform = pkgs.rustPlatform_1_31;
   };
   check-journal = callPackage ./check-journal.nix {
-    rustPlatform = pkgs.rustPlatform_1_31;
   };
   collectdproxy = callPackage ./collectdproxy {};
   fix-so-rpath = callPackage ./fix-so-rpath {};
   logcheckhelper = callPackage ./logcheckhelper {
-    rustPlatform = pkgs.rustPlatform_1_31;
   };
   multiping = callPackage ./multiping.nix {
-    rustPlatform = pkgs.rustPlatform_1_31;
   };
   sensusyntax = callPackage ./sensusyntax {
-    rustPlatform = pkgs.rustPlatform_1_31;
   };
   sensuplugins = callPackage ./sensuplugins {};
   userscan = callPackage ./userscan.nix {
-    rustPlatform = pkgs.rustPlatform_1_31;
   };
 }
