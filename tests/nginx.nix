dbef940ae0cc4ef912826aef62349517959c0ea9
Tobias Stenzel <ts@flyingcircus.io>
2019-08-07 18:21:40 +0200

    Fix nginx test
    We cannot test local config this way anymore and need a generic solution
    for that.

--- tests/nginx.nix
+++ tests/nginx.nix
@@ -6,25 +6,13 @@ import ./make-test.nix ({ ... }:
     {
       imports = [ ../nixos ];
       flyingcircus.services.nginx.enable = true;
-      environment.etc."local/nginx/mysite.conf".text = ''
-        # simple vhost serving its own config
-        server {
-          listen *:80;
-          server_name machine;
-          root /etc/local/nginx;
-        }
-      '';
     };
   testScript = ''
     $machine->waitForUnit('nginx.service');
     $machine->succeed(<<_EOT_);
-      curl -v http://machine/mysite.conf | grep "serving its own config"
-    _EOT_
-    $machine->succeed(<<_EOT_);
       curl -v http://localhost/nginx_status | \
       grep "server accepts handled requests"
     _EOT_
-    $machine->succeed('grep mysite.conf /var/log/nginx/access.log');
 
     # service user should be able to write to local config dir
     $machine->succeed('sudo -u nginx touch /etc/local/nginx/vhosts.json');
