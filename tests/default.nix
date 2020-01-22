d2addacbb6324630659a26a9b608a9dbdf799e7c
Tobias Stenzel <ts@flyingcircus.io>
2020-01-22 11:01:42 +0100

    Fix duplicate test attr name

--- tests/default.nix
+++ tests/default.nix
@@ -25,7 +25,7 @@ in {
   fcagent = callTest ./fcagent.nix {};
   garbagecollect = callTest ./garbagecollect.nix {};
   graylog = callTest ./graylog.nix {};
-  login = callTest ./journal.nix {};
+  journal = callTest ./journal.nix {};
   login = callTest ./login.nix {};
   logrotate = callTest ./logrotate.nix {};
   mail = callSubTests ./mail.nix {};
