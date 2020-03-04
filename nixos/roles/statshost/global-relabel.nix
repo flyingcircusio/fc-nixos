b5474e270f22c71e391c9c1185e1afb920cda4e0
Tobias Stenzel <ts@flyingcircus.io>
2020-03-04 11:29:06 +0100

    Statshost: fix netstat_tcp metric relabeling
    The relabel rule was only effective if the statshost role was selected.
    It's now working for statshost-master (per RG), too.
    
    bugs id: #123713

--- nixos/roles/statshost/global-relabel.nix
+++ nixos/roles/statshost/global-relabel.nix
@@ -20,28 +20,5 @@ in mkIf config.flyingcircus.roles.statshost.enable
 {
   flyingcircus.roles.statshost.prometheusMetricRelabel =
     markAllowedMetrics ++
-    dropUnmarkedMetrics ++
-    (let
-      rename_merge = options: [
-        {
-          source_labels = [ "__name__" ];
-          # Only if there is no command set.
-          regex = options.regex;
-          replacement = "\${1}";
-          target_label = options.target_label;
-        }
-        {
-          source_labels = [ "__name__" ];
-          regex = options.regex;
-          replacement = options.target_name;
-          target_label = "__name__";
-        }
-      ];
-    in
-    rename_merge {
-      regex = "netstat_tcp_(.*)";
-      target_label = "state";
-      target_name = "netstat_tcp";
-    }
-  );
+    dropUnmarkedMetrics;
 }
