c2832d13e0c14691ca1e4e9fe60bc03bec37bc65
Tobias Stenzel <ts@flyingcircus.io>
2020-05-14 12:53:30 +0200

    Generate all locales
    Case 126182

---
+++ tests/locale.nix
@@ -0,0 +1,15 @@
+import ./make-test.nix ({ ... }:
+{
+  name = "locale";
+  machine =
+    { ... }:
+    {
+      imports = [ ../nixos ../nixos/roles ];
+    };
+
+  testScript = ''
+    $machine->succeed("locale -a | grep -q de_DE.utf8");
+    $machine->succeed("locale -a | grep -q en_US.utf8");
+    $machine->succeed('(($(locale -a | wc -l) > 100))');
+  '';
+})
