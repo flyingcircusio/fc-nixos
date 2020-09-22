c2c8ec4980626e07e31ef6d37fd497f55e1268eb
Tobias Stenzel <ts@flyingcircus.io>
2020-09-22 23:26:39 +0200

    Simplify mongodb check script and add feature compat check
    * feature compat check applies to mongo versions starting with 3.4
    * new simplified script is based on a Python 3 version of the old check script
    * mongodb check script moved from roles to pkgs
    * test rewritten in Python, also tests sensu checks now
    
    Case 127913

--- pkgs/fc/default.nix
+++ pkgs/fc/default.nix
@@ -8,6 +8,7 @@
   check-age = pkgs-unstable.callPackage ./check-age {};
   check-haproxy = callPackage ./check-haproxy {};
   check-journal = callPackage ./check-journal.nix {};
+  check-mongodb = callPackage ./check-mongodb {};
   check-postfix = pkgs-unstable.callPackage ./check-postfix {};
   collectdproxy = callPackage ./collectdproxy {};
   roundcube-chpasswd = pkgs-unstable.callPackage ./roundcube-chpasswd {};
