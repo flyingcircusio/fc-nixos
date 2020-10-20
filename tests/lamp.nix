2f8946a096e54aec8292ac3ae9ab481fc6c343a1
Christian Theune <ct@flyingcircus.io>
2020-10-20 15:07:16 +0200

    Make LAMP-configuration more nixified ...
    Stop piling layers of abstraction - that's not our style!
    
    This keeps backwardscompatibility for now, but as only a single
    customer was using the intermediate product we can rip this out in
    a future release.

--- tests/lamp.nix
+++ tests/lamp.nix
@@ -6,23 +6,39 @@ import ./make-test.nix ({ ... }:
       { ... }:
       {
         imports = [ ../nixos ../nixos/roles ];
-        flyingcircus.roles.lamp.enable = true;
+        flyingcircus.roles.lamp = {
+          enable = true;
 
-        # On real machines this is fed via /etc/local/lamp/docroot and 
-        flyingcircus.roles.lamp.simple_docroot = true;
-        # ... /etc/local/lamp/*.vhost.json
-        flyingcircus.roles.lamp.vhosts = [ { port = 8100; docroot = "/srv/docroot2"; } ];
+          # BBB
+          # On real machines this is fed via /etc/local/lamp/docroot and 
+          simple_docroot = true;
+
+          vhosts = [ { port = 8100; docroot = "/srv/docroot2"; } ];
+
+          apache_conf = ''
+            # XXX test-i-am-the-custom-apache-conf
+          '';
+
+          php_ini = ''
+            # XXX test-i-a-m-the-custom-php-ini
+          '';
+        };
       };
   };
 
-  testScript = ''
+  testScript = { nodes, ... }:
+    ''
     $lamp->waitForUnit("httpd.service");
     $lamp->waitForOpenPort(8000);
 
     $lamp->waitForUnit("tideways-daemon.service");
     $lamp->waitForOpenPort(9135);
 
-    $lamp->succeed('journalctl -u tideways.daemon');
+    $lamp->succeed("journalctl -u tideways.daemon");
+
+    # see that our changes for config files are there
+    $lamp->succeed("grep 'custom-apache-conf' ${nodes.lamp.config.services.httpd.configFile}");
+    $lamp->succeed("grep 'custom-php-ini' ${nodes.lamp.config.systemd.services.httpd.environment.PHPRC}");
 
     # The simple docroot
     $lamp->succeed('mkdir -p /srv/docroot');
@@ -56,6 +72,6 @@ import ./make-test.nix ({ ... }:
     $lamp->succeed('echo "<? phpinfo(); ?>" > /srv/docroot2/test2.php');
     $lamp->succeed("curl -f -v http://localhost:8100/test2.php -o result2");
     $lamp->succeed("grep 'tideways.api_key' result2");
-  '';
+    '';
 
 })
