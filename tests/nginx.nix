b7101c08790a3e79adb628af1eb838afc362dbdd
Tobias Stenzel <ts@flyingcircus.io>
2020-10-22 21:53:53 +0200

    Nginx: make test work
    Still hangs for a long time because setup for eth1 and eth2 is failing
    (they are renamed to ethsrv and ethfe).
    
    Should have a client that tests connecting from the outside.

--- tests/nginx.nix
+++ tests/nginx.nix
@@ -1,6 +1,27 @@
 import ./make-test-python.nix ({ lib, pkgs, testlib, ... }:
 
 let
+  netLoc4Srv = "10.0.1";
+  server4Srv = netLoc4Srv + ".2";
+
+  netLoc6Srv = "2001:db8:1::";
+  server6Srv = netLoc6Srv + "2";
+
+  net4Fe = "10.0.3";
+  client4Fe = net4Fe + ".1";
+  server4Fe = net4Fe + ".2";
+
+  net6Fe = "2001:db8:3::";
+  client6Fe = net6Fe + "1";
+  server6Fe = net6Fe + "2";
+
+  hosts = {
+    "127.0.0.1" = [ "localhost" ];
+    "::1" = [ "localhost" ];
+    ${server6Fe} = [ "server" ];
+    ${server4Fe} = [ "server" ];
+  };
+
   stableMajorVersion = "1.18";
   mainlineMajorVersion = "1.19";
 
@@ -15,76 +36,101 @@ let
   '';
 in {
   name = "nginx";
-  machine =
-    { lib, pkgs, ... }:
-    {
+  nodes = {
+    server = { lib, pkgs, ... }: {
       imports = [ ../nixos ];
+
+      networking.hosts = lib.mkForce hosts;
+
+      flyingcircus.enc.parameters = {
+        resource_group = "test";
+        interfaces.srv = {
+          mac = "52:54:00:12:01:01";
+          networks = {
+            "${netLoc4Srv}.0/24" = [ server4Srv ];
+            "${netLoc6Srv}/64" = [ server6Srv ];
+          };
+          gateways = {};
+        };
+        interfaces.fe = {
+          mac = "52:54:00:12:02:01";
+          networks = {
+            "${net4Fe}.0/24" = [ server4Fe ];
+            "${net6Fe}/64" = [ server6Fe ];
+          };
+          gateways = {};
+        };
+      };
+
       flyingcircus.services.nginx.enable = true;
 
       # Vhost for localhost is predefined by the nginx module and serves the
       # nginx status page which is expected by the sensu check.
 
       # Vhost for config reload check.
-      services.nginx.virtualHosts.machine = {
+      services.nginx.virtualHosts.server = {
         root = rootInitial;
       };
 
       # Display the nginx version on the 404 page.
       services.nginx.serverTokens = true;
+
+      virtualisation.vlans = [ 1 2 ];
     };
+  };
 
   testScript = { nodes, ... }:
   let
-    sensuCheck = testlib.sensuCheckCmd nodes.machine;
+    sensuCheck = testlib.sensuCheckCmd nodes.server;
   in ''
-    machine.wait_for_unit('nginx.service')
-    machine.wait_for_open_port(80)
+    server.wait_for_unit('nginx.service')
+    server.wait_for_open_port(80)
 
     with subtest("nginx should respond with configured content"):
-      machine.succeed("curl machine | grep -q 'initial content'")
+      server.succeed("curl server | grep -q 'initial content'")
 
     with subtest("running nginx should have the expected version"):
-      machine.succeed("curl machine/404 | grep -q ${stableMajorVersion}")
+      server.succeed("curl server/404 | grep -q ${stableMajorVersion}")
 
     with subtest("nginx should use changed config after reload"):
       # Replace config symlink with a new config file.
-      machine.execute("sed 's#${rootInitial}#${rootChanged}#' /run/nginx/config > /run/nginx/changed_config")
-      machine.execute("mv /run/nginx/changed_config /run/nginx/config")
+      server.execute("sed 's#${rootInitial}#${rootChanged}#' /run/nginx/config > /run/nginx/changed_config")
+      server.execute("mv /run/nginx/changed_config /run/nginx/config")
       # Trigger reload manually because the reload script would reset the symlink.
-      machine.systemctl("kill -s HUP nginx")
-      machine.wait_until_succeeds("curl machine | grep -q 'changed content'")
+      server.systemctl("kill -s HUP nginx")
+      server.wait_until_succeeds("curl server | grep -q 'changed content'")
       # Back to initial configuration from Nix store.
-      machine.systemctl("reload nginx")
-      machine.wait_until_succeeds("curl machine | grep -q 'initial content'")
+      server.systemctl("reload nginx")
+      server.wait_until_succeeds("curl server | grep -q 'initial content'")
 
     with subtest("nginx should use changed binary after reload"):
       # Prepare change to mainline nginx. We are not interested in testing mainline itself here.
       # We only need it as a different version so we can test binary reloading.
-      machine.execute("ln -sfT ${pkgs.nginxMainline} /run/nginx/package")
+      server.execute("ln -sfT ${pkgs.nginxMainline} /run/nginx/package")
       # Mainline doesn't know about our custom remote_addr_anon variable, patch our config.
-      machine.execute("sed 's#remote_addr_anon#remote_addr#' /run/nginx/config > /run/nginx/changed_config")
-      machine.execute("mv /run/nginx/changed_config /run/nginx/config")
+      server.execute("sed 's#remote_addr_anon#remote_addr#' /run/nginx/config > /run/nginx/changed_config")
+      server.execute("mv /run/nginx/changed_config /run/nginx/config")
       # Go to mainline.
-      machine.succeed("nginx-reload-master")
-      machine.wait_until_succeeds("curl machine/404 | grep -q ${mainlineMajorVersion}")
+      server.succeed("nginx-reload-master")
+      server.wait_until_succeeds("curl server/404 | grep -q ${mainlineMajorVersion}")
       # Back to initial binary from nginx stable.
-      machine.systemctl("reload nginx")
-      machine.wait_until_succeeds("curl machine/404 | grep -q ${stableMajorVersion}")
+      server.systemctl("reload nginx")
+      server.wait_until_succeeds("curl server/404 | grep -q ${stableMajorVersion}")
 
     with subtest("service user should be able to write to local config dir"):
-      machine.succeed('sudo -u nginx touch /etc/local/nginx/vhosts.json')
+      server.succeed('sudo -u nginx touch /etc/local/nginx/vhosts.json')
 
     with subtest("all sensu checks should be green"):
-      machine.succeed("${sensuCheck "nginx_config"}")
-      machine.succeed("${sensuCheck "nginx_worker_age"}")
-      machine.succeed("${sensuCheck "nginx_status"}")
+      server.succeed("${sensuCheck "nginx_config"}")
+      server.succeed("${sensuCheck "nginx_worker_age"}")
+      server.succeed("${sensuCheck "nginx_status"}")
 
     with subtest("killing the nginx process should trigger an automatic restart"):
-      machine.succeed("pkill -9 -F /run/nginx/nginx.pid");
-      machine.wait_until_succeeds("${sensuCheck "nginx_status"}")
+      server.succeed("pkill -9 -F /run/nginx/nginx.pid");
+      server.wait_until_succeeds("${sensuCheck "nginx_status"}")
 
     with subtest("status check should be red after shutting down nginx"):
-      machine.systemctl('stop nginx')
-      machine.fail("${sensuCheck "nginx_status"}")
+      server.systemctl('stop nginx')
+      server.fail("${sensuCheck "nginx_status"}")
   '';
 })
