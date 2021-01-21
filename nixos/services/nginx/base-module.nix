flyingcircusio/fc-nixos

commit 9957c2e48c8d30c767c03db83dc92b531c3efc80

Nginx: reuseport for structured vhost config, also recommended for plain config
listen ... reuseport ... starts a separate socket listener for each
Nginx worker process instead of having only one in the master process.
This is meant for higher performance but also allows worker reloading
when the listen IP changes from 127.0.0.1 to 0.0.0.0, for example.

Add an `reuseport` option for `listen` which is on by default.
This affects structured JSON and Nix config.

For plain config files, the option is recommended in the documentation and
example config.

 #PL-129589

nixos/services/nginx/base-module.nix
@@ -235,11 +235,12 @@ let
             then filter (x: x.ssl) defaultListen
             else defaultListen;
 
-        listenString = { addr, port, ssl, extraParameters ? [], ... }:
+        listenString = { addr, port, ssl, reuseport ? true, extraParameters ? [], ... }:
           "listen ${addr}:${toString port} "
           + optionalString ssl "ssl "
           + optionalString (ssl && vhost.http2) "http2 "
           + optionalString vhost.default "default_server "
+          + optionalString reuseport "reuseport "
           + optionalString (extraParameters != []) (concatStringsSep " " extraParameters)
           + ";";
 

nixos/services/nginx/example-config.nix
@@ -7,8 +7,8 @@ let
     builtins.concatStringsSep "\n    "
       (lib.concatMap
         (formatted_addr: [
-          "listen ${formatted_addr}:80;"
-          "listen ${formatted_addr}:443 ssl;"])
+          "listen ${formatted_addr}:80 reuseport;"
+          "listen ${formatted_addr}:443 ssl reuseport;"])
         (map
           (addr:
             if fclib.isIp4 addr then addr else "[${addr}]")

nixos/services/nginx/vhost-options.nix
@@ -35,6 +35,7 @@ with lib;
         addr = mkOption { type = str;  description = "IP address.";  };
         port = mkOption { type = int;  description = "Port number."; default = 80; };
         ssl  = mkOption { type = bool; description = "Enable SSL.";  default = false; };
+        reuseport = mkOption { type = bool; description = "Each worker gets its own socket listener.";  default = true; };
         extraParameters = mkOption { type = listOf str; description = "Extra parameters of this listen directive."; default = []; example = [ "reuseport" "deferred" ]; };
       }; });
       default = [];

commit c4cd0429ac77b198b5b1eecebb0924189535d683

Nginx: only use reuseport on default vhosts
Commit 9957c2e48c8d30c767c03db83dc92b531c3efc80 broke structured vhost
config with more than one vhost that use the same listen ip:port.

Nginx doesn't support adding reuseport to multiple ocurrences of the
same listen ip:port and complains about "duplicate listen options".
This is very annoying for generated configs.

Also change the recommendations in the README to avoid wrong configs.

 # PL-129589

nixos/services/nginx/base-module.nix
@@ -240,7 +240,7 @@ let
           + optionalString ssl "ssl "
           + optionalString (ssl && vhost.http2) "http2 "
           + optionalString vhost.default "default_server "
-          + optionalString reuseport "reuseport "
+          + optionalString (vhost.default && reuseport) "reuseport "
           + optionalString (extraParameters != []) (concatStringsSep " " extraParameters)
           + ";";
 

nixos/services/nginx/example-config.nix
@@ -7,8 +7,8 @@ let
     builtins.concatStringsSep "\n    "
       (lib.concatMap
         (formatted_addr: [
-          "listen ${formatted_addr}:80 reuseport;"
-          "listen ${formatted_addr}:443 ssl reuseport;"])
+          "listen ${formatted_addr}:80 default_server reuseport;"
+          "listen ${formatted_addr}:443 ssl default_server reuseport;"])
         (map
           (addr:
             if fclib.isIp4 addr then addr else "[${addr}]")