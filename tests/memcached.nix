33c357002a2a3d6e7aadb1f0a06b6bc090d071df
Nils Möller <nm@flyingcircus.io>
2021-03-25 10:07:26 +0100

    First batch of perl tests converted to python

--- tests/memcached.nix
+++ tests/memcached.nix
@@ -1,4 +1,4 @@
-import ./make-test.nix ({ ... }:
+import ./make-test-python.nix ({ ... }:
 let 
   ipv4 = "192.168.101.1";
   ipv6 = "2001:db8:f030:1c3::1";
@@ -24,16 +24,16 @@ in {
     };
 
   testScript = ''
-    $machine->waitForUnit('memcached.service');
-    $machine->waitForOpenPort(11211);
+    machine.wait_for_unit('memcached.service')
+    machine.wait_for_open_port(11211)
     # connecting with ipv6 often fails if we don't wait
-    $machine->waitUntilSucceeds("ping -c1 ${ipv6}");
+    machine.wait_until_succeeds("ping -c1 ${ipv6}")
 
-    $machine->succeed("echo -e 'add my_key 0 60 11\\r\\nhello world\\r\\nquit' | nc ::1 11211 | grep STORED");
-    $machine->succeed("echo -e 'get my_key\\r\\nquit' | nc ${ipv4} 11211 | grep 'hello world'");
-    $machine->succeed("echo -e 'get my_key\\r\\nquit' | nc ${ipv6} 11211 | grep 'hello world'");
+    machine.succeed("echo -e 'add my_key 0 60 11\\r\\nhello world\\r\\nquit' | nc ::1 11211 | grep STORED")
+    machine.succeed("echo -e 'get my_key\\r\\nquit' | nc ${ipv4} 11211 | grep 'hello world'")
+    machine.succeed("echo -e 'get my_key\\r\\nquit' | nc ${ipv6} 11211 | grep 'hello world'")
 
     # service user should be able to write to local config dir
-    $machine->succeed('sudo -u memcached touch /etc/local/memcached/memcached.json');
+    machine.succeed('sudo -u memcached touch /etc/local/memcached/memcached.json')
   '';
 })
