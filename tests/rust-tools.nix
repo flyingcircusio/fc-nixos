41ef3230a766e599f74ef5a3efe03846bd0258cd
Tobias Stenzel <ts@flyingcircus.io>
2021-04-06 14:06:20 +0200

    Add a test stub for our rust tools
    We just want to know if they can be executed and cache the build
    results.

---
+++ tests/rust-tools.nix
@@ -0,0 +1,40 @@
+# This is just a stub to check if our rust tools build and to cache them.
+import ./make-test-python.nix ({ pkgs, lib, ... }:
+{
+  name = "rust-tools";
+
+  machine = {
+    imports = [ ../nixos ];
+
+    environment.systemPackages = with pkgs; [
+      fc.check-age
+      fc.check-postfix
+      fc.logcheckhelper
+      fc.multiping
+      fc.roundcube-chpasswd
+      fc.sensusyntax
+      fc.userscan
+    ];
+
+  };
+
+  testScript = ''
+    with subtest("check_age"):
+      machine.succeed("check_age -h")
+
+    with subtest("check-postfix"):
+      machine.succeed("check_mailq -h")
+
+    with subtest("logcheck-helper"):
+      machine.succeed("logcheck-helper -h")
+
+    with subtest("multiping"):
+      machine.succeed("multiping -h")
+
+    with subtest("userscan"):
+      machine.succeed("fc-userscan -h")
+
+    with subtest("userscan"):
+      machine.succeed("fc-userscan -h")
+  '';
+})
