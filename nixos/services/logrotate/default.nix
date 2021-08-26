flyingcircusio/fc-nixos

commit 95e4e56fe53046ad4e60bdea149bd7e1d067439f

ceph: fix log rotation
This also includes a small fix to the overall log rotation code
so that we get debugging output.

nixos/services/logrotate/default.nix
@@ -88,9 +88,20 @@ in
         in listToAttrs (map cfgDir serviceUsers);
 
       systemd.services = {
-        # Upstream puts logrotate in multi-user.target which triggers unwanted
-        # service starts on fc-manage. It should only be activated by the timer.
-        logrotate.wantedBy = lib.mkForce [ ];
+        # XXX logrotate does not allow setting options and I need to rebuild the
+        # command line here so we can get decent debugging output.
+        logrotate = { ... }: {
+          options = {
+            script = lib.mkOption {
+              apply = v: lib.replaceStrings [ "sbin/logrotate /nix" ] [ "sbin/logrotate -v /nix" ] v;
+            };
+          };
+          config = {
+            # Upstream puts logrotate in multi-user.target which triggers unwanted
+            # service starts on fc-manage. It should only be activated by the timer.
+             wantedBy = lib.mkForce [ ];
+          };
+        };
       } // listToAttrs (
         map (u: nameValuePair "user-logrotate-${u.name}" {
           description = "logrotate for ${u.name}";

nixos/services/ceph/server.nix
@@ -44,12 +44,14 @@ in
           create 0644 root adm
           prerotate
               for dmn in $(cd /run/ceph && ls ceph-*.asok 2>/dev/null); do
-                  ceph --admin-daemon /run/ceph/''${dmn} log flush &>/dev/null || true
+                  echo "Flushing log for $dmn"
+                  ${pkgs.ceph}/bin/ceph --admin-daemon /run/ceph/''${dmn} log flush || true
               done
           endscript
           postrotate
               for dmn in $(cd /run/ceph && ls ceph-*.asok 2>/dev/null); do
-                  ceph --admin-daemon /run/ceph/''${dmn} log reopen &>/dev/null || true
+                  echo "Reopening log for $dmn"
+                  ${pkgs.ceph}/bin/ceph --admin-daemon /run/ceph/''${dmn} log reopen || true
               done
           endscript
       }      

commit 96ee6ddaa048965705efd48515528e88ee827a44

Fix logrotate-config-file
Regression introduced by 95e4e56fe53046ad4e60bdea149bd7e1d067439f
because -v was added to the logrotate command line.

nixos/services/logrotate/default.nix
@@ -67,7 +67,7 @@ in
       environment.systemPackages = with pkgs; [
         logrotate
         (pkgs.writeScriptBin "logrotate-config-file" ''
-          grep exec $(systemctl cat logrotate | grep ExecStart= | cut -f 2 -d=) | cut -f 3 -d" "
+          grep exec $(systemctl cat logrotate | grep ExecStart= | cut -f 3 -d=) | cut -f 3 -d" "
         '')
       ];
 