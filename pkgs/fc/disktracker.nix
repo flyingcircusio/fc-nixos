a0e4e1735a3f1abff4c5fb49f19a743939f69314
Nils Möller <nm@flyingcircus.io>
2021-10-05 15:47:17 +0200

    Disktracker: Update Disktracker

--- pkgs/fc/disktracker.nix
+++ pkgs/fc/disktracker.nix
@@ -7,8 +7,8 @@ buildPythonApplication{
   version = "1.0.0";
   src = fetchgit {
     url = "https://gitlab.flyingcircus.io/flyingcircus/fc-disktracker.git";
-    rev = "713a847509764fca17232f4d9b0d633142cdeedb"; #Commit: Add ability to print given SnipeIT token and url
-    sha256 = "0ydx4jqzvl2r0kz6p9ydvj8h5grsfgg6xz6dsfd6cp8zxhwyiqyf";
+    rev = "ce905abb817945164fac6b24f44ff6dc6ba65bf7"; #Commit: Fix disktracker breaking when commandline arguments are missing
+    sha256 = "0261a7nrm0499ixkdvliwx38l73aj50yln2s33jg6az5b4l9ism9";
   };
 
   dontStrip = true;
