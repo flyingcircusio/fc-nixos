dd140e823950a6e3d5fe589efcc5eccb4e026fc9
Maciej Krüger <mkg20001@gmail.com>
2021-10-31 06:52:09 +0100

    fix: fc-get-current-channel-url
    - made release parameter chooseable
    - added error if no params
    - support for h2 (lowercase headers)

--- fc-get-current-channel-url.sh
+++ fc-get-current-channel-url.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-curl --silent --head https://hydra.flyingcircus.io/channel/custom/flyingcircus/fc-19.03-$1/release/nixexprs.tar.xz | grep Location | cut -f 2 -d " "
+set -euo pipefail
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <release> <channel>" >&2
+  exit 2
+fi
+
+RELEASE="$1"
+CHANNEL="$2"
+
+curl --silent --head https://hydra.flyingcircus.io/channel/custom/flyingcircus/fc-$RELEASE-$CHANNEL/release/nixexprs.tar.xz | grep "[Ll]ocation" | cut -f 2 -d " "
