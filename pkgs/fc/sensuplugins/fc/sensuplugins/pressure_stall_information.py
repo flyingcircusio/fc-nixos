a31fb3c17512fb1d1f5bf7830cfe87202f1ecca7
YoEli Kogan-Wang <elikowa@gmail.com>
2021-11-08 15:39:31 +0100

    Make it work and put code review comment in

--- pkgs/fc/sensuplugins/fc/sensuplugins/pressure_stall_information.py
+++ pkgs/fc/sensuplugins/fc/sensuplugins/pressure_stall_information.py
@@ -10,9 +10,10 @@ import argparse
 import sys
 import re
 
-PSI_PATTERN = r'(?<extent>some|full) avg10=(?<avg10>[0-9\.]+) avg60=(?<avg60>[0-9\.]+) avg300=(?<avg300>[0-9\.]+) total=(?:[0-9\.]+)'
+PSI_PATTERN = r'(?P<extent>some|full) avg10=(?P<avg10>[0-9\.]+) avg60=(?P<avg60>[0-9\.]+) avg300=(?P<avg300>[0-9\.]+) total=(?:[0-9\.]+)'
 
 def triple(triple_string):
+    """Parse a triple of numbers. The numbers are separated by commas only."""
     triple_list = triple_string.split(',')
     if(len(triple_list) != 3):
         print('The arguments to the warning values are triples, corresponding'
@@ -25,7 +26,6 @@ def threshold_exceeded(threshold, value):
             return True
     return False
 
-
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('device', choices=['cpu', 'memory', 'io'])
@@ -37,7 +37,6 @@ def main():
         help='Share of time all non-idle tasks stalling considered warning')
     p.add_argument('--full-critical', type=triple,
         help='Share of time all non-idle tasks stalling considered critical')
-
 
     args = p.parse_args()
 
@@ -68,15 +67,17 @@ def main():
 
     exit_code = 0
 
-    if not actual:
+    if set(actual) != set(critical):
         exit_code = max(exit_code, 1)
-        print(f'WARNING - Pressure Stalling Check encountered unexpected data')
-
+        print("WARNING - Pressure Stalling Check encountered unexpected data, or we don't have some critical thresholds for the values we got")
+        print("Values I read: ", actual)
+    
     for extent in actual:
-        if threshold_exceeded(critical[extent], actual[extent]):
+        if extent in critical and threshold_exceeded(critical[extent], actual[extent]):
             exit_code = max(exit_code, 2)
             print(f'CRITICAL - {args.device} Pressure Stalling {extent} {actual[extent]} > {critical[extent]}')
-        if threshold_exceeded(warning[extent], actual[extent]):
+            continue # if critical then don't push out a warning
+        if extent in warning and threshold_exceeded(warning[extent], actual[extent]):
             exit_code = max(exit_code, 1)
             print(f'WARNING - {args.device} Pressure Stalling {extent} {actual[extent]} > {warning[extent]}')
 
