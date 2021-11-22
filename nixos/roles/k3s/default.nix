c9828f1d68c6e1bd4bc7d81e9757540a204bd325
Christian Theune <ct@flyingcircus.io>
2021-11-22 17:29:01 +0100

    k3s: do not allow agent and server role on same machine.

--- nixos/roles/k3s/default.nix
+++ nixos/roles/k3s/default.nix
@@ -7,4 +7,15 @@
     (mkRenamedOptionModule [ "flyingcircus" "roles" "kubernetes-master" ] [ "flyingcircus" "roles" "k3s-server" ])
   ];
 
+  assertions =
+    let server = config.flyingcircus.roles.k3s-server.enable;
+        agent = config.flyingcircus.roles.k3s-agent.enable;
+    in
+    [
+      {
+        assertion = !(server && agent);
+        message = "The k3s-agent role must not be enabled together with the k3s-server role.";
+      }
+    ];
+
 }
