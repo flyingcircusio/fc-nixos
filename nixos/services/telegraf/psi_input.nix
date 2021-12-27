a94a7f418ede8da32e7ef049072939e5334b3d6d
Eli Kogan-Wang <elikowa@gmail.com>
2021-12-27 17:33:08 +0100

    Add telegraf monitoring for pressure stall information for cgroups
    Add regex for cgroup psi
    
    Fix collet typo
    
    rename to camelCase

--- nixos/services/telegraf/psi_input.nix
+++ nixos/services/telegraf/psi_input.nix
@@ -1,12 +1,29 @@
-{ pkgs, ... }:
-{
-  flyingcircus.services.telegraf.inputs = {
-    exec = [{
-      commands = [ "${pkgs.fc.telegraf-collect-psi}/bin/collect_psi" ];
-      timeout = "10s";
-      data_format = "json";
-      json_name_key = "name";
-      tag_keys = ["period" "extent"];
-    }];
+{ pkgs, lib, config, ... }:
+let 
+  inherit (lib) types;
+  psiCgroupRegex = lib.concatStringsSep "|" config.flyingcircus.services.telegraf.psiCgroupRegex;
+in {
+  options.flyingcircus.services.telegraf.psiCgroupRegex = lib.mkOption {
+    type = types.listOf types.str;
+    default = [ ];
+  };
+  config = {
+    flyingcircus.services.telegraf.psiCgroupRegex = [ "^/system.slice$" "^/system.slice.*service$" "^/user.slice$" ];
+    flyingcircus.services.telegraf.inputs = {
+      exec = [{
+        commands = [ "${pkgs.fc.telegraf-collect-psi}/bin/collect_psi" ];
+        timeout = "10s";
+        data_format = "json";
+        json_name_key = "name";
+        tag_keys = ["period" "extent"];
+      }
+      {
+        commands = [ "${pkgs.fc.telegraf-collect-psi}/bin/collect_psi_cgroups --regex \"${psiCgroupRegex}\"" ];
+        timeout = "10s";
+        data_format = "json";
+        json_name_key = "name";
+        tag_keys = ["period" "extent" "cgroup"];
+      }];
+    };
   };
 }
