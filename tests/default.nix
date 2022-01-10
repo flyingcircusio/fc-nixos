3226b3dae52a3f909744e87a95f23886fbcf5aa1
Tobias Stenzel <ts@flyingcircus.io>
2022-01-10 22:24:09 +0100

    drop php56 which doesn't build anymore

--- tests/default.nix
+++ tests/default.nix
@@ -47,7 +47,6 @@ in {
   k3s = callTest ./k3s {};
 
   lamp = callTest ./lamp.nix { };
-  lamp56 = callTest ./lamp.nix { version = "lamp_php56"; };
   lamp73 = callTest ./lamp.nix { version = "lamp_php73"; };
   lamp73_tideways = callTest ./lamp.nix { version = "lamp_php73"; tideways = "1234"; };
   lamp74 = callTest ./lamp.nix { version = "lamp_php74"; };
