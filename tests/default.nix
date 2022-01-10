512e166ea10d5d5b60045a66c65bbf43f8db4d33
Tobias Stenzel <ts@flyingcircus.io>
2022-01-10 14:13:47 +0100

    Activate gitlab test
    It works!

--- tests/default.nix
+++ tests/default.nix
@@ -38,7 +38,7 @@ in {
   ffmpeg = callTest ./ffmpeg.nix {};
   filebeat = callTest ./filebeat.nix {};
   garbagecollect = callTest ./garbagecollect.nix {};
-  #gitlab = callTest ./gitlab.nix {};
+  gitlab = callTest ./gitlab.nix {};
 
   graylog = callTest ./graylog.nix {};
   haproxy = callTest ./haproxy.nix {};
