bbc7a8d8bdbb1d84f2f936f86bdb187f28a4076d
Tobias Stenzel <ts@flyingcircus.io>
2022-01-10 14:17:40 +0100

    Fix login test, update from upstream
    The test didn't finish because the parentheses for the
    method call of wait_for_shutdown were missing.
    Updated the test from upstream, also fixes the problem.

--- tests/login.nix
+++ tests/login.nix
@@ -1,4 +1,4 @@
-import ./make-test-python.nix ({ pkgs, latestKernel ? false, ... }:
+import ./make-test-python.nix ({ pkgs, ... }:
 
 {
   name = "login";
@@ -8,13 +8,12 @@ import ./make-test-python.nix ({ pkgs, latestKernel ? false, ... }:
       imports = [ ../nixos ../nixos/roles ];
 
       services.telegraf.enable = false;
+      sound.enable = true; # needed for the factl test, /dev/snd/* exists without them but udev doesn't care then
 
-      boot.kernelPackages = lib.mkIf latestKernel pkgs.linuxPackages_latest;
     };
 
-  testScript =
-    ''
-      machine.wait_for_unit('multi-user.target')
+  testScript = ''
+      machine.wait_for_unit("multi-user.target")
       machine.wait_until_succeeds("pgrep -f 'agetty.*tty1'")
       machine.screenshot("postboot")
 
@@ -22,17 +21,14 @@ import ./make-test-python.nix ({ pkgs, latestKernel ? false, ... }:
           machine.succeed("useradd -m alice")
           machine.succeed("(echo foobar; echo foobar) | passwd alice")
 
-      # Check whether switching VTs works.
-      with subtest("virtual console switching"):
+      with subtest("Check whether switching VTs works"):
           machine.fail("pgrep -f 'agetty.*tty2'")
           machine.send_key("alt-f2")
-
           machine.wait_until_succeeds("[ $(fgconsole) = 2 ]")
-          machine.wait_for_unit('getty@tty2.service')
+          machine.wait_for_unit("getty@tty2.service")
           machine.wait_until_succeeds("pgrep -f 'agetty.*tty2'")
 
-      # Log in as alice on a virtual console.
-      with subtest("virtual console login"):
+      with subtest("Log in as alice on a virtual console"):
           machine.wait_until_tty_matches(2, "login: ")
           machine.send_chars("alice\n")
           machine.wait_until_tty_matches(2, "login: alice")
@@ -43,9 +39,7 @@ import ./make-test-python.nix ({ pkgs, latestKernel ? false, ... }:
           machine.send_chars("touch done\n")
           machine.wait_for_file("/home/alice/done")
 
-      # Check whether systemd gives and removes device ownership as
-      # needed.
-      with subtest("device permissions"):
+      with subtest("Systemd gives and removes device ownership as needed"):
           machine.succeed("getfacl /dev/snd/timer | grep -q alice")
           machine.send_key("alt-f1")
           machine.wait_until_succeeds("[ $(fgconsole) = 1 ]")
@@ -53,15 +47,13 @@ import ./make-test-python.nix ({ pkgs, latestKernel ? false, ... }:
           machine.succeed("chvt 2")
           machine.wait_until_succeeds("getfacl /dev/snd/timer | grep -q alice")
 
-      # Log out.
-      with subtest("virtual console logout"):
+      with subtest("Virtual console logout"):
           machine.send_chars("exit\n")
           machine.wait_until_fails("pgrep -u alice bash")
-          machine.screenshot("mingetty")
+          machine.screenshot("getty")
 
-      # Check whether ctrl-alt-delete works.
-      with subtest("ctrl-alt-delete"):
+      with subtest("Check whether ctrl-alt-delete works"):
           machine.send_key("ctrl-alt-delete")
-          machine.wait_for_shutdown
+          machine.wait_for_shutdown()
     '';
 })
