1debc076e63a5d890c3abef82622d6743732e35e
Eli Kogan-Wang <elikowa@gmail.com>
2022-01-18 00:23:05 +0100

    fix k3s test, add generator for images

---
+++ tests/k3s/airgapped-k3s-images.nix
@@ -0,0 +1,59 @@
+[
+{
+  imageName = "docker.io/rancher/klipper-helm";
+  imageDigest = "sha256:961459b8641aa77dae39c96ba8efba88805a8949dcaafa131d1aecc1510128bf";
+  sha256 = "1bs136s6v7gqfxijz31ryzb5jlyfc3pnswnmncwb6ygfvdqg2zk9";
+  finalImageName = "docker.io/rancher/klipper-helm";
+  finalImageTag = "v0.6.6-build20211022";
+}
+{
+  imageName = "docker.io/rancher/klipper-lb";
+  imageDigest = "sha256:d5ec8350ee1a80ec783effe95a2fefae371d1498ac04865cb6024499882f291c";
+  sha256 = "0biq9rjjqwi9fcxd9fhwlrbnjpmgsfwdq90alxpwbq6zqr3h62jk";
+  finalImageName = "docker.io/rancher/klipper-lb";
+  finalImageTag = "v0.3.4";
+}
+{
+  imageName = "docker.io/rancher/local-path-provisioner";
+  imageDigest = "sha256:d5999b20a1b180940061677db3bdb48dd7eb432cd48147c4ff15469fb74ade80";
+  sha256 = "01q9cfiw2r3lb35hmjr5qbc8g5chrjl894bvdf3w0jysb029x2y2";
+  finalImageName = "docker.io/rancher/local-path-provisioner";
+  finalImageTag = "v0.0.20";
+}
+{
+  imageName = "docker.io/rancher/mirrored-coredns-coredns";
+  imageDigest = "sha256:1113439e909a4b03b85f080d63df48734520356bd34da890bc2b5583467ba43b";
+  sha256 = "0xgzx1j6lzqwzrwcgq6hhbdxqx49ssv2wciixsqamfc97i7sd465";
+  finalImageName = "docker.io/rancher/mirrored-coredns-coredns";
+  finalImageTag = "1.8.4";
+}
+{
+  imageName = "docker.io/rancher/mirrored-library-busybox";
+  imageDigest = "sha256:6216b36ad2a781614fa5f2850b70e318c7a4c4384730072a9a35fa946acb14e8";
+  sha256 = "0g3w9ks10f1v8hspwg7pfj7bjad9y52n4nyvk9c0vnzs9q90ag7a";
+  finalImageName = "docker.io/rancher/mirrored-library-busybox";
+  finalImageTag = "1.32.1";
+}
+{
+  imageName = "docker.io/rancher/mirrored-library-traefik";
+  imageDigest = "sha256:7523a6c0ff383450739451e3cbf9206c149f14c51bbf2bb469486653bea1e337";
+  sha256 = "0jn2j0bk5i5qlwayw5syhvi890bgc7qlphqw5yw28q8diaxlz6l1";
+  finalImageName = "docker.io/rancher/mirrored-library-traefik";
+  finalImageTag = "2.5.0";
+}
+{
+  imageName = "docker.io/rancher/mirrored-metrics-server";
+  imageDigest = "sha256:37e86275eb9821e933925640bc05fd1403eee34f214f5ed5716a44f26502e510";
+  sha256 = "1435w1jfjk1g2ykw4v5s474rmb27iv38vz0b2bmgxc4iifwvqz0r";
+  finalImageName = "docker.io/rancher/mirrored-metrics-server";
+  finalImageTag = "v0.5.0";
+}
+{
+  imageName = "docker.io/rancher/mirrored-pause";
+  imageDigest = "sha256:82f4f58a1877a3a819de055a111b33fc63127b896b928b0bd7419da549f88d65";
+  sha256 = "11wp0msgizai32r5g0ny071psdjj0pv37v7g7xa0711hqh5ndxfm";
+  finalImageName = "docker.io/rancher/mirrored-pause";
+  finalImageTag = "3.1";
+}
+
+]
---
+++ tests/k3s/airgapped-k3s-images.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i python3 -p "python3.withPackages (ps: [ ps.requests ])" -p nix-prefetch-docker
+
+import requests
+import re
+import subprocess
+
+commit = "f7dcc139ffb328a5fe4a6d44c223e6add8e936ed"
+
+url = f"https://raw.githubusercontent.com/k3s-io/k3s/{commit}/scripts/airgap/image-list.txt"
+
+# file looks like this:
+# docker.io/rancher/klipper-helm:v0.6.6-build20211022
+# docker.io/rancher/klipper-lb:v0.3.4
+# docker.io/rancher/local-path-provisioner:v0.0.20
+# docker.io/rancher/mirrored-coredns-coredns:1.8.4
+# docker.io/rancher/mirrored-library-busybox:1.32.1
+# docker.io/rancher/mirrored-library-traefik:2.5.0
+# docker.io/rancher/mirrored-metrics-server:v0.5.0
+# docker.io/rancher/mirrored-pause:3.1
+
+image_regex = r"(?P<image>[^:]+):(?P<tag>[^:]+)"
+
+if __name__ == "__main__":
+    r = requests.get(url)
+    # split lines
+    lines = r.text.splitlines()
+    # parse lines
+    images = [re.match(image_regex, line).groupdict() for line in lines]
+    # prefetch images
+    imageNix = []
+    for image in images:
+        print(f"prefetching {image['image']}:{image['tag']}")
+        p = subprocess.run(["nix-prefetch-docker", f"{image['image']}", f"{image['tag']}"], capture_output=True)
+        # stdout contains nix code to neccessary to fetch the image
+        imageNix.append(p.stdout.decode("utf-8"))
+    # write to file
+    with open("airgapped-k3s-images.nix", "w") as f:
+        f.write(f"""[
+{"".join(imageNix)}
+]""")
+    print("done")
+
