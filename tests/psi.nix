ec0f3d157d7f241c4f81da4c8dee862b12bfbbc2
Eli Kogan-Wang <elikowa@gmail.com>
2022-01-20 12:08:50 +0100

    work on psi test

--- tests/psi.nix
+++ tests/psi.nix
@@ -6,6 +6,19 @@ import ./make-test-python.nix ({ ... }:
     {
       imports = [ ../nixos ../nixos/roles ];
       config = {
+        flyingcircus.enc.parameters = {
+          resource_group = "test";
+          interfaces.srv = {
+            mac = "52:54:00:12:34:57";
+            bridged = false;
+            networks = {
+              "192.168.1.0/24" = [ "192.168.1.1" ];
+            };
+            gateways = {};
+          };
+        };
+        services.telegraf.enable = true;  # set in infra/fc but not in infra/testing
+        
       };
     };
     machinecgroup = { lib, ... }:
@@ -13,6 +26,18 @@ import ./make-test-python.nix ({ ... }:
       imports = [ ../nixos ../nixos/roles ];
       config = {
         flyingcircus.services.telegraf.psiCgroupRegex = [ ".*\\.service" ];
+        flyingcircus.enc.parameters = {
+          resource_group = "test";
+          interfaces.srv = {
+            mac = "52:54:00:12:34:58";
+            bridged = false;
+            networks = {
+              "192.168.1.0/24" = [ "192.168.1.2" ];
+            };
+            gateways = {};
+          };
+        };
+        services.telegraf.enable = true;  # set in infra/fc but not in infra/testing        
       };
     };
   };
@@ -27,8 +52,7 @@ import ./make-test-python.nix ({ ... }:
       machine.fail("curl machine:9126 | grep cgroup")
     
     with subtest("psi data should show up and cgroup data should also show up"):
-      machinecgroup.wait_until_succeeds("curl machine:9126 | grep psi")
-      machinecgroup.succeed("curl machine:9126 | grep cgroup")
-
+      machinecgroup.wait_until_succeeds("curl machinecgroup:9126 | grep psi")
+      machinecgroup.succeed("curl machinecgroup:9126 | grep cgroup")
   '';
-}
+})
