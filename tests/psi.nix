246334dfc6ad2ad5a593e0c5a089825fdda0ca75
Eli Kogan-Wang <elikowa@gmail.com>
2022-01-25 19:30:32 +0100

    Add test for psi cgroups

--- tests/psi.nix
+++ tests/psi.nix
@@ -1,58 +1,37 @@
-import ./make-test-python.nix ({ ... }:
+import ./make-test-python.nix ({ testlib, ... }:
 {
   name = "psi";
-  nodes = {
-    machine = { lib, ... }:
-    {
-      imports = [ ../nixos ../nixos/roles ];
-      config = {
-        flyingcircus.enc.parameters = {
-          resource_group = "test";
-          interfaces.srv = {
-            mac = "52:54:00:12:34:57";
-            bridged = false;
-            networks = {
-              "192.168.1.0/24" = [ "192.168.1.1" ];
-            };
-            gateways = {};
-          };
-        };
+  testCases = {
+    justpsi = { 
+      name = "justpsi";
+      machine = { lib, ... }:
+      {
+        imports = [ (testlib.fcConfig { net.fe = false; }) ];
         services.telegraf.enable = true;  # set in infra/fc but not in infra/testing
-        
       };
+      testScript = ''
+        start_all()
+        machine.wait_for_unit("telegraf.service")
+        with subtest("psi data should show up and no cgroup data should pop up"):
+          machine.wait_until_succeeds("curl machine:9126/metrics | grep psi")
+          machine.fail("curl machine:9126/metrics | grep cgroup")
+      '';
     };
-    machinecgroup = { lib, ... }:
-    {
-      imports = [ ../nixos ../nixos/roles ];
-      config = {
+    psicgroup = {
+      name = "psicgroup";
+      machine = { lib, ... }:
+      {
+        imports = [ (testlib.fcConfig { net.fe = false; }) ];
         flyingcircus.services.telegraf.psiCgroupRegex = [ ".*\\.service" ];
-        flyingcircus.enc.parameters = {
-          resource_group = "test";
-          interfaces.srv = {
-            mac = "52:54:00:12:34:58";
-            bridged = false;
-            networks = {
-              "192.168.1.0/24" = [ "192.168.1.2" ];
-            };
-            gateways = {};
-          };
-        };
         services.telegraf.enable = true;  # set in infra/fc but not in infra/testing        
       };
+      testScript = ''
+        start_all()
+        machine.wait_for_unit("telegraf.service")
+        with subtest("psi data should show up and cgroup data should also show up"):
+          machine.wait_until_succeeds("curl machine:9126/metrics | grep psi")
+          machine.succeed("curl machine:9126/metrics | grep cgroup")
+      '';
     };
   };
-  testScript = ''
-    start_all()
-    machines = [machine, machinecgroup]
-    for machine in machines:
-      machine.wait_for_unit("telegraf.service")
-    
-    with subtest("psi data should show up and no cgroup data should pop up"):
-      machine.wait_until_succeeds("curl machine:9126 | grep psi")
-      machine.fail("curl machine:9126 | grep cgroup")
-    
-    with subtest("psi data should show up and cgroup data should also show up"):
-      machinecgroup.wait_until_succeeds("curl machinecgroup:9126 | grep psi")
-      machinecgroup.succeed("curl machinecgroup:9126 | grep cgroup")
-  '';
 })
