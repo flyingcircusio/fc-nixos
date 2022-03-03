ea6ad4e937f05fd11d3c8a9b0ffe303d35c597b4
Eli Kogan-Wang <elikowa@gmail.com>
2022-03-03 16:29:35 +0100

    Search entire dmesg

--- pkgs/fc/check-xfs-broken/default.nix
+++ pkgs/fc/check-xfs-broken/default.nix
@@ -2,15 +2,15 @@
 pkgs.writeScript "check-xfs-broken.sh" ''
   # script to check if xfs is broken
   # does so by checking if '"echo 0 > /proc/sys/kernel/hung_task_timeout_secs" disables this message.'
-  # is in `dmesg --kernel`
+  # is in `dmesg`
 
   # if it is, then output the lines of dmesg that contain this message, and exit with a exit code of 2
 
-  ${pkgs.util-linux}/bin/dmesg --kernel | grep -q '"echo 0 > /proc/sys/kernel/hung_task_timeout_secs" disables this message.'
+  ${pkgs.util-linux}/bin/dmesg | grep -q '"echo 0 > /proc/sys/kernel/hung_task_timeout_secs" disables this message.'
   # quiet grep
   if [ $? -eq 0 ]; then
     echo "CRITICAL - xfs is broken, offending lines:"
-    ${pkgs.util-linux}/bin/dmesg --kernel | grep '"echo 0 > /proc/sys/kernel/hung_task_timeout_secs" disables this message.'
+    ${pkgs.util-linux}/bin/dmesg | grep '"echo 0 > /proc/sys/kernel/hung_task_timeout_secs" disables this message.'
     exit 2
   fi
   echo "OK - xfs is not broken"
