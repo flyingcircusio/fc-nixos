833fd47f003995f45910559d010a4c310e79d76a
Christian Theune <ct@flyingcircus.io>
2022-05-05 10:24:36 +0200

    lamp: disable proxy reuse to avoid apache confusing the fpm pools
    update tideways daemon and module

--- pkgs/tideways/daemon.nix
+++ pkgs/tideways/daemon.nix
@@ -2,7 +2,7 @@
 
 assert stdenv.hostPlatform.system == "x86_64-linux";
 
-let version = "1.6.32"; in
+let version = "1.7.28"; in
 
 stdenv.mkDerivation {
   name = "tideways-daemon-${version}";
@@ -11,7 +11,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://s3-eu-west-1.amazonaws.com/tideways/daemon/${version}/tideways-daemon_linux_amd64-${version}.tar.gz";
-    sha256 = "0ksnq6z89n9y8qyd46slia5ykqiwz7qvw53c9f9cyjbfbkqkzzzc";
+    sha256 = "sha256-9FezE7/yYiZmYC3PjPNMR6Lt0sqa5PMB2q3GK0H+1bY";
   };
 
   meta = {
