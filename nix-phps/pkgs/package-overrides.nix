f5d45eac9b84ebff4f5e65e8eb9fd29099baec06
Oliver Schmidt <os@flyingcircus.io>
2022-05-05 19:51:34 +0200

    php73-dom: fix patch removal of specific tests
    since php-7.3.25, the tests file paths for tests/bug80268.phpt and
    tests/bug43364.phpt have moved.

--- nix-phps/pkgs/package-overrides.nix
+++ nix-phps/pkgs/package-overrides.nix
@@ -75,12 +75,12 @@ in
 
           (lib.optionalString (lib.versionOlder prev.php.version "7.4" && lib.versionAtLeast prev.php.version "7.3") ''
             # 4cc261aa6afca2190b1b74de39c3caa462ec6f0b deletes this file but fetchpatch does not support deletions.
-            rm tests/bug80268.phpt
+            rm ext/dom/tests/bug80268.phpt
           '')
 
           (lib.optionalString (lib.versionOlder prev.php.version "7.4") ''
             # 4cc261aa6afca2190b1b74de39c3caa462ec6f0b deletes this file but fetchpatch does not support deletions.
-            rm tests/bug43364.phpt
+            rm ext/dom/tests/bug43364.phpt
           '')
         ];
     });
