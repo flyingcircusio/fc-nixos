c986d817caee34c20ec69e99bde551d040e048ac
Tobias Stenzel <ts@flyingcircus.io>
2022-07-28 19:23:22 +0200

    Remove bash_5_1_p12 package, upstream bash is newer

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -26,7 +26,6 @@ in {
   # imports from other nixpkgs versions or local definitions
   #
 
-  bash_5_1_p12 = super.callPackage ./bash/5.1.nix { };
   bundlerSensuPlugin = super.callPackage ./sensuplugins-rb/bundler-sensu-plugin.nix { };
   busybox = super.busybox.overrideAttrs (oldAttrs: {
       meta.priority = 10;
