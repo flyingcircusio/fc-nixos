c4c1a65e3ce76dfea91c34c72772c72bfb8698ae
Tobias Stenzel <ts@flyingcircus.io>
2022-08-05 13:16:18 +0200

    Replace non-code occurences of 21.11 with 22.05

--- nixos/roles/mysql.nix
+++ nixos/roles/mysql.nix
@@ -294,7 +294,7 @@ in
       `sudo systemctl restart mysql`
 
       For more information, see our documentation at
-      https://doc.flyingcircus.io/roles/fc-21.11-production/mysql.html
+      https://doc.flyingcircus.io/roles/fc-22.05-production/mysql.html
     '';
 
     systemd.services.fc-mysql-post-init = {
--- pkgs/fc/default.nix
+++ pkgs/fc/default.nix
@@ -6,13 +6,13 @@ rec {
   agent = callPackage ./agent {};
 
   check-age = callPackage ./check-age {};
-  # XXX: ceph is broken on 21.11
+  # XXX: ceph is broken, needs integration of changes from 21.05
   # check-ceph = callPackage ./check-ceph {};
   check-haproxy = callPackage ./check-haproxy {};
   check-journal = callPackage ./check-journal.nix {};
   check-mongodb = callPackage ./check-mongodb {};
   check-postfix = callPackage ./check-postfix {};
-  # XXX: ceph is broken on 21.11
+  # XXX: ceph is broken, needs integration of changes from 21.05
   # ceph = callPackage ./ceph { inherit blockdev agent util-physical; };
   check-xfs-broken = callPackage ./check-xfs-broken {};
   blockdev = callPackage ./blockdev {};
@@ -26,7 +26,7 @@ rec {
   sensuplugins = callPackage ./sensuplugins {};
   sensusyntax = callPackage ./sensusyntax {};
   userscan = callPackage ./userscan.nix {};
-  # XXX: ceph is broken on 21.11
+  # XXX: ceph is broken, needs integration of changes from 21.05
   # util-physical = callPackage ./util-physical {};
   telegraf-collect-psi = callPackage ./telegraf-collect-psi {};
 
