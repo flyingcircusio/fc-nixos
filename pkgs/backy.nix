1acbf298589a99a1dcf9b22c494e722871b363a8
Eli Kogan-Wang <elikowa@gmail.com>
2022-08-12 11:16:39 +0200

    workaround poetry2nix bug

--- pkgs/backy.nix
+++ pkgs/backy.nix
@@ -5,13 +5,15 @@
   lzo ? pkgs.lzo,
   ...
 }:
-poetry2nix.mkPoetryApplication {
-    projectDir = fetchFromGitHub {
-      owner = "flyingcircusio";
-      repo = "backy";
-      rev = "9ff642ab45f295984bbac797509921c76c3a0e2b";
-      sha256 = "sha256:085ll6mrvk02kzmhyz75ld61mx5brrk78wlcgbapd78vyb7x30c8";
-    };
+let src = fetchFromGitHub {
+  owner = "flyingcircusio";
+  repo = "backy";
+  rev = "9ff642ab45f295984bbac797509921c76c3a0e2b";
+  sha256 = "sha256:085ll6mrvk02kzmhyz75ld61mx5brrk78wlcgbapd78vyb7x30c8";
+};
+in poetry2nix.mkPoetryApplication {
+    projectDir = src;
+    src = src;
     overrides = poetry2nix.overrides.withDefaults (self: super: {
       python-lzo = super.python-lzo.overrideAttrs (old: {
         buildInputs = [ lzo ];
