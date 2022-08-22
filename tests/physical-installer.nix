52d2680ea62fe87083969a1e45249e725347ded1
Tobias Stenzel <ts@flyingcircus.io>
2022-08-22 23:06:28 +0200

    tests: fix some type errors

--- tests/physical-installer.nix
+++ tests/physical-installer.nix
@@ -39,38 +39,38 @@ import ./make-test-python.nix ({ nixpkgs, ... }:
     print(machine.succeed("lsblk"))
     print(machine.succeed("fc-secure-erase /dev/vdc"))
 
-    machine.wait_until_tty_matches(1, "nixos@machine")
+    machine.wait_until_tty_matches("1", "nixos@machine")
 
     machine.screenshot('01boot')
 
     machine.send_chars("sudo -i\n")
-    machine.wait_until_tty_matches(1, "root@machine")
+    machine.wait_until_tty_matches("1", "root@machine")
 
     machine.screenshot('02sudo')
 
     machine.send_chars("fc-install\n")
-    machine.wait_until_tty_matches(1, "52:54:00:12:34:56")
-    machine.wait_until_tty_matches(1, "Ready to continue")
+    machine.wait_until_tty_matches("1", "52:54:00:12:34:56")
+    machine.wait_until_tty_matches("1", "Ready to continue")
     machine.screenshot('03lldp')
     machine.send_chars("\n")
 
-    machine.wait_until_tty_matches(1, "ENC wormhole URL")
+    machine.wait_until_tty_matches("1", "ENC wormhole URL")
     machine.send_chars("file:///tmp/wormhole.json\n")
     machine.screenshot('04wormhole')
 
-    machine.wait_until_tty_matches(1, "Root disk")
+    machine.wait_until_tty_matches("1", "Root disk")
     # Erase the default
     for x in "sda":
       machine.send_key("backspace")
     machine.send_chars("vdb\n")
     machine.screenshot('05rootdisk')
 
-    machine.wait_until_tty_matches(1, "Root password:")
+    machine.wait_until_tty_matches("1", "Root password:")
     machine.send_chars("asdf\n")
-    machine.wait_until_tty_matches(1, "IPMI password:")
+    machine.wait_until_tty_matches("1", "IPMI password:")
     machine.send_chars("asdf2\n")
     machine.screenshot("06passwords")
-    machine.wait_until_tty_matches(1, "Wipe whole disk?")
+    machine.wait_until_tty_matches("1", "Wipe whole disk?")
 
     machine.execute("ln -sf /dev/vdb /dev/disk/by-id/wwn-34789374891")
 
@@ -79,7 +79,7 @@ import ./make-test-python.nix ({ nixpkgs, ... }:
 
     # This is how far I got creating a test. We now would have to create
     # a fake server serving the channel and the nix store...
-    machine.wait_until_tty_matches(1, "error: unable to download")
+    machine.wait_until_tty_matches("1", "error: unable to download")
 
     machine.screenshot("99finish")
 
