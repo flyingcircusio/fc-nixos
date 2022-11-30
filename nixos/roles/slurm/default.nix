fd29d07b0b62dea39accf919908700496249cd00
Tobias Stenzel <ts@flyingcircus.io>
2022-11-30 18:33:04 +0100

    slurm: role changes, move readme doc tools to fclib, add dynamic README

--- nixos/roles/slurm/default.nix
+++ nixos/roles/slurm/default.nix
@@ -4,13 +4,46 @@ let
   cfg = config.flyingcircus.slurm;
   slurmCfg = config.services.slurm;
   inherit (config) fclib;
-  roleEnabled = config.flyingcircus.roles.slurm-node.enable || config.flyingcircus.roles.slurm-controller.enable;
+  controllerEnabled = config.flyingcircus.roles.slurm-controller.enable;
+  nodeEnabled = config.flyingcircus.roles.slurm-node.enable;
+  anyRoleEnabled = controllerEnabled || nodeEnabled;
 
-  controlMachine = "slurmtest10";
-  nodes = [ "slurmtest10" "slurmtest11" "slurmtest12" ];
-  nodeStr = lib.concatStringsSep "," nodes;
-  memory = 1983;
-  cores = 2;
+  serviceHostName = service: lib.head ( lib.splitString "." service.address );
+
+  controlMachine = serviceHostName (fclib.findOneService "slurm-controller-controller");
+  defaultSlurmNodes = map serviceHostName (fclib.findServices "slurm-node-node");
+
+  nodeStr = lib.concatStringsSep "," cfg.nodes;
+
+  thisNode = config.networking.hostName;
+
+  # XXX: C&P from upstream slurm to get access to the wrapper.
+  wrappedSlurm = pkgs.stdenv.mkDerivation {
+    name = "wrappedSlurm";
+
+    builder = pkgs.writeText "builder.sh" ''
+      source $stdenv/setup
+      mkdir -p $out/bin
+      find  ${lib.getBin slurmCfg.package}/bin -type f -executable | while read EXE
+      do
+        exename="$(basename $EXE)"
+        wrappername="$out/bin/$exename"
+        cat > "$wrappername" <<EOT
+      #!/bin/sh
+      if [ -z "$SLURM_CONF" ]
+      then
+        SLURM_CONF="${slurmCfg.etcSlurm}/slurm.conf" "$EXE" "\$@"
+      else
+        "$EXE" "\$0"
+      fi
+      EOT
+        chmod +x "$wrappername"
+      done
+
+      mkdir -p $out/share
+      ln -s ${lib.getBin slurmCfg.package}/share/man $out/share/man
+    '';
+  };
 
 in
 {
@@ -21,9 +54,25 @@ in
       partitionName = mkOption {
         type = types.str;
         default = "all";
-        description = ''
-          Name of the default partition which is automatically generated from all known slurm nodes in the
-          resource group. Don't use `default` as partition name, it will fail!
+        description = lib.mdDoc ''
+          Name of the default partition which includes the machines defined via the `nodes` option.
+          Don't use `default` as partition name, it will fail!
+        '';
+      };
+
+      mungeKeyFile = mkOption {
+        type = types.str;
+        default = "/var/lib/munge/munge.key";
+      };
+
+      nodes = mkOption {
+        type = types.listOf types.str;
+        default = defaultSlurmNodes;
+        defaultText = lib.mdDoc "all Slurm nodes in the resource group";
+        description = lib.mdDoc ''
+          Names of the nodes that are added to the automatically generated partition.
+          By default, all Slurm nodes in a resource group are part of the partition
+          called `partitionName`.
         '';
       };
 
@@ -42,65 +91,131 @@ in
     };
   };
 
-  config = lib.mkIf roleEnabled {
+  config = lib.mkMerge [
 
-    environment.etc.slurmd.source = slurmCfg.etcSlurm;
+    (lib.mkIf anyRoleEnabled {
+      environment.etc.slurmd.source = slurmCfg.etcSlurm;
 
-    environment.systemPackages = [
-      (pkgs.writeShellScriptBin "slurm-config-dir" "echo ${slurmCfg.etcSlurm}")
-      (pkgs.writeShellScriptBin "slurm-show-config" ''
-        for x in ${slurmCfg.etcSlurm}/*; do
-          echo "''${x}:"
-          cat $x
-        done
-      '')
-    ];
+      environment.etc."local/slurm/README.md".text =
+        let
+          roleStr =
+            if nodeEnabled && controllerEnabled
+            then "slurm controller and node"
+            else if controllerEnabled
+            then "slurm controller"
+            else "slurm node";
+        in
+        ''
+        # Slurm Workload Manager
 
+        This VM is acting as ${roleStr}.
+        Generated config is at `${slurmCfg.etcSlurm}`.
 
-    flyingcircus.passwordlessSudoRules = [
-      {
-        commands = [ "ALL" ];
-        groups = [ "sudo-srv" ];
-        runAs = "slurm";
-      }
-    ];
+        The role works without additional config.
 
-    services.slurm = {
-      inherit controlMachine;
-      client.enable = true;
-      server.enable = config.flyingcircus.roles.slurm-controller.enable;
-      nodeName = [ "${nodeStr} CPUs=${toString cores} State=UNKNOWN" ];
-      partitionName = [ "${cfg.partitionName} Nodes=${nodeStr} Default=YES MaxTime=INFINITE State=UP" ];
-      extraConfig = ''
-        SelectType = select/cons_res
-        SelectTypeParameters = CR_CPU_Memory
+        There's an automatically generated partition named `${cfg.partitionName}`.
+
+        The following nodes are members of the `${cfg.partitionName}` partition:
+        `${fclib.docList cfg.nodes}`
+
+        ## NixOS Options
+
+        ${fclib.docOption "flyingcircus.slurm.nodes"}
+
+        ${fclib.docOption "flyingcircus.slurm.partitionName"}
       '';
-    };
 
-    systemd.services.slurmd = {
-      after = [ "munged.service" ];
-      serviceConfig = {
-        Restart = "always";
+      environment.systemPackages = [
+        (pkgs.writeShellScriptBin "slurm-config-dir" "echo ${slurmCfg.etcSlurm}")
+        (pkgs.writeShellScriptBin "slurm-show-config" ''
+          for x in ${slurmCfg.etcSlurm}/*; do
+            echo "''${x}:"
+            cat $x
+          done
+        '')
+      ];
+
+      flyingcircus.passwordlessSudoRules = [
+        {
+          commands = [ "ALL" ];
+          groups = [ "sudo-srv" ];
+          runAs = "slurm";
+        }
+      ];
+
+      services.slurm = {
+        inherit controlMachine;
+        nodeName = [ "${nodeStr} State=UNKNOWN" ];
+        partitionName = [ "${cfg.partitionName} Nodes=${nodeStr} Default=YES MaxTime=INFINITE State=UP" ];
+        extraConfig = ''
+          SelectType = select/cons_res
+          SelectTypeParameters = CR_CPU_Memory
+        '';
       };
-    };
 
+      systemd.services.fc-set-munge-key = {
+        path = [ pkgs.jq ];
+        script = ''
+          umask 0266
+          jq -r \
+            '.[] | select(.service =="slurm-controller-controller") | .password' \
+            /etc/nixos/services.json | sha256sum | head -c64 \
+            > ${cfg.mungeKeyFile}
 
-    systemd.services.slurmctld = {
-      serviceConfig = {
-        Restart = "always";
+          chown munge:munge ${cfg.mungeKeyFile}
+          echo "fc-set-munge-key finished"
+        '';
+
+        serviceConfig = {
+          Type = "oneshot";
+        };
       };
-    };
 
-    systemd.tmpfiles.rules = [
-      "f /etc/munge/munge.key 0400 munge munge - chMrKFAcXMtZACNZUq6A11dIvWn6BJaL"
-    ];
+      systemd.services.munged = {
+        after = [ "fc-set-munge-key.service" ];
+        requires = [ "fc-set-munge-key.service" ];
+        serviceConfig = {
+          ExecStartPre = lib.mkForce [
+            "${pkgs.coreutils}/bin/stat ${cfg.mungeKeyFile}"
+          ];
+        };
+      };
 
-    services.munge.password = "/etc/munge/munge.key";
+      services.munge.password = cfg.mungeKeyFile;
+    })
+
+    (lib.mkIf config.flyingcircus.roles.slurm-controller.enable {
+      services.slurm = {
+        server.enable = true;
+      };
+
+      systemd.services.slurmctld = {
+        serviceConfig = {
+          Restart = "always";
+        };
+      };
+
+    })
+
+    (lib.mkIf config.flyingcircus.roles.slurm-node.enable {
+
+      flyingcircus.agent.maintenance.slurm-node = {
+        enter = "${wrappedSlurm}/bin/scontrol update nodename=${thisNode} state=drain reason='activating maintenance mode'";
+        leave = "${wrappedSlurm}/bin/scontrol update nodename=${thisNode} state=resume";
+      };
+
+      services.slurm = {
+        client.enable = true;
+      };
+
+      systemd.services.slurmd = {
+        after = [ "munged.service" ];
+        serviceConfig = {
+          Restart = "always";
+        };
+      };
 
 
-
-
-    # XXX: tmpfs???
-
-  };
+    })
+  ];
 }
