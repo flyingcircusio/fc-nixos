bb9c8506c134e2018bfc47746b112741197331ed
Tobias Stenzel <ts@flyingcircus.io>
2022-12-18 00:13:04 +0100

    slurm: controller and node roles
    - slurm-controller: central control service (server, slurmctld)
      - should be set up first, nodes later
      - drain jobs on all nodes before going into maintenance
      - resume all in-service nodes after coming back from maintenance
    - slurm-node: compute node that does the work (client, slurmd)
      - automatically join the partition all
      - drain jobs before going into maintenance
      - don't resume
    - fc-slurm command is used by agent for maintenance entry+exit and can
      be used as interactive tool

---
+++ nixos/roles/slurm/default.nix
@@ -0,0 +1,232 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfg = config.flyingcircus.slurm;
+  slurmCfg = config.services.slurm;
+  inherit (config) fclib;
+  controllerEnabled = config.flyingcircus.roles.slurm-controller.enable;
+  nodeEnabled = config.flyingcircus.roles.slurm-node.enable;
+  anyRoleEnabled = controllerEnabled || nodeEnabled;
+
+  serviceHostName = service: lib.head ( lib.splitString "." service.address );
+
+  controlMachine = serviceHostName (fclib.findOneService "slurm-controller-controller");
+  defaultSlurmNodes = map serviceHostName (fclib.findServices "slurm-node-node");
+
+  nodeStr = lib.concatStringsSep "," cfg.nodes;
+
+  thisNode = config.networking.hostName;
+
+  # XXX: C&P from upstream slurm to get access to the wrapper.
+  wrappedSlurm = pkgs.stdenv.mkDerivation {
+    name = "wrappedSlurm";
+
+    builder = pkgs.writeText "builder.sh" ''
+      source $stdenv/setup
+      mkdir -p $out/bin
+      find  ${lib.getBin slurmCfg.package}/bin -type f -executable | while read EXE
+      do
+        exename="$(basename $EXE)"
+        wrappername="$out/bin/$exename"
+        cat > "$wrappername" <<EOT
+      #!/bin/sh
+      if [ -z "$SLURM_CONF" ]
+      then
+        SLURM_CONF="${slurmCfg.etcSlurm}/slurm.conf" "$EXE" "\$@"
+      else
+        "$EXE" "\$0"
+      fi
+      EOT
+        chmod +x "$wrappername"
+      done
+
+      mkdir -p $out/share
+      ln -s ${lib.getBin slurmCfg.package}/share/man $out/share/man
+    '';
+  };
+
+in
+{
+
+  options = with lib; {
+    flyingcircus.slurm = {
+
+      partitionName = mkOption {
+        type = types.str;
+        default = "all";
+        description = lib.mdDoc ''
+          Name of the default partition which includes the machines defined via the `nodes` option.
+          Don't use `default` as partition name, it will fail!
+        '';
+      };
+
+      mungeKeyFile = mkOption {
+        type = types.str;
+        default = "/var/lib/munge/munge.key";
+      };
+
+      nodes = mkOption {
+        type = types.listOf types.str;
+        default = defaultSlurmNodes;
+        defaultText = lib.mdDoc "all Slurm nodes in the resource group";
+        description = lib.mdDoc ''
+          Names of the nodes that are added to the automatically generated partition.
+          By default, all Slurm nodes in a resource group are part of the partition
+          called `partitionName`.
+        '';
+      };
+
+    };
+
+    flyingcircus.roles = {
+      slurm-node = {
+        enable = mkEnableOption "";
+        supportsContainers = fclib.mkDisableContainerSupport;
+      };
+
+      slurm-controller = {
+        enable = mkEnableOption "";
+        supportsContainers = fclib.mkDisableContainerSupport;
+      };
+    };
+  };
+
+  config = lib.mkMerge [
+
+    (lib.mkIf anyRoleEnabled {
+      environment.etc.slurm.source = slurmCfg.etcSlurm;
+
+      environment.etc."local/slurm/README.md".text =
+        let
+          roleStr =
+            if nodeEnabled && controllerEnabled
+            then "slurm controller and node"
+            else if controllerEnabled
+            then "slurm controller"
+            else "slurm node";
+        in
+        ''
+        # Slurm Workload Manager
+
+        This VM is acting as ${roleStr}.
+        Generated config is at `${slurmCfg.etcSlurm}`.
+
+        The role works without additional config.
+
+        There's an automatically generated partition named `${cfg.partitionName}`.
+
+        The following nodes are members of the `${cfg.partitionName}` partition:
+        `${fclib.docList cfg.nodes}`
+
+        ## NixOS Options
+
+        ${fclib.docOption "flyingcircus.slurm.nodes"}
+
+        ${fclib.docOption "flyingcircus.slurm.partitionName"}
+      '';
+
+      environment.systemPackages = [
+        (pkgs.writeShellScriptBin "slurm-config-dir" "echo ${slurmCfg.etcSlurm}")
+        (pkgs.writeShellScriptBin "slurm-show-config" ''
+          for x in ${slurmCfg.etcSlurm}/*; do
+            echo "''${x}:"
+            cat $x
+          done
+        '')
+      ];
+
+      flyingcircus.passwordlessSudoRules = [
+        {
+          commands = [ "ALL" ];
+          groups = [ "sudo-srv" ];
+          runAs = "slurm";
+        }
+      ];
+
+      services.slurm = {
+        inherit controlMachine;
+        nodeName = [ "${nodeStr} State=UNKNOWN" ];
+        partitionName = [ "${cfg.partitionName} Nodes=${nodeStr} Default=YES MaxTime=INFINITE State=UP" ];
+        extraConfig = ''
+          SelectType = select/cons_res
+          SelectTypeParameters = CR_CPU_Memory
+        '';
+      };
+
+      systemd.services.fc-set-munge-key = {
+        path = [ pkgs.jq ];
+        script = ''
+          umask 0266
+          jq -r \
+            '.[] | select(.service =="slurm-controller-controller") | .password' \
+            /etc/nixos/services.json | sha256sum | head -c64 \
+            > ${cfg.mungeKeyFile}
+
+          chown munge:munge ${cfg.mungeKeyFile}
+          echo "fc-set-munge-key finished"
+        '';
+
+        serviceConfig = {
+          Type = "oneshot";
+        };
+      };
+
+      systemd.services.munged = {
+        after = [ "fc-set-munge-key.service" ];
+        requires = [ "fc-set-munge-key.service" ];
+        serviceConfig = {
+          ExecStartPre = lib.mkForce [
+            "${pkgs.coreutils}/bin/stat ${cfg.mungeKeyFile}"
+          ];
+        };
+      };
+
+      services.munge.password = cfg.mungeKeyFile;
+    })
+
+    (lib.mkIf controllerEnabled {
+
+      flyingcircus.agent.maintenance.slurm-controller = {
+        enter = "fc-slurm -v all-nodes drain-and-down --nothing-to-do-is-ok";
+        leave = "fc-slurm -v all-nodes ready --nothing-to-do-is-ok";
+      };
+
+      services.slurm = {
+        server.enable = true;
+      };
+
+      systemd.services.slurmctld = {
+        serviceConfig = {
+          Restart = "always";
+        };
+      };
+
+    })
+
+    (lib.mkIf (nodeEnabled && !controllerEnabled) {
+      flyingcircus.agent.maintenance.slurm-node = {
+        enter = "fc-slurm -v drain-and-down --nothing-to-do-is-ok";
+      };
+    })
+
+    (lib.mkIf nodeEnabled {
+
+      services.slurm = {
+        client.enable = true;
+      };
+
+      systemd.services.slurmd = {
+        after = [ "munged.service" ];
+        serviceConfig = {
+          Restart = "always";
+        };
+      };
+
+      systemd.services.fc-agent.environment = {
+        SLURM_CONF = "${slurmCfg.etcSlurm}/slurm.conf";
+      };
+
+
+    })
+  ];
+}
---
+++ pkgs/fc/agent/fc/util/slurm.py
@@ -0,0 +1,240 @@
+from typing import NamedTuple
+
+import time
+
+import pyslurm
+
+NODE_API = pyslurm.node()
+
+DOWN_STATES = ("DOWN", "POWERED_DOWN", "DOWN+DRAIN")
+DRAIN_STATES = ("IDLE+DRAIN", "DRAINING", "DOWN+DRAIN")
+
+
+class NodeStateError(Exception):
+    def __init__(self, state):
+        self.state = state
+
+
+class NodeStateTimeout(Exception):
+    def __init__(self, state):
+        self.state = state
+
+
+def get_node_info(node_name):
+    return NODE_API.get_node(node_name)[node_name]
+
+
+def get_all_node_names():
+    return [k for k, v in NODE_API.get().items()]
+
+
+class DrainPreCheckResult(NamedTuple):
+    needs_draining: bool
+
+
+def run_drain_pre_checks(log, node_name, nothing_to_do_is_ok):
+    log = log.bind(node=node_name)
+
+    node_info = get_node_info(node_name)
+    node_state = node_info["state"]
+    if "DRAIN" in node_state:
+        if nothing_to_do_is_ok:
+            log.info(
+                "drain-already-reached",
+                _replace_msg="Node {node} is already in a draining state",
+            )
+            return DrainPreCheckResult(needs_draining=False)
+        else:
+            log.error("drain-state-error", state=node_state)
+            raise NodeStateError(node_state)
+
+    if node_state == "DOWN":
+        if nothing_to_do_is_ok:
+            log.info(
+                "drain-already-down",
+                _replace_msg="Node {node} is already down",
+            )
+            return
+        else:
+            log.error("drain-state-error", state=node_state)
+            raise NodeStateError(node_state)
+
+    return DrainPreCheckResult(needs_draining=True)
+
+
+def drain(
+    log,
+    node_name,
+    timeout: int,
+    reason: str,
+    nothing_to_do_is_ok: bool,
+):
+    log.info("drain-start")
+
+    check_result = run_drain_pre_checks(log, node_name, nothing_to_do_is_ok)
+    if not check_result.needs_draining:
+        return
+
+    state_change_drain = {
+        "node_names": node_name,
+        "node_state": pyslurm.NODE_STATE_DRAIN,
+        "reason": reason,
+    }
+    result = NODE_API.update(state_change_drain)
+    log.debug("node-update-result", result=result)
+
+    for ii in range(timeout):
+        node_state = get_node_info(node_name)["state"]
+        if node_state in ("IDLE+DRAIN", "DOWN+DRAIN"):
+            log.info("drain-finished", result=result)
+            return
+
+        log.debug(
+            "drain-wait", waiting_for=ii, timeout=timeout, state=node_state
+        )
+        time.sleep(1)
+
+    log.error(
+        "drain-timeout",
+        timeout=timeout,
+        _replace=(
+            "Node {node} did not reach IDLE+DRAIN state in time, "
+            "waited for {timeout} seconds)"
+        ),
+    )
+    raise NodeStateTimeout(node_state)
+
+
+def drain_many(
+    log,
+    node_names,
+    timeout: int,
+    reason: str,
+    nothing_to_do_is_ok: bool,
+):
+    log.info("drain-all-start", nodes=node_names)
+
+    waiting_nodes = set()
+
+    for node_name in node_names:
+        check_result = run_drain_pre_checks(log, node_name, nothing_to_do_is_ok)
+        if check_result.needs_draining:
+            waiting_nodes.add(node_name)
+
+    if not waiting_nodes:
+        log.info(
+            "drain-many-nothing-to-do",
+            _replace_msg="OK: All nodes are already in a draining state.",
+        )
+        return
+
+    state_change_drain = {
+        "node_names": ",".join(node_names),
+        "node_state": pyslurm.NODE_STATE_DRAIN,
+        "reason": reason,
+    }
+    result = NODE_API.update(state_change_drain)
+    log.debug("node-update-result", result=result)
+
+    for ii in range(timeout):
+
+        for node_name in set(waiting_nodes):
+            node_state = get_node_info(node_name)["state"]
+            if node_state == "IDLE+DRAIN":
+                log.info(
+                    "drain-node-finished",
+                    node=node_name,
+                    result=result,
+                    time=ii,
+                )
+
+                waiting_nodes.remove(node_name)
+
+                if not waiting_nodes:
+                    log.info("drain-all-finished", result=result, time=ii)
+                    return
+
+            log.debug(
+                "drain-all-wait",
+                waiting_for=ii,
+                timeout=timeout,
+                state=node_state,
+            )
+
+        time.sleep(1)
+
+    log.error(
+        "drain-timeout",
+        timeout=timeout,
+        waiting_nodes=waiting_nodes,
+        _replace=(
+            "Nodes {waiting_nodes} did not reach IDLE+DRAIN state in time, "
+            "waited for {timeout} seconds)"
+        ),
+    )
+    raise NodeStateTimeout(node_state)
+
+
+def down(log, node_name, nothing_to_do_is_ok: bool, reason: str):
+    log = log.bind(node=node_name)
+    log.info("down-start")
+
+    node_info = get_node_info(node_name)
+    node_state = node_info["state"]
+
+    if node_state in DOWN_STATES:
+        if nothing_to_do_is_ok:
+            log.info(
+                "down-already-reached",
+                _replace_msg="Node {node} is already in a DOWN state",
+            )
+            return
+        else:
+            log.error("down-error", state=node_state)
+            raise NodeStateError(node_state)
+
+    state_change_down = {
+        "node_names": node_name,
+        "node_state": pyslurm.NODE_STATE_DOWN,
+        "reason": reason,
+    }
+    result = NODE_API.update(state_change_down)
+    log.debug("node-update-result", result=result)
+    log.info(
+        "down-finished",
+        _replace_msg="Node {node} is now marked as down",
+    )
+
+
+def ready(
+    log,
+    node_name,
+    nothing_to_do_is_ok: bool,
+):
+    log = log.bind(node=node_name)
+    log.info("ready-start")
+
+    node_info = get_node_info(node_name)
+    node_state = node_info["state"]
+
+    if node_state in ("IDLE", "ALLOCATED", "POWERING_UP"):
+        if nothing_to_do_is_ok:
+            log.info(
+                "ready-already-reached",
+                _replace_msg="Node {node} is already in a ready state",
+            )
+            return
+        else:
+            log.error("ready-error", state=node_state)
+            raise NodeStateError(node_state)
+
+    state_change_ready = {
+        "node_names": node_name,
+        "node_state": pyslurm.NODE_RESUME,
+    }
+    result = NODE_API.update(state_change_ready)
+    log.debug("node-update-result", result=result)
+    log.info(
+        "ready-finished",
+        _replace_msg="Node {node} has resumed operations",
+    )
