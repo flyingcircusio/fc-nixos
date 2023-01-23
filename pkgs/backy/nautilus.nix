43c042152e81fd375eb7e317465412c0061dcdce
Christian Theune <ct@flyingcircus.io>
2023-01-23 17:22:21 +0100

    We revamped backy's poetry2nix integration.
    Due to a bug in poetry2nix some package changes (like setuptools-scm)
    became incompatible. To get those fixed we're now pulling in Python
    3.10 and the surrounding ecosystem (poetry, poetry2nix) via our upstream
    22.11 tracking repository.
    
    We also stumbled over the sqlite build: the patch was already applied -
    I'm not sure why we're running into this in this change, but I had to
    remove it to get it building again.

--- pkgs/backy/nautilus.nix
+++ pkgs/backy/nautilus.nix
@@ -3,24 +3,39 @@
   poetry2nix ? pkgs.poetry2nix,
   fetchFromGitHub ? pkgs.fetchFromGitHub,
   lzo ? pkgs.lzo,
+  python310 ? pkgs.python310,
   ...
 }:
-let src = fetchFromGitHub {
-  owner = "flyingcircusio";
-  repo = "backy";
-  # TODO: this points to a branch head that only differs by the Nautilus-specific commits
-  # from what we use for Luminous, but not the latest `main` head as that currently
-  # fails to build in NixOS.
-  rev = "7914012a4e74dfeca597f9dcde237de5ee2f41b0";
-  sha256 = "sha256-EOWqQa//6UhvIIy3LGsFapmK2LMIBIQ90Om4XGOxS2g=";
-};
+let
+# src = ../path/to/checkout;
+  src = fetchFromGitHub {
+    owner = "flyingcircusio";
+    repo = "backy";
+    # TODO: this points to a branch head that only differs by the Nautilus-specific commits
+    # from what we use for Luminous, but not the latest `main` head as that currently
+    # fails to build in NixOS.
+    rev = "7914012a4e74dfeca597f9dcde237de5ee2f41b0";
+    sha256 = "sha256-EOWqQa//6UhvIIy3LGsFapmK2LMIBIQ90Om4XGOxS2g=";
+  };
+
 in poetry2nix.mkPoetryApplication {
     projectDir = src;
     src = src;
     doCheck = true;
+    python = python310;
+    extras = [];
     overrides = poetry2nix.overrides.withDefaults (self: super: {
       python-lzo = super.python-lzo.overrideAttrs (old: {
         buildInputs = [ lzo ];
+      });
+      telnetlib3 = super.telnetlib3.overrideAttrs (old: {
+        buildInputs = [ super.setuptools ];
+      });
+      pytest-flake8 = super.pytest-flake8.overrideAttrs (old: {
+        buildInputs = [ super.setuptools ];
+      });
+      consulate = super.consulate.overrideAttrs (old: {
+        buildInputs = [ super.setuptools ];
       });
     });
 }
