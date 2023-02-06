d2b0e95010a4e520638e81e48790c17aa6f879a7
Oliver Schmidt <os@flyingcircus.io>
2023-02-06 16:13:46 +0100

    backy: 2.5.0 with support for multiple rbd versions
    - pulls in the latest upstream revision of version 2.5.0
    - that version again supports the ceph releases from Jewel to Nautilus
      at once
    - includes switch to poetry packaging

--- pkgs/backy/nautilus.nix
+++ pkgs/backy/nautilus.nix
@@ -1,21 +1,16 @@
 {
-  pkgs ? import <nixpkgs> {},
-  poetry2nix ? pkgs.poetry2nix,
-  fetchFromGitHub ? pkgs.fetchFromGitHub,
-  lzo ? pkgs.lzo,
-  python310 ? pkgs.python310,
-  ...
+  poetry2nix,
+  fetchFromGitHub,
+  lzo,
+  python310
 }:
 let
-# src = ../path/to/checkout;
+  #src = /home/os/backy;
   src = fetchFromGitHub {
     owner = "flyingcircusio";
     repo = "backy";
-    # TODO: this points to a branch head that only differs by the Nautilus-specific commits
-    # from what we use for Luminous, but not the latest `main` head as that currently
-    # fails to build in NixOS.
-    rev = "7914012a4e74dfeca597f9dcde237de5ee2f41b0";
-    sha256 = "sha256-EOWqQa//6UhvIIy3LGsFapmK2LMIBIQ90Om4XGOxS2g=";
+    rev = "93b18b09b3db1cba4a10c6613bfec0ad0d82895a";
+    hash = "sha256-HRndYUW08c8ncxwGdSOZWyXszzJ0JDKBAlFSS+UiQRU=";
   };
 
 in poetry2nix.mkPoetryApplication {
@@ -34,8 +29,26 @@ in poetry2nix.mkPoetryApplication {
       pytest-flake8 = super.pytest-flake8.overrideAttrs (old: {
         buildInputs = [ super.setuptools ];
       });
-      consulate = super.consulate.overrideAttrs (old: {
-        buildInputs = [ super.setuptools ];
+      filelock = super.iniconfig.overrideAttrs (old: {
+        buildInputs = [ super.hatchling super.hatch-vcs ];
+      });
+      iniconfig = super.iniconfig.overrideAttrs (old: {
+        buildInputs = [ super.hatchling super.hatch-vcs ];
+      });
+      humanize = super.humanize.overrideAttrs (old: {
+        buildInputs = [ super.hatchling super.hatch-vcs ];
+      });
+      prettytable = super.prettytable.overrideAttrs (old: {
+        buildInputs = [ super.hatchling super.hatch-vcs ];
+      });
+      # dirty workaround, as the virtualenv-20.17.1 extracted via poetry2nix did not
+      # find its `filelock` dependency
+      virtualenv = python310.pkgs.virtualenv;
+      #virtualenv = super.virtualenv.overrideAttrs (old: {
+      #  buildInputs = [ self.distlib self.filelock self.platformdirs ];
+      #});
+      consulate-fc-nix-test = super.consulate-fc-nix-test.overrideAttrs (old: {
+        buildInputs = [ super.setuptools super.setuptools-scm ];
       });
     });
 }
