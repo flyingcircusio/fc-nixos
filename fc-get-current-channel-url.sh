00e2a8c88e9b66cbfa4f49a8dcb32fd7685b4b5d
Tobias Stenzel <ts@flyingcircus.io>
2023-04-20 15:05:19 +0200

    Add nixos-version file, adapt scripts
    nixos-version contains the (major) version of NixOS which we also
    use for our platform.
    
    This is helpful for release scripts and similar things to avoid
    repeating ourselves and forgetting to change the version everywhere
    on a platform upgrade.
    
    fc-release.sh now uses this file.
    
    fc-get-current-channel-url.sh uses it, too.
    
    `production`, `dev` and `staging` can be specified as the only argument to get
    the final (redirects resolved) URLs for the branches.
    
    Note that the URLs returned for `production` might already be a newer version
    than the one communicated in the changelog as persistent release URL.

--- fc-get-current-channel-url.sh
+++ fc-get-current-channel-url.sh
@@ -2,12 +2,8 @@
 
 set -euo pipefail
 
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 <release> <channel>" >&2
-  exit 2
-fi
+branch=$1
+nixos_version=$(< nixos-version)
+jobset="fc-$nixos_version-$branch"
 
-RELEASE="$1"
-CHANNEL="$2"
-
-curl --silent --head https://hydra.flyingcircus.io/channel/custom/flyingcircus/fc-$RELEASE-$CHANNEL/release/nixexprs.tar.xz | grep "[Ll]ocation" | cut -f 2 -d " "
+curl --silent --head https://hydra.flyingcircus.io/channel/custom/flyingcircus/${jobset}/release/nixexprs.tar.xz | grep "[Ll]ocation" | cut -f 2 -d " "
