flyingcircusio/fc-nixos

commit 8c0cb428ab035be3544ecd408d0560f1237e681b

Update motd with ChangeLog URL and release name if available inside enc
DIR-766

nixos/platform/shell.nix
@@ -63,12 +63,16 @@ in
       )
     );
 
-    users.motd = ''
+    users.motd = let
+      has_release_name = enc.parameters ? release_name;
+    in
+    ''
       Welcome to the Flying Circus!
 
       Status:     https://status.flyingcircus.io/
-      Docs:       https://flyingcircus.io/doc/
-      Release:    ${config.system.nixos.label}
+      Docs:       https://doc.flyingcircus.io/
+      ${ opt (enc.parameters ? release_changelog ) ("ChangeLog:  " + enc.parameters.release_changelog)}
+      Release:    ${ opt has_release_name "${enc.parameters.release_name} (" + config.system.nixos.label + opt has_release_name ")"}
 
     '' +
     (opt (enc ? name && parameters ? location && parameters ? environment)

commit 39d9d0dd0ae3518bac15f8253bda87e7ac91ea2a

Fix motd
`enc.parameters` may not exist, just use parameters defined in the let
above. Fixes regression introduced by 8c0cb428ab035be3544ecd408d0560f1237e681b

DIR-766

nixos/platform/shell.nix
@@ -64,15 +64,15 @@ in
     );
 
     users.motd = let
-      has_release_name = enc.parameters ? release_name;
+      has_release_name = parameters ? release_name;
     in
     ''
       Welcome to the Flying Circus!
 
       Status:     https://status.flyingcircus.io/
       Docs:       https://doc.flyingcircus.io/
-      ${ opt (enc.parameters ? release_changelog ) ("ChangeLog:  " + enc.parameters.release_changelog)}
-      Release:    ${ opt has_release_name "${enc.parameters.release_name} (" + config.system.nixos.label + opt has_release_name ")"}
+      ${ opt (parameters ? release_changelog ) ("ChangeLog:  " + parameters.release_changelog)}
+      Release:    ${ opt has_release_name "${parameters.release_name} (" + config.system.nixos.label + opt has_release_name ")"}
 
     '' +
     (opt (enc ? name && parameters ? location && parameters ? environment)