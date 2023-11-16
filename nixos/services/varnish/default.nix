51278b89bcbcdc30e41ee504bd46e73fb8b435bc
Christian Theune <ct@flyingcircus.io>
2023-11-16 10:42:45 +0100

    Smaller adjustments during code review.

--- nixos/services/varnish/default.nix
+++ nixos/services/varnish/default.nix
@@ -7,16 +7,26 @@
 
   mkHostSelection = hcfg: let
     includefile = if (builtins.isPath hcfg.config) then hcfg.config else (pkgs.writeText "${hcfg.host}.vcl" hcfg.config);
-    # vcl_hash-${hcfg.host} -> has to change with the file's contents
+    # Varnish uses a two-step approach with config names and labels, that we
+    # leverage in this way:
+    # 1. Every vhost received a config file that is written as a VCL config
+    #    in the nix store, so every vhost's config file name changes when
+    #    the config changes. We reflect this change in the config name within
+    #    Varnish so that we can load multiple configs for the same vhost
+    #    at the same time to facilitate graceful switchover.
+    # 2. The label for every vhost stays the same, independent of any changes
+    #    in the config. The label is then pointed to a new (versioned) name
+    #    from step 1 to perform the switch-over.
     name = mkVclName includefile;
-    # has to stay the same when only the file's the content changes, such that a reload only "moves" the label over to the new config
     label = "label-${sanitizeConfigName hcfg.host}";
   in {
+    # This is the VCL snippet that will be embedded into the main VCL config.
     config = ''
       if (${hcfg.condition}) {
         return(vcl(${label}));
       }
     '';
+    # These are the commands to activate new config (both at startup and reload.)
     command = ''
       vcl.load ${name} ${includefile}
       vcl.label ${label} ${name}
@@ -27,6 +37,8 @@
     vcl 4.0;
     import std;
 
+    # An (invalid) backend that will never be used but is needed
+    # to created a syntactically valid config.
     backend default {
       .host = "0.0.0.0";
       .port = "80";
@@ -41,14 +53,20 @@
 
   vhosts = map mkHostSelection (builtins.attrValues cfg.virtualHosts);
   virtualHostSelection = lib.concatStringsSep "else" (map (x: x.config) vhosts);
-  commandsfile = pkgs.writeText "varnishd-commands" (lib.concatStringsSep "\n" ((map (x: x.command) vhosts) ++ (let
-    name = mkVclName mainConfig;
-  in [''
-    vcl.load ${name} ${mainConfig}
-    vcl.use ${name}
-  ''])));
 
-  extraCommandLine = cfg.extraCommandLine + (lib.optionalString (cfg.extraCommandLine != "") " ") + lib.optionalString (cfg.config == null) "-I ${commandsfile}";
+  vhostActivationCommands = lib.concatStringsSep "\n" (map (x: x.command) vhosts);
+  mainActivationCommands = ''
+    vcl.load ${name} ${mkVclName mainConfig}
+    vcl.use ${name}
+  '';
+
+  commandsfile = pkgs.writeText "varnishd-commands" ''
+    ${vhostActivationCommands}
+    ${mainActivationCommands}
+    '';
+
+  # Only activate the NixOS module config if there is no legacy config.
+  commandsFileArg = lib.optionalString (cfg.config == null) "-I ${commandsfile})";
 in {
   options.flyingcircus.services.varnish = {
     enable = mkEnableOption "varnish";
@@ -89,7 +107,7 @@ in {
     services.varnish = {
       enable = true;
       enableConfigCheck = false;
-      inherit extraCommandLine;
+      extraCommandLine = lib.concatStringsSep [ cfg.extraCommandLine commandsFileArg ];
       inherit (cfg) http_address;
       config = if cfg.config == null then ''
         vcl 4.0;
