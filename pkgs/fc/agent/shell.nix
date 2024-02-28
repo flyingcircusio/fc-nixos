1af850c48a9e77c8c3b08f56211b6b2422c4b2c1
Tobias Stenzel <ts@flyingcircus.io>
2024-02-28 19:56:28 +0100

    agent: fix shell.nix for 21.05

--- pkgs/fc/agent/shell.nix
+++ pkgs/fc/agent/shell.nix
@@ -1,6 +1,8 @@
 let
   pkgs = import <nixpkgs> {};
-  fcagent = pkgs.python310Packages.callPackage ./. {};
+  versions = import ../../../versions.nix { inherit pkgs; };
+  nixos-23_05 = import versions.nixpkgs-23_05 {};
+  fcagent = nixos-23_05.python310Packages.callPackage ./. {};
 in
 fcagent.overridePythonAttrs(_: {
   doCheck = true;
