55a74105411591d0b4b671963ad9aca7200fd72d
Tobias Stenzel <ts@flyingcircus.io>
2024-06-04 14:00:31 +0200

    Update nixpkgs & nixos-mailserver
    Pull upstream NixOS changes, security fixes and package updates:
    
    - chromedriver: 125.0.6422.76 -> 125.0.6422.112
    - chromedriver: 125.0.6422.78 -> 125.0.6422.141
    - chromium: 125.0.6422.112 -> 125.0.6422.141
    - chromium: 125.0.6422.76 -> 125.0.6422.112
    - discourse: 3.1.0 -> 3.2.2
    - discourse: build discourseAllPlugins in passthru.tests.
    - discourse: rely on packaging module for version comparison
    - discourse: strip markers from plugin compat spec
    - discourse: update plugins
    - grafana: 10.4.2 -> 10.4.3
    - imagemagick: fix passthru.tests.pkg-config
    - k3s: 1.30.0+k3s1 -> 1.30.1+k3s1
    - linux_5_15: 5.15.159 -> 5.15.160
    - mastodon: 4.2.8 -> 4.2.9
    - mastodon: remove outdated override for openssl 3 support
    - openjdk: 11.0.19+7 -> 11.0.23+9
    - pdns: 4.9.0 -> 4.9.1
    - slurm: 23.11.6.1 -> 23.11.7.1
    
    nixos-mailserver has been rebased to the current upstream/nixos-24.05
    version.

--- flake.nix
+++ flake.nix
@@ -27,8 +27,7 @@
     nixos-mailserver = {
       url = "gitlab:flyingcircus/nixos-mailserver/23.11?host=gitlab.flyingcircus.io";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-22_11.follows = "nixpkgs";
-      inputs.nixpkgs-23_05.follows = "nixpkgs";
+      inputs.nixpkgs-24_05.follows = "nixpkgs";
     };
     devenv = {
       url = "github:cachix/devenv/a1290a186b9420e2c0b21700f300b486ad90dcc9";
