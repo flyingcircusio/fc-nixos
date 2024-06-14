70175f9b5792b1ec5bb499d7d5a7bf0646366db9
Christian Theune <ct@flyingcircus.io>
2024-06-14 11:56:59 +0200

    devhost: clean up integration of hydra_eval vs. explicit urls
    We stay backwards compatible and have enhanced a few minor aspects.
    
    Re PL-132065

--- nixos/roles/devhost/fc-devhost.py
+++ nixos/roles/devhost/fc-devhost.py
@@ -1,6 +1,7 @@
 import argparse
 import datetime
 import fcntl
+import hashlib
 import ipaddress
 import json
 import os
@@ -22,6 +23,8 @@ CONFIG_DIR = Path("/etc/devhost/vm-configs")
 VM_BASE_IMAGE_DIR = Path("/var/lib/devhost/base-images")
 VM_DATA_DIR = Path("/var/lib/devhost/vms")
 LOCKFILE_PATH = "/run/fc-devhost-vm"
+
+MONTH = 60 * 60 * 24 * 30
 
 
 def run(*args, **kwargs):
@@ -98,18 +101,69 @@ class Manager:
         return open(LOCKFILE_PATH, "a+")
 
     def destroy(self, location=None):
+        print("Assuming devhost lock ...")
         fcntl.flock(self.lockfile, fcntl.LOCK_EX)
         # We want do destroy everything existing for a VM.
         # If something in the provisioning failed, there might not be all files.
+        print(f"Removing {self.name} from NixOS config ...")
         if os.path.isfile(self.config_file):
             self.config_file.unlink()
         if os.path.isfile(self.nix_file):
             self.nix_file.unlink()
         shutil.rmtree(self.data_dir, ignore_errors=True)
+        print(f"Deleting {self.name} data ...")
         run("fc-manage", "-v", "-b")
 
-    def provision(self, cpu, memory, image_url, channel_url, aliases, location):
+    def ensure(
+        self,
+        cpu,
+        memory,
+        aliases,
+        location,
+        hydra_eval=None,
+        image_url=None,
+        channel_url=None,
+    ):
+        print("Assuming devhost lock ...")
         fcntl.flock(self.lockfile, fcntl.LOCK_EX)
+
+        if hydra_eval:
+            print("Converting hydra eval to channel and image urls")
+            # Compatibility layer: convert the hydra eval to image_url and
+            # channel_url
+            if image_url or channel_url:
+                raise ValueError(
+                    "Either `hydra_eval` or both of `image_url` and `channel_url` must be given - not both."
+                )
+
+            response = requests.get(
+                f"https://hydra.flyingcircus.io/eval/{hydra_eval}/job/release",
+                headers={"Accept": "application/json"},
+            )
+            response.raise_for_status()
+            build_id = response.json()["id"]
+            channel_url = f"https://hydra.flyingcircus.io/build/{build_id}/download/1/nixexprs.tar.xz"
+            print(f"\tchannel_url={channel_url}")
+            response = requests.get(
+                f"https://hydra.flyingcircus.io/eval/{hydra_eval}/job/images.dev-vm",
+                headers={"Accept": "application/json"},
+            )
+            response.raise_for_status()
+            for id, product in response.json()["buildproducts"].items():
+                if product["subtype"] == "img":
+                    image_url = f"https://hydra.flyingcircus.io/build/{response.json()['id']}/download/{id}"
+                    break
+            else:
+                raise RuntimeError(
+                    f"Could not find URL for base image for hydra eval {hydra_eval}."
+                )
+            print(f"\timage_url={image_url}")
+        del hydra_eval
+
+        if not channel_url:
+            raise ValueError("Missing `channel_url` parameter.")
+        if not image_url:
+            raise ValueError("Missing `image_url` parameter.")
 
         if os.path.isfile(self.config_file):
             self.cfg = json.load(open(self.config_file))
@@ -117,9 +171,10 @@ class Manager:
         self.cfg["cpu"] = cpu
         self.cfg["name"] = self.name
         self.cfg["memory"] = memory
-        # self.cfg["hydra_eval"] = hydra_eval
         self.cfg["aliases"] = aliases
         self.cfg["location"] = location
+        self.cfg["image_url"] = image_url
+        self.cfg["channel_url"] = image_url
 
         if "user" not in self.cfg:
             self.cfg["user"] = os.getlogin()
@@ -161,6 +216,7 @@ class Manager:
             with open(self.config_file, mode="w") as f:
                 f.write(json.dumps(self.cfg))
 
+            # Nixify the alias list
             nix_aliases = " ".join(map(lambda x: f'"{x}"', self.cfg["aliases"]))
             with open(self.nix_file, mode="w") as f:
                 f.write(
@@ -184,25 +240,40 @@ class Manager:
                     )
                 )
 
-            os.makedirs(self.data_dir, exist_ok=True)
-            os.makedirs(VM_BASE_IMAGE_DIR, exist_ok=True)
-
+            self.data_dir.mkdir(exist_ok=True)
+            VM_BASE_IMAGE_DIR.mkdir(exist_ok=True)
             if not os.path.isfile(self.image_file):
-                vm_base_image_path = VM_BASE_IMAGE_DIR / "vm.qcow2"
+                image_url_hash = hashlib.sha256(
+                    image_url.encode("utf-8")
+                ).hexdigest()
+                vm_base_image_path = (
+                    VM_BASE_IMAGE_DIR / f"{image_url_hash}.qcow2"
+                )
                 if not os.path.isfile(vm_base_image_path):
-                    vm_base_image_path_tmp = VM_BASE_IMAGE_DIR / "vm.qcow2.tmp"
-
+                    print(
+                        f"Downloading base image from {image_url} to {vm_base_image_path}"
+                    )
+                    vm_base_image_path_tmp = (
+                        VM_BASE_IMAGE_DIR / f"{image_url_hash}.qcow2.tmp"
+                    )
+                    # Download the base image. We rename the file afterwards
+                    # to ensure that the image is fully there.
                     r = requests.get(image_url)
                     with open(vm_base_image_path_tmp, "wb") as f:
                         f.write(r.content)
                     os.rename(vm_base_image_path_tmp, vm_base_image_path)
+                print("Creating VM image ...")
                 run(
                     "cp",
                     "--reflink=auto",
                     vm_base_image_path,
                     self.image_file_tmp,
                 )
+                # Update cache freshness, avoid this base image being deleted
+                # in the next 3 months.
+                vm_base_image_path.touch()
 
+                print("Preparing VM image for first boot ...")
                 with tempfile.TemporaryDirectory() as image_mount_directory:
                     # the 10 is the number of max. nbd devices provided by the kernel
                     nbd_number = None
@@ -247,6 +318,7 @@ class Manager:
                         run("qemu-nbd", "--disconnect", f"/dev/nbd{nbd_number}")
                 os.rename(self.image_file_tmp, self.image_file)
             else:
+                print("Syncing VM enc data into running VM ...")
                 with tempfile.NamedTemporaryFile(mode="w") as f:
                     f.write(generate_enc_json(self.cfg, channel_url))
                     f.flush()
@@ -263,6 +335,7 @@ class Manager:
 
             fcntl.flock(self.lockfile, fcntl.LOCK_UN)
             # We need to wait for the VM to get online
+            print("Waiting for VM to become pingable ...")
             while True:
                 response = os.system(f"ping -c 1 {self.cfg['srv-ip']}")
                 if response == 0:
@@ -271,207 +344,11 @@ class Manager:
                     time.sleep(0.5)
 
         except Exception as e:
-            # We want the script to end in a state, where other VMs can be started without a problem.
-            # So mainly, if the VM is started for the first time, we just destroy it.
-            # If a VM is new, is determined by the existence of their nix file, as it controls the
-            # associated systemd unit.
-            if not vm_nix_file_existed:
-                self.destroy()
-            raise e
-
-    def ensure(self, cpu, memory, hydra_eval, aliases, location):
-        fcntl.flock(self.lockfile, fcntl.LOCK_EX)
-        # Nixify the alias list
-        response = requests.get(
-            f"https://hydra.flyingcircus.io/eval/{hydra_eval}/job/release",
-            headers={"Accept": "application/json"},
-        )
-        response.raise_for_status()
-        build_id = response.json()["id"]
-        channel_url = f"https://hydra.flyingcircus.io/build/{build_id}/download/1/nixexprs.tar.xz"
-
-        if os.path.isfile(self.config_file):
-            self.cfg = json.load(open(self.config_file))
-
-        self.cfg["cpu"] = cpu
-        self.cfg["name"] = self.name
-        self.cfg["memory"] = memory
-        self.cfg["hydra_eval"] = hydra_eval
-        self.cfg["aliases"] = aliases
-        self.cfg["location"] = location
-
-        if "user" not in self.cfg:
-            self.cfg["user"] = os.getlogin()
-        if "creation-date" not in self.cfg:
-            self.cfg["creation-date"] = datetime.datetime.utcnow().isoformat()
-
-        if "id" not in self.cfg:
-            known_ids = set(vm["id"] for vm in list_all_vm_configs())
-            for candidate in range(MAX_VM_ID):
-                if candidate not in known_ids:
-                    self.cfg["id"] = candidate
-                    break
-            else:
-                raise RuntimeError("Could not find free VM ID.")
-
-        # The MAC address is calculated every time deterministically
-        srv_mac = f"0203{self.cfg['id']:08x}"
-        self.cfg["srv-mac"] = ":".join(
-            srv_mac[i : i + 2] for i in range(0, 12, 2)
-        )
-
-        if "srv-ip" not in self.cfg:
-            known_ips = set(
-                ipaddress.ip_address(vm["srv-ip"])
-                for vm in list_all_vm_configs()
-            )
-            known_ips.add(NETWORK.broadcast_address)
-            known_ips.add(NETWORK.network_address)
-            known_ips.add(NETWORK[1])  # gateway
-            for candidate in NETWORK:
-                if candidate not in known_ips:
-                    self.cfg["srv-ip"] = candidate.exploded
-                    break
-            else:
-                raise RuntimeError("Could not find free SRV IP address.")
-
-        vm_nix_file_existed = os.path.isfile(self.nix_file)
-        try:
-            with open(self.config_file, mode="w") as f:
-                f.write(json.dumps(self.cfg))
-
-            nix_aliases = " ".join(map(lambda x: f'"{x}"', self.cfg["aliases"]))
-            with open(self.nix_file, mode="w") as f:
-                f.write(
-                    textwrap.dedent(
-                        f"""\
-                # DO NOT TOUCH!
-                # Managed by fc-devhost
-                {{ ... }}: {{
-                  flyingcircus.roles.devhost.virtualMachines = {{
-                    "{self.cfg['name']}" = {{
-                      id = {self.cfg['id']};
-                      memory = "{self.cfg['memory']}";
-                      cpu = {self.cfg['cpu']};
-                      srvIp = "{self.cfg['srv-ip']}";
-                      srvMac = "{self.cfg['srv-mac']}";
-                      aliases = [ {nix_aliases} ];
-                    }};
-                  }};
-                }}
-                """
-                    )
-                )
-
-            os.makedirs(self.data_dir, exist_ok=True)
-            os.makedirs(VM_BASE_IMAGE_DIR, exist_ok=True)
-
-            if not os.path.isfile(self.image_file):
-                response = requests.get(
-                    f"https://hydra.flyingcircus.io/eval/{hydra_eval}/job/images.dev-vm",
-                    headers={"Accept": "application/json"},
-                )
-                response.raise_for_status()
-
-                vm_base_image_path = VM_BASE_IMAGE_DIR / f"{hydra_eval}.qcow2"
-                if not os.path.isfile(vm_base_image_path):
-                    vm_base_image_path_tmp = (
-                        VM_BASE_IMAGE_DIR / f"{hydra_eval}.qcow2.tmp"
-                    )
-                    vm_base_image_url: str
-                    for id, product in response.json()["buildproducts"].items():
-                        if product["subtype"] == "img":
-                            vm_base_image_url = f"https://hydra.flyingcircus.io/build/{response.json()['id']}/download/{id}"
-                            break
-                    else:
-                        raise RuntimeError(
-                            "Could not find store path for base image"
-                        )
-                    # Download the base image. We rename the file afterwards
-                    # to ensure that the image is fully there.
-                    r = requests.get(vm_base_image_url)
-                    with open(vm_base_image_path_tmp, "wb") as f:
-                        f.write(r.content)
-                    os.rename(vm_base_image_path_tmp, vm_base_image_path)
-                run(
-                    "cp",
-                    "--reflink=auto",
-                    vm_base_image_path,
-                    self.image_file_tmp,
-                )
-
-                with tempfile.TemporaryDirectory() as image_mount_directory:
-                    # the 10 is the number of max. nbd devices provided by the kernel
-                    nbd_number = None
-                    for i in range(8):
-                        if check_if_nbd_device_is_used(i):
-                            nbd_number = i
-                            break
-                    if nbd_number is None:
-                        raise RuntimeError("There is no unused nbd device.")
-                    try:
-                        run(
-                            "qemu-nbd",
-                            f"--connect=/dev/nbd{nbd_number}",
-                            self.image_file_tmp,
-                        )
-                        while True:
-                            if check_if_nbd_device_is_used(nbd_number):
-                                time.sleep(0.5)
-                                break
-
-                        new_fs_uuid = str(uuid.uuid4())
-                        run(
-                            "xfs_admin",
-                            "-U",
-                            new_fs_uuid,
-                            f"/dev/nbd{nbd_number}p1",
-                        )
-
-                        run(
-                            "mount",
-                            f"/dev/nbd{nbd_number}p1",
-                            image_mount_directory,
-                        )
-
-                        enc_file_path = (
-                            Path(image_mount_directory) / "etc/nixos/enc.json"
-                        )
-                        with open(enc_file_path, mode="w") as f:
-                            f.write(generate_enc_json(self.cfg, channel_url))
-                    finally:
-                        run("umount", image_mount_directory)
-                        run("qemu-nbd", "--disconnect", f"/dev/nbd{nbd_number}")
-                os.rename(self.image_file_tmp, self.image_file)
-            else:
-                with tempfile.NamedTemporaryFile(mode="w") as f:
-                    f.write(generate_enc_json(self.cfg, channel_url))
-                    f.flush()
-                    run(
-                        "rsync",
-                        "-e",
-                        "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/devhost/ssh_bootstrap_key",
-                        "--rsync-path=sudo rsync",
-                        f.name,
-                        f"developer@{self.name}:/etc/nixos/enc.json",
-                    )
-
-            run("fc-manage", "-v", "-b")
-
-            fcntl.flock(self.lockfile, fcntl.LOCK_UN)
-            # We need to wait for the VM to get online
-            while True:
-                response = os.system(f"ping -c 1 {self.cfg['srv-ip']}")
-                if response == 0:
-                    break
-                else:
-                    time.sleep(0.5)
-
-        except Exception as e:
-            # We want the script to end in a state, where other VMs can be started without a problem.
-            # So mainly, if the VM is started for the first time, we just destroy it.
-            # If a VM is new, is determined by the existence of their nix file, as it controls the
-            # associated systemd unit.
+            # We want the script to end in a state, where other VMs can be
+            # started without a problem. So mainly, if the VM is started for
+            # the first time, we just destroy it. If a VM is new, is
+            # determined by the existence of their nix file, as it controls
+            # the associated systemd unit.
             if not vm_nix_file_existed:
                 self.destroy()
             raise e
@@ -498,16 +375,12 @@ class Manager:
 
     def cleanup(self, location=None):
         fcntl.flock(self.lockfile, fcntl.LOCK_EX)
-        vms = list_all_vm_configs()
-        # Clean up old base images
-        os.makedirs(VM_BASE_IMAGE_DIR, exist_ok=True)
-        stored_images = filter(
-            lambda x: x.endswith(".qcow2"), os.listdir(VM_BASE_IMAGE_DIR)
-        )
-        in_use_images = [f"{vm['hydra_eval']}.qcow2" for vm in vms]
-        to_delete_images = set(stored_images) - set(in_use_images)
-        for image in to_delete_images:
-            (VM_BASE_IMAGE_DIR / image).unlink()
+        VM_BASE_IMAGE_DIR.mkdir(exist_ok=True)
+        for stored_image in VM_BASE_IMAGE_DIR.glob("*"):
+            age = time.time() - stored_image.stat().st_mtime
+            if age < 3 * MONTH:
+                continue
+            stored_image.unlink()
 
     def login(self, location=None):
         os.execvp(
@@ -543,38 +416,23 @@ def main():
     p.set_defaults(func="ensure")
     p.add_argument("--cpu", type=int, help="number of cores")
     p.add_argument("--memory", type=int, help="amount of memory")
-    p.add_argument(
-        "--hydra-eval", type=int, help="hydra eval to use for base image"
-    )
-    p.add_argument(
-        "--aliases",
-        type=space_separated_list,
-        default=[],
-        help="aliases for the nginx",
-    )
-    p.add_argument("name", help="name of the VM")
     p.add_argument("--location", help="location the VMs live in")
-
-    # ---------------------------------
-
-    p = sub.add_parser(
-        "provision", help="provision a devhost vm with a provided image"
-    )
-    p.set_defaults(func="provision")
-    p.add_argument("--cpu", type=int, help="number of cores")
-    p.add_argument("--memory", type=int, help="amount of memory")
     p.add_argument("--image-url", type=str, help="url to an image for the vm")
     p.add_argument(
         "--channel-url", type=str, help="url to the nix channel for the vm"
     )
     p.add_argument(
+        "--hydra-eval",
+        type=int,
+        help="hydra eval to use for base image (deprecated, use --image-url and --channel-url)",
+    )
+    p.add_argument(
         "--aliases",
         type=space_separated_list,
         default=[],
         help="aliases for the nginx",
     )
     p.add_argument("name", help="name of the VM")
-    p.add_argument("--location", help="location the VMs live in")
 
     # ---------------------------------
 
@@ -630,7 +488,7 @@ def main():
         a.print_usage()
         sys.exit(1)
 
-    os.makedirs(CONFIG_DIR, exist_ok=True)
+    CONFIG_DIR.mkdir(exist_ok=True)
 
     name = getattr(args, "name", None)
     kwargs = dict(args._get_kwargs())
