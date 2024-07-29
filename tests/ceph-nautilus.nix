86f82da2ffc67fa79223d1ac46567a8a4345a073
Oliver Schmidt <os@flyingcircus.io>
2024-07-29 18:26:32 +0200

    tests.ceph: fix and activate test for successful bootup
    Reactivating the already-implemented, but commented-out test for
    successful daemon activation after reboots of encrypted ceph hosts.
    The test was broken due to stateful modifications of the key material in
    previous parts of the test, moving the subtest in front of these
    modifications resolved it.
    
    PL-132687

--- tests/ceph-nautilus.nix
+++ tests/ceph-nautilus.nix
@@ -436,6 +436,31 @@ in
       host3.succeed('fc-ceph osd create-bluestore --no-encrypt --wal=external /dev/vdc > /dev/kmsg 2>&1')
       assert_clean_cluster(host2, 3, 3, 3, 320)
 
+    with subtest("Verify keystore automount on boot"):
+      host1.execute("systemctl poweroff --force")
+      host1.wait_for_shutdown()
+      host1.start()
+      host1.wait_for_unit("local-fs.target")
+      show(host1, "mount")
+      host1.execute("sleep 5")
+      show(host1, "mount")
+      show(host1, "systemctl status multi-user.target")
+      show(host1, "lsblk")
+      show(host1, "cat /etc/fstab")
+      host1.succeed("${pkgs.util-linux}/bin/findmnt /mnt/keys > /dev/kmsg 2>&1")
+
+    with subtest("Verify all services are up after a reboot"):
+      #host1.sleep(30)
+      show(host1, "systemctl status -l fc-ceph-mon.service")
+      show(host1, "systemctl status -l fc-ceph-mgr.service")
+      show(host1, "systemctl status -l fc-ceph-rgw.service")
+      show(host1, "systemctl status -l fc-ceph-osd@0.service")
+      host1.wait_for_unit("fc-ceph-mon.service")
+      host1.wait_for_unit("fc-ceph-mgr.service")
+      host1.wait_for_unit("fc-ceph-rgw.service")
+      host1.wait_for_unit("fc-ceph-osds-all.service")
+      host1.wait_for_unit("fc-ceph-osd@0.service")
+
     with subtest("`fc-luks keystore test-open` verifies that volumes can be unlocked"):
       host1.succeed('echo -e "newphrase\n" | setsid -w fc-luks keystore test-open "*" > /dev/kmsg 2>&1')
       host1.succeed('echo -e "newphrase\n" | setsid -w fc-luks keystore test-open "*osd-*" > /dev/kmsg 2>&1')
@@ -501,37 +526,6 @@ in
     #  snapfillcheck = host1.execute('sudo -u sensuclient ' + check_command + ' >&2')
     #  print(snapfillcheck[1])
     #  assert snapfillcheck[0] == 2
-
-    with subtest("Verify keystore automount on boot"):
-      host1.execute("systemctl poweroff --force")
-      host1.wait_for_shutdown()
-      host1.start()
-      host1.wait_for_unit("local-fs.target")
-      show(host1, "mount")
-      host1.execute("sleep 5")
-      show(host1, "mount")
-      show(host1, "systemctl status multi-user.target")
-      show(host1, "lsblk")
-      show(host1, "cat /etc/fstab")
-      host1.succeed("${pkgs.util-linux}/bin/findmnt /mnt/keys > /dev/kmsg 2>&1")
-
-    #with subtest("Verify all services are up after a reboot"):
-    #  host1.sleep(30)
-    #  show(host1, "systemctl status -l fc-ceph-mon.service")
-    #  show(host1, "cat /var/log/ceph/ceph-mon.host1.log")
-    #  show(host1, "systemctl status -l fc-ceph-mgr.service")
-    #  show(host1, "cat /var/log/ceph/ceph-mgr.host1.log")
-    #  show(host1, "systemctl status -l fc-ceph-rgw.service")
-    #  show(host1, "systemctl status -l fc-ceph-osd@0.service")
-    #  show(host1, "cat /var/log/ceph/ceph-osd.0.log")
-    #  show(host1, "journalctl -b -u systemd-tmpfiles-setup.service")
-    #  show(host1, "stat /run")
-    #  show(host1, "stat /run/ceph")
-    #  host1.wait_for_unit("fc-ceph-mon.service")
-    #  host1.wait_for_unit("fc-ceph-mgr.service")
-    #  host1.wait_for_unit("fc-ceph-rgw.service")
-    #  host1.wait_for_unit("fc-ceph-osds-all.service")
-    #  host1.wait_for_unit("fc-ceph-osd@0.service")
 
     print("Time spent waiting", time_waiting)
   '';
