789803b9a9d79d225da286765058a2d742af9d74
Molly Miller <mm@flyingcircus.io>
2024-08-05 13:56:47 +0200

    doc: update URLs to web documentation for 24.05.
    For README files generated out of Nix code, interpolate from the
    platform function which automatically constructs the URLs with the
    appropriate platform version slug.
    
    PL-132885

--- nixos/platform/firewall.nix
+++ nixos/platform/firewall.nix
@@ -74,7 +74,7 @@ in
       - nixos-nat-pre: prerouting rules, e.g. port redirects (-t nat)
       - nixos-nat-post: postrouting rules, e.g. masquerading (-t nat)
 
-      See also https://doc.flyingcircus.io/roles/fc-23.11-production/firewall.html
+      See also ${fclib.roleDocUrl "firewall"}
     '';
 
     flyingcircus.services.sensu-client.checks = {
