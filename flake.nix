6b952582e62e7758d894d59e5f986911b14883ef
Tobias Stenzel <ts@flyingcircus.io>
2024-09-04 21:47:44 +0200

    Switch nixpkgs to current nixos-unstable (24.11)

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
   description = "Flying Circus NixOS platform (dev/release tooling)";
 
   inputs = {
-    nixpkgs.url = "github:flyingcircusio/nixpkgs/PL-132971-downgrade-kernel-5.15";
+    nixpkgs.url = "github:flyingcircusio/nixpkgs/nixos-24.11";
     nixos-mailserver = {
       url = "gitlab:flyingcircus/nixos-mailserver/nixos-24.05?host=gitlab.flyingcircus.io";
       inputs.nixpkgs.follows = "nixpkgs";
