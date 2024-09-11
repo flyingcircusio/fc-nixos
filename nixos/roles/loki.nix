936644b6343ee3554e5cb770c26389d8cc383634
Molly Miller <mm@flyingcircus.io>
2024-09-11 16:46:48 +0200

    loki: add options for configuring S3 storage
    Provide a minimal set of options for providing configuration for an S3
    endpoint used for storing log data, and for providing the required
    storage schedule entries in order to use the configured S3 endpoint.
    
    PL-132891

--- nixos/roles/loki.nix
+++ nixos/roles/loki.nix
@@ -3,21 +3,93 @@
 let
   cfg = config.flyingcircus.roles.loki;
   fclib = config.fclib;
+
+  storageScheduleSubmodule = with lib; with types; submodule {
+    options = {
+      startDate = mkOption {
+        type = strMatching "[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}";
+      };
+      backend = mkOption { type = enum [ "filesystem" "s3" ]; };
+      schemaVersion = mkOption { type = ints.positive; default = 13; };
+    };
+  };
+
+  renderStorageSchema = opts: {
+    from = opts.startDate;
+    # always tsdb for index tables
+    store = "tsdb";
+    schema = "v${toString opts.schemaVersion}";
+    # default values in loki 3.1.1, here specified explicitly
+    index.prefix = "";
+    index.path_prefix = "index/";
+    index.period = "24h";
+
+    object_store = opts.backend;
+  };
 in
 {
   options = with lib; {
     flyingcircus.roles.loki = {
       enable = mkEnableOption "Flying Circus Grafana Loki server";
       supportsContainers = fclib.mkEnableContainerSupport;
+
       logRetentionPeriod = mkOption {
         type = types.ints.unsigned;
         default = 30;
         description = "Global retention period for log data in days. Setting to zero disables automatic log expiry";
       };
+
+      s3 = mkOption {
+        description = "Configure log storage in S3-compatible object store";
+        default = {};
+        type = types.submodule {
+          options = {
+            enable = mkEnableOption "store log data in S3";
+            endpoint = mkOption {
+              description = "HTTP(S) endpoint of S3 storage server";
+              type = types.str;
+              default = "http://rgw.local:7840";
+            };
+            bucketName = mkOption {
+              description = "S3 bucket name";
+              type = types.str;
+            };
+            credentialFile = mkOption {
+              description = "Path to file containing credentials used for authenticating to the S3 server (must be readable by the loki user)";
+              type = types.path;
+              default = "/etc/local/loki/s3.cfg";
+            };
+          };
+        };
+      };
+
+      storageSchedule = mkOption {
+        description = "Log storage schedule configuration";
+        default = {};
+        type = with types; submodule {
+          options = {
+            default = mkOption {
+              visible = false;
+              type = listOf storageScheduleSubmodule;
+              default = [{ startDate = "2024-09-10"; backend = "filesystem"; }];
+            };
+            extra = mkOption {
+              description = "Additional entries to add to the log storage schedule";
+              type = listOf storageScheduleSubmodule;
+              default = [];
+              defaultText = "[]";
+            };
+          };
+        };
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
+    environment.etc."local/loki/README.txt".text = ''
+      This is a stub README for the Loki role.
+    '';
+
     services.loki = {
       enable = true;
       configuration = {
@@ -25,16 +97,8 @@ in
 
         auth_enabled = false;
 
-        schema_config.configs = [{
-          from = "2024-09-10";
-          store = "tsdb";
-          object_store = "filesystem";
-          schema = "v13";
-          # default values in loki 3.1.1, here specified explicitly
-          index.prefix = "";
-          index.path_prefix = "index/";
-          index.period = "24h";
-        }];
+        schema_config.configs = map renderStorageSchema
+          (cfg.storageSchedule.default ++ cfg.storageSchedule.extra);
 
         storage_config = {
           # index file management
@@ -44,6 +108,13 @@ in
           };
           # log data configuration
           filesystem.directory = "/var/lib/loki/chunk-store";
+        } // lib.optionalAttrs (cfg.s3.enable) {
+          s3 = {
+            # authentication configured separately
+            endpoint = cfg.s3.endpoint;
+            bucketNames = cfg.s3.bucketNames;
+            s3forcepathstyle = true;
+          };
         };
 
         compactor = {
@@ -53,7 +124,7 @@ in
         };
 
         limits_config = {
-          retention_period = (builtins.toString cfg.logRetentionPeriod) + "d";
+          retention_period = (toString cfg.logRetentionPeriod) + "d";
         };
 
         # configuration stubs for multi-process management plane
@@ -62,6 +133,10 @@ in
           ring.kvstore.store = "inmemory";
         };
       };
+    };
+
+    systemd.services.loki.serviceConfig = lib.mkIf (cfg.s3.enable) {
+      Environment = "AWS_SHARED_CREDENTIALS_FILE=${cfg.s3.credentialFile}";
     };
 
     flyingcircus.services.nginx = {
