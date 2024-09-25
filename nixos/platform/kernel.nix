cae23b3782665fd4a1a0948480685d6b599ffccb
Tobias Stenzel <ts@flyingcircus.io>
2024-09-25 14:43:44 +0200

    platform/kernel: tabs -> spaces

--- nixos/platform/kernel.nix
+++ nixos/platform/kernel.nix
@@ -1,35 +1,35 @@
 { lib, pkgs, config, ...}:
 
 let
-	defaultUseVerificationKernel =
-		if
-			(config.flyingcircus.enc.parameters.location == "dev") ||
-			(config.flyingcircus.enc.parameters.location == "whq") ||
-			(config.flyingcircus.enc.parameters.production  == false)
-		then true
-		else false;
+  defaultUseVerificationKernel =
+    if
+      (config.flyingcircus.enc.parameters.location == "dev") ||
+      (config.flyingcircus.enc.parameters.location == "whq") ||
+      (config.flyingcircus.enc.parameters.production  == false)
+    then true
+    else false;
 in {
-	options = {
-		flyingcircus.kernelOptions = lib.mkOption {
-		  default = null;
-		  type = lib.types.nullOr lib.types.str;
-		  description = "Additional options for the kernel configuration";
-		};
-		flyingcircus.useVerificationKernel = lib.mkOption {
-			default = defaultUseVerificationKernel;
-			type = lib.types.bool;
-			description = "Participate in using an evaluation kernel.";
-		};
-	};
+  options = {
+    flyingcircus.kernelOptions = lib.mkOption {
+      default = null;
+      type = lib.types.nullOr lib.types.str;
+      description = "Additional options for the kernel configuration";
+    };
+    flyingcircus.useVerificationKernel = lib.mkOption {
+      default = defaultUseVerificationKernel;
+      type = lib.types.bool;
+      description = "Participate in using an evaluation kernel.";
+    };
+  };
 
-	# This is a lift-and-shift from Gentoo and can be modularized and
-	# structured when needed.
+  # This is a lift-and-shift from Gentoo and can be modularized and
+  # structured when needed.
 
-	config = {
+  config = {
 
       boot.kernelPackages = if config.flyingcircus.useVerificationKernel
-      	then pkgs.linuxPackagesFor pkgs.linuxKernelVerify
-      	else pkgs.linuxKernel.packages.linux_5_15;
+        then pkgs.linuxPackagesFor pkgs.linuxKernelVerify
+        else pkgs.linuxKernel.packages.linux_5_15;
 
       # Use this spelling if you need to try out custom kernels, try out patches
       # or otherwise deviate from our nixpkgs upstream.
@@ -56,31 +56,31 @@ in {
       #   }));
 
 
-		flyingcircus.kernelOptions =
-			''
-			ASYNC_TX_DMA y
-			CPU_FREQ_STAT y
-			BLK_DEV_MD y
-			MQ_IOSCHED_DEADLINE y
-			IPMI_PANIC_EVENT y
-			IPMI_PANIC_STRING y
-			LATENCYTOP y
-			NET_IPGRE_BROADCAST y
-			SCHEDSTATS y
-			SCSI_DH y
-			VLAN_8021Q_GVRP y
-			XFS_POSIX_ACL y
-			XFS_QUOTA y
-			WARN_ALL_UNSEEDED_RANDOM y
-			'' + (if !config.flyingcircus.useVerificationKernel
-				then ''
-				RANDOM_TRUST_CPU y
-				'' else "");
+    flyingcircus.kernelOptions =
+      ''
+      ASYNC_TX_DMA y
+      CPU_FREQ_STAT y
+      BLK_DEV_MD y
+      MQ_IOSCHED_DEADLINE y
+      IPMI_PANIC_EVENT y
+      IPMI_PANIC_STRING y
+      LATENCYTOP y
+      NET_IPGRE_BROADCAST y
+      SCHEDSTATS y
+      SCSI_DH y
+      VLAN_8021Q_GVRP y
+      XFS_POSIX_ACL y
+      XFS_QUOTA y
+      WARN_ALL_UNSEEDED_RANDOM y
+      '' + (if !config.flyingcircus.useVerificationKernel
+        then ''
+        RANDOM_TRUST_CPU y
+        '' else "");
 
-		boot.kernelPatches = lib.mkIf ( config.flyingcircus.kernelOptions != null ) [ {
-			name = "fcio-kernel-options";
-			patch = null;
-			extraConfig = config.flyingcircus.kernelOptions;
-		}];
-	};
+    boot.kernelPatches = lib.mkIf ( config.flyingcircus.kernelOptions != null ) [ {
+      name = "fcio-kernel-options";
+      patch = null;
+      extraConfig = config.flyingcircus.kernelOptions;
+    }];
+  };
 }
