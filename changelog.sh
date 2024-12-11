462ee096b37e5fd703b9aa76ffa14327ecfb2d3c
Johanna Bahl <jb@flyingcircus.io>
2024-12-11 14:45:20 +0100

    fix changelog.sh

--- changelog.sh
+++ changelog.sh
@@ -2,8 +2,20 @@
 
 set -e
 
-base="./changelog.d"
+if [ -z "$EDITOR" ]; then
+  echo "Error: \$EDITOR is not set or empty"
+  exit 1
+fi
 
-new_item="${base}/$(date '+%Y%m%d_%H%M%S')_$(git rev-parse --abbrev-ref HEAD)_scriv.md"
-cp "${base}/new_fragment.md.j2" ${new_item}
-$EDITOR $new_item
+base="./changelog.d"
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+new_item="${base}/$(date '+%Y%m%d_%H%M%S')_${branch/\//-}_scriv.md"
+template="${base}/new_fragment.md.j2"
+
+cp "$template" "$new_item"
+
+if ! $EDITOR "$new_item" || diff -q "$template" "$new_item" > /dev/null; then
+  echo "Editor failed or content not changed. Deleting fragment..."
+  rm "${new_item}"
+fi
