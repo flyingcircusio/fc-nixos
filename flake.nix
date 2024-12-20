a3e51a0869d9101a8b82a9a56af9fb6d4d0fd97d
Oliver Schmidt <os@flyingcircus.io>
2024-12-20 12:49:40 +0100

    nixpkgs: pull in our scripted network code changes

--- flake.nix
+++ flake.nix
@@ -23,8 +23,7 @@
   description = "Flying Circus NixOS platform (dev/release tooling)";
 
   inputs = {
-    #nixpkgs.url = "github:flyingcircusio/nixpkgs/nixos-24.11";
-    nixpkgs.url = "github:flyingcircusio/nixpkgs/PL-132122-scripted-network-interfaces";
+    nixpkgs.url = "github:flyingcircusio/nixpkgs/nixos-24.11";
     nixos-mailserver = {
       url = "gitlab:flyingcircus/nixos-mailserver/nixos-24.05?host=gitlab.flyingcircus.io";
       inputs.nixpkgs.follows = "nixpkgs";
