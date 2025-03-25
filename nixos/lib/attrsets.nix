75ca612d49f4156a385e305265ad35524f21658b
Oliver Schmidt <os@flyingcircus.io>
2025-03-25 11:26:01 +0100

    services/postgresql: only consider extensions that don't `throw`
    Some postgres extensions were returning a `throw` when being recursively
    evaluated, e.g. to signal a package removal. This broke the whole
    service build, regardless of whether the extension was used.
    We now do not consider extension names of such extensions anymore.

--- nixos/lib/attrsets.nix
+++ nixos/lib/attrsets.nix
@@ -2,7 +2,7 @@
 
 with lib;
 
-rec {
+{
   # Returns a list of attribute names that appear in more than one attrset.
   # Useful for checking if merging the attrsets would overwrite a previous value.
   #
@@ -10,4 +10,12 @@ rec {
   # => [ "a" ]
   duplicateAttrNames =
     listOfAttrs: attrNames (filterAttrs (n: a: a > 1) (foldAttrs (n: acc: acc + 1) 0 listOfAttrs));
+
+  # Applies `builtins.tryEval` to an attrset of derivations, and returns an
+  # attrset containing only the successful ones.
+  filterSuccessfulEval =
+    attrs:
+    builtins.mapAttrs (_: val: val.value) (
+      lib.filterAttrs (_: val: val.success) (builtins.mapAttrs (_: builtins.tryEval) attrs)
+    );
 }
