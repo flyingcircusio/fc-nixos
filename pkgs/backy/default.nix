ec3cfbf6af7f55a94f3a8be40128ca0da63b4a0f
Oliver Schmidt <os@flyingcircus.io>
2025-04-29 15:54:00 +0200

    backy: package agains python312
    currently targets a PR branch. Necessary due to broken eval with
    python310.
    
    PL-133651

--- pkgs/backy/default.nix
+++ pkgs/backy/default.nix
@@ -1,19 +1,25 @@
 {
   fetchFromGitHub,
-  fetchPypi,
+  lib,
+  stdenv,
   poetry2nix,
   lzo,
-  python310,
+  python312,
   mkShellNoCC,
   poetry,
   runCommand,
+  libiconv,
+  darwin,
+  rustPlatform,
+
 }@inputs:
 let
   src = fetchFromGitHub {
     owner = "flyingcircusio";
     repo = "backy";
-    rev = "2.5.2";
-    hash = "sha256-Tp6mQ9a/PBocw9unGexLvz55nMX+mnUUiUC6ZCCZ+8w=";
+    # FIXME: currently targets a PR branch PL-133651
+    rev = "9fa1a828495e14d3eb65000935022aef8083db9a";
+    hash = "sha256-ssU4sUtD8SliP/ZcqGwXkSZ0xPpSdiCx3R3rAROwb3k=";
   };
 
   lib = import "${src}/lib.nix" inputs;
