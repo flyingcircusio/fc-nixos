9cc54462a1ea382199db5381367019be87483119
Leona Maroni <lm@flyingcircus.io>
2025-04-30 17:02:34 +0200

    fc.check-postfix: regenerate Cargo.lock for compatability with fetchCargoVendor
    PL-133559

--- pkgs/fc/check-postfix/default.nix
+++ pkgs/fc/check-postfix/default.nix
@@ -11,16 +11,13 @@ rustPlatform.buildRustPackage rec {
     src = cleanSource ./.;
   };
 
-  # FIXME: need to migrate to new cargo fetcher, but currently generates
-  # a key error during locking.
-  #useFetchCargoVendor = true;
-  cargoHash = "sha256-iNzwHchOFzMoWE3v2I6FiGmDP8ar36dNGJ8H44qQCP8=";
+  cargoHash = "sha256-DVrPLXPU9nkuMziwkAtxvmwikaS15EOXfYnv4MzJ67A=";
 
   meta = {
     description = ''
       Nagios/Sensu check to determine the number of mails in the queue
     '';
     license = licenses.zpl21;
-    maintainer = with maintainers; [ ckauhaus ];
+    maintainer = with maintainers; [ ];
   };
 }
