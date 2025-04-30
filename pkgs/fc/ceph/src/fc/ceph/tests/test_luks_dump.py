flyingcircusio/fc-nixos

commit 87905aa5cbcf160838658eaf1e72d676a61cf4cb

fc-luks: `test-open` subcommand for testing unlock
For streamlining the regular task of verifying whether (all) volumes of
a host can still be opened with the admin key and device key, I
implemented the `fc-luks keystore test-open` subcommand.

As this relies on the same volume discovery logic as the `reencrypt`
command, I refactored and reused that discoverly logic out into a
separate method.

Also includes a NixOS integration test for this, but no unit tests.

pkgs/fc/ceph/src/fc/ceph/luks/manage.py
@@ -5,9 +5,11 @@ import os
 import secrets
 import shutil
 from pathlib import Path
+from subprocess import CalledProcessError
 from typing import NamedTuple, Optional
 
 from fc.ceph.luks import KEYSTORE  # singleton
+from fc.ceph.luks import Cryptsetup
 from fc.ceph.lvm import XFSVolume
 from fc.ceph.util import console, run
 
@@ -17,21 +19,59 @@ class LuksDevice(NamedTuple):
     name: str  # the LUKS name of the device
     # required for external header discovery, which we only utilise for backy
     mountpoint: Optional[str]
+    header: Optional[str] = None
 
+    @classmethod
+    def lsblk_to_cryptdevices(cls, lsblk_blockdevs: list) -> list["LuksDevice"]:
+        """parses the output of lsblk -Js -o NAME,PATH,TYPE,MOUNTPOINT"""
+        return [
+            cls(
+                base_blockdev=dev["children"][0]["path"],
+                name=dev["name"],
+                mountpoint=dev["mountpoint"],
+            )
+            for dev in lsblk_blockdevs
+            if dev["type"] == "crypt"
+        ]
 
-# TODO: better typing signature
-# Todo: should this be a static class method instead?
-def lsblk_to_cryptdevices(lsblk_blockdevs: list) -> list:
-    """parses the output of lsblk -Js -o NAME,PATH,TYPE,MOUNTPOINT"""
-    return [
-        LuksDevice(
-            base_blockdev=dev["children"][0]["path"],
-            name=dev["name"],
-            mountpoint=dev["mountpoint"],
+    @classmethod
+    def filter_cryptvolumes(
+        cls, name_glob: str, header: Optional[str]
+    ) -> list["LuksDevice"]:
+        """Retrieves visible crypt volumes via `lsblk`, filters their name to
+        match `name_glob`.
+
+        Optionally takes a path to an external `header` file, otherwise does
+        auto-discovery based on looking for a corresponding header file named
+        <mountpoint>.luks and passes an Optional[str].
+        """
+        candidates = cls.lsblk_to_cryptdevices(
+            run.json.lsblk("-s", "-o", "NAME,PATH,TYPE,MOUNTPOINT")
         )
-        for dev in lsblk_blockdevs
-        if dev["type"] == "crypt"
-    ]
+
+        matching_devs = []
+        for candidate in candidates:
+            if not fnmatch.fnmatch(candidate.name, name_glob):
+                continue
+
+            # adjust headers with autodetected heuristics
+            if (
+                (not header)
+                and (mp := candidate.mountpoint)
+                and os.path.exists(headerfile := f"{mp}.luks")
+            ):
+                matching_devs.append(candidate._replace(header=headerfile))
+            else:
+                matching_devs.append(candidate._replace(header=header))
+
+        if header and (match_count := len(matching_devs)) > 1:
+            raise ValueError(
+                f"Got {match_count} matching devices for glob '{name_glob}'.\n"
+                "When specifying an external header file, the target device "
+                "needs to be a single specific match."
+            )
+
+        return matching_devs
 
 
 class LUKSKeyStoreManager(object):
@@ -112,35 +152,9 @@ class LUKSKeyStoreManager(object):
         else:
             raise ValueError(f"slot={slot}")
 
-        candidates = lsblk_to_cryptdevices(
-            run.json.lsblk("-s", "-o", "NAME,PATH,TYPE,MOUNTPOINT")
-        )
-        matching_devs = [
-            candidate
-            for candidate in candidates
-            if fnmatch.fnmatch(candidate.name, name_glob)
-        ]
-        if header and (match_count := len(matching_devs)) > 1:
-            raise ValueError(
-                f"Got {match_count} matching devices for glob '{name_glob}'.\n"
-                "When specifying an external header file, the target device "
-                "needs to be a single specific match."
-            )
-        for dev in matching_devs:
-            dev_header = header
-            console.print(f"Replacing key for {dev.name}")
-
-            if (
-                (not dev_header)
-                and (mp := dev.mountpoint)
-                and os.path.exists(headerfile := f"{mp}.luks")
-            ):
-                dev_header = headerfile
-            self._do_rekey(
-                slot=slot,
-                device=dev.base_blockdev,
-                header=dev_header,
-            )
+        for dev in LuksDevice.filter_cryptvolumes(name_glob, header=header):
+            console.print(f"Rekeying {dev.name}")
+            self._do_rekey(slot, device=dev.base_blockdev, header=dev.header)
 
         console.print("Key updated.", style="bold green")
 
@@ -178,6 +192,73 @@ class LUKSKeyStoreManager(object):
             new_key_file,
             input=add_input,
         )
+
+    def test_open(self, name_glob: str, header: Optional[str]) -> int:
+        # Ensure to request the admin key early on.
+        self._KEYSTORE.admin_key_for_input()
+
+        devices = LuksDevice.filter_cryptvolumes(name_glob, header=header)
+        if not devices:
+            console.print(
+                f"Warning: The glob `{name_glob}` matches no volume.",
+                style="yellow",
+            )
+            return 1
+
+        failing_devices = []
+        for dev in devices:
+            console.print(f"Test opening {dev.name}")
+            if not self._do_test_open(dev.base_blockdev, header=dev.header):
+                failing_devices.append(dev)
+
+        if failing_devices:
+            console.print(
+                "The following devices failed to open:\n"
+                + (
+                    "\n".join(
+                        (
+                            f"{dev.base_blockdev} ({dev.name})"
+                            for dev in failing_devices
+                        )
+                    )
+                ),
+                style="red",
+            )
+            return 2
+
+        return 0
+
+    def _do_test_open(self, device: str, header: Optional[str]) -> bool:
+        header_arg = [f"--header={header}"] if header else []
+        success = True
+
+        # test unlocking both with local key file as well as with admin key
+        try:
+            test_admin = Cryptsetup.cryptsetup(
+                "open",
+                "--test-passphrase",
+                device,
+                input=self._KEYSTORE.admin_key_for_input(),
+            )
+        except CalledProcessError:
+            console.print(
+                f"Failed to open {device} with admin passphrase.", style="red"
+            )
+            success = False
+        try:
+            test_local = Cryptsetup.cryptsetup(
+                "open",
+                "--test-passphrase",
+                f"--key-file={self._KEYSTORE.local_key_path()}",
+                device,
+            )
+        except CalledProcessError:
+            console.print(
+                f"Failed to open {device} with local key file.", style="red"
+            )
+            success = False
+
+        return success
 
         if header:
             self._KEYSTORE.backup_external_header(Path(header))

pkgs/fc/ceph/src/fc/ceph/main.py
@@ -452,7 +452,7 @@ def luks(args=sys.argv[1:]):
     parser_rekey = keystore_sub.add_parser("rekey", help="Rekey volumes.")
     parser_rekey.add_argument(
         "name_glob",
-        help="Names of LUKS volumes to update (globbing allowed), e.g. '*osd-*', 'backy'. Mutually exclusive with `--device`.",
+        help="Names of LUKS volumes to update (globbing allowed), e.g. '*osd-*', 'backy'.",
     )
     parser_rekey.add_argument(
         "--header",
@@ -466,6 +466,21 @@ def luks(args=sys.argv[1:]):
         default="local",
     )
     parser_rekey.set_defaults(action="rekey")
+
+    parser_test = keystore_sub.add_parser(
+        "test-open",
+        help="Test that encrypted volumes can be successfully unlocked.",
+    )
+    parser_test.add_argument(
+        "name_glob",
+        help="Names of LUKS volumes to check (globbing allowed), e.g. '*osd-*', 'backy'.",
+    )
+    parser_test.add_argument(
+        "--header",
+        help="When using an external LUKS header file, provide a path to it here."
+        "\nDefaults to autodetecting and using a file called ${mountpoint}.luks",
+    )
+    parser_test.set_defaults(action="test_open")
 
     parser_fingerprint = keystore_sub.add_parser(
         "fingerprint", help="Compute a fingerprint of an admin passphrase."

pkgs/fc/ceph/src/fc/ceph/tests/test_luks.py
@@ -327,36 +327,39 @@ def test_lsblk_to_cryptdevices():
     from fc.ceph.luks import manage
 
     assert set(
-        manage.lsblk_to_cryptdevices(json.loads(LSBLK_PATTY_JSON))
+        manage.LuksDevice.lsblk_to_cryptdevices(json.loads(LSBLK_PATTY_JSON))
     ) == set(
         (
             manage.LuksDevice(
                 base_blockdev="/dev/mapper/vgsys-ceph--mon--crypted",
                 name="ceph-mon",
                 mountpoint="/srv/ceph/mon/ceph-patty",
+                header=None,
             ),
             manage.LuksDevice(
                 base_blockdev="/dev/sdb1",
                 name="backy",
                 mountpoint="/srv/backy",
+                header=None,
             ),
             manage.LuksDevice(
                 base_blockdev="/dev/mapper/vgsys-ceph--mgr--crypted",
                 name="ceph-mgr",
                 mountpoint="/srv/ceph/mgr/ceph-patty",
+                header=None,
             ),
         )
     )
-    assert manage.lsblk_to_cryptdevices([]) == []
+    assert manage.LuksDevice.lsblk_to_cryptdevices([]) == []
     # disks, but no crypt and no children
     assert (
-        manage.lsblk_to_cryptdevices(
+        manage.LuksDevice.lsblk_to_cryptdevices(
             [{"name": "sdb", "path": "/dev/sda", "type": "disk"}]
         )
         == []
     )
     # disks with mountpoint = null
-    assert manage.lsblk_to_cryptdevices(
+    assert manage.LuksDevice.lsblk_to_cryptdevices(
         [
             {
                 "name": "ceph-osd3-block",

tests/ceph-nautilus.nix
@@ -397,6 +397,8 @@ in
       show(host1, "vgs")
       assert_clean_cluster(host2, 3, 3, 3, 320)
 
+    # TODO: this is non-ceph-specific and could be moved to a separate test.
+    # But we need some encrypted volumes to work with.
     with subtest("Destroy and re-create the keystore, rekey the OSD"):
       host1.succeed("fc-luks keystore destroy --no-overwrite > /dev/kmsg 2>&1")
       host1.succeed("fc-luks keystore create /dev/vde > /dev/kmsg 2>&1")
@@ -434,6 +436,19 @@ in
       host3.succeed('fc-ceph osd create-bluestore --no-encrypt --wal=external /dev/vdc > /dev/kmsg 2>&1')
       assert_clean_cluster(host2, 3, 3, 3, 320)
 
+    with subtest("`fc-luks keystore test-open` verifies that volumes can be unlocked"):
+      host1.succeed('echo -e "newphrase\n" | setsid -w fc-luks keystore test-open "*" > /dev/kmsg 2>&1')
+      host1.succeed('echo -e "newphrase\n" | setsid -w fc-luks keystore test-open "*osd-*" > /dev/kmsg 2>&1')
+      # no volume matched
+      host1.fail('echo -e "newphrase\n" | setsid -w fc-luks keystore test-open "asdfhjkl" > /dev/kmsg 2>&1')
+      # wrong admin passphrase (will also modify the fingerprint)
+      host1.fail('echo -e "wrongphrase\ny\n" | setsid -w fc-luks keystore test-open "*" > /dev/kmsg 2>&1')
+      # wrong local keyfile (will correct admin keyphrase fingerprint again)
+      host1.execute("mv /mnt/keys/host1.key{,.bak}; echo garbage > /mnt/keys/host1.key")
+      host1.fail('echo -e "newphrase\ny\n" | setsid -w fc-luks keystore test-open "*" > /dev/kmsg 2>&1')
+      host1.execute("mv /mnt/keys/host1.key{.bak,}")
+
+    # FIXME: deleting this might affect following tests, especially the reboot test
     with subtest("The check discovers non-conforming host key conditions"):
       host1.execute('chmod o+rw /mnt/keys/*')
       host1.fail("${check_key_file_cmd} > /dev/kmsg 2>&1")

commit b97e9aa67e16b07ab0f24a4ee7c7d71c4433a163

fc-luks: fix external header backup
Bug was introduced by 87905aa5cbcf160838658eaf1e72d676a61cf4cb in #1087, which was merged on 2024-08-22.
However, all backup server encryptions happened before that and the first rekeying is scheduled for 2025-06-01, so no servers are affected.
All backup servers were also manually checked.

pkgs/fc/ceph/src/fc/ceph/backup.py
@@ -63,7 +63,7 @@ class BackyVolume:
                         os.remove(ext_header)
                         break
                     case "n" | "":
-                        breakpoint
+                        break
                     case _:
                         console.print("invalid choice, retry.")
 

pkgs/fc/ceph/src/fc/ceph/luks/checks.py
@@ -1,11 +1,15 @@
-from typing import Iterator
+import filecmp
+from pathlib import Path
+from typing import Iterator, Optional
+
+from fc.ceph.luks import KEYSTORE
 
 # In case these checks ever break: More recent cryptsetup versions also support
 # JSON-formatted output via `cryptsetup luksDump --dump-json-metadata <dev>`.
 # It most likely makes sense parsing that data instead once available.
 
 
-def check_cipher(lines: str) -> Iterator[str]:
+def check_cipher(lines: list[str], **_) -> Iterator[str]:
     checked = False  # plausibility check: did we get any cipher info?
     for line in lines:
         line = line.strip()
@@ -20,7 +24,7 @@ def check_cipher(lines: str) -> Iterator[str]:
         yield "Unable to check cipher correctness, no `Cipher:` found in dump"
 
 
-def _extract_keyslot_numbers(lines: str):
+def _extract_keyslot_numbers(lines: list[str]):
     known_keyslots: set[int] = set()
     lines_iter = iter(lines)
     for line in lines_iter:
@@ -44,13 +48,13 @@ def _extract_keyslot_numbers(lines: str):
     return known_keyslots
 
 
-def check_key_slots_exactly_1_and_0(lines: str) -> Iterator[str]:
+def check_key_slots_exactly_1_and_0(lines: list[str], **_) -> Iterator[str]:
     keyslots = _extract_keyslot_numbers(lines)
     if set([0, 1]) != keyslots:
         yield f"keyslots: unexpected configuration ({keyslots})"
 
 
-def check_512_bit_keys(lines: str) -> Iterator[str]:
+def check_512_bit_keys(lines: list[str], **_) -> Iterator[str]:
     checked = False
     for line in lines:
         line = line.strip()
@@ -65,7 +69,7 @@ def check_512_bit_keys(lines: str) -> Iterator[str]:
         yield "Unable to check key size correctness, no `Key:` found in dump"
 
 
-def check_pbkdf_is_argon2id(lines: str) -> Iterator[str]:
+def check_pbkdf_is_argon2id(lines: list[str], **_) -> Iterator[str]:
     checked = False
     for line in lines:
         line = line.strip()
@@ -80,10 +84,25 @@ def check_pbkdf_is_argon2id(lines: str) -> Iterator[str]:
         yield "Unable to check PBKDF correctness, no `PBKDF:` found in dump"
 
 
+def check_header_backup_valid(
+    lines: list[str], header: Optional[str]
+) -> Iterator[str]:
+    if header is None:
+        return
+    backup = KEYSTORE.local_key_dir / Path(header).name
+    try:
+        filecmp.clear_cache()
+        if not filecmp.cmp(header, backup, shallow=False):
+            yield "Header backup differs from local header"
+    except IOError:
+        yield "Error checking header backup"
+
+
 # All these checks work on a list of lines output by `cryptsetup luksDump`
 all_checks = [
     check_cipher,
     check_key_slots_exactly_1_and_0,
     check_512_bit_keys,
     check_pbkdf_is_argon2id,
+    check_header_backup_valid,
 ]

pkgs/fc/ceph/src/fc/ceph/luks/manage.py
@@ -199,6 +199,9 @@ class LUKSKeyStoreManager(object):
             input=add_input,
         )
 
+        if header:
+            self._KEYSTORE.backup_external_header(Path(header))
+
     @staticmethod
     def check_luks(name_glob: str, header: Optional[str]) -> int:
         devices = LuksDevice.filter_cryptvolumes(name_glob, header=header)
@@ -225,7 +228,7 @@ class LUKSKeyStoreManager(object):
             dump_lines = luks_dump.decode("utf-8").splitlines()
             for check in all_checks:
                 check_ok = True
-                for error in check(dump_lines):
+                for error in check(dump_lines, header=dev.header):
                     errors += 1
                     check_ok = False
                     console.print(f"{check.__name__}: {error}", style="red")
@@ -302,9 +305,6 @@ class LUKSKeyStoreManager(object):
             success = False
 
         return success
-
-        if header:
-            self._KEYSTORE.backup_external_header(Path(header))
 
     def fingerprint(self, verify: bool, confirm: bool) -> int:
         """

pkgs/fc/ceph/src/fc/ceph/tests/test_luks_dump.py
@@ -2,6 +2,8 @@ import textwrap
 import unittest.mock
 from unittest.mock import MagicMock
 
+from fc.ceph.luks import KEYSTORE
+
 data_correct = """LUKS header information
 Version:       	2
 Epoch:         	8
@@ -205,6 +207,31 @@ def test_check_pbkdf_is_argon2id_error():
     ]
 
 
+def test_check_header_backup_valid(tmp_path, monkeypatch):
+    from fc.ceph.luks.checks import check_header_backup_valid
+
+    keystore_path = tmp_path / "keystore"
+    keystore_path.mkdir()
+    orig_header = tmp_path / "header.luks"
+    backup_header = keystore_path / "header.luks"
+    monkeypatch.setattr(KEYSTORE, "local_key_dir", keystore_path)
+
+    assert list(check_header_backup_valid([], orig_header)) == [
+        "Error checking header backup"
+    ]
+
+    orig_header.write_text("abc")
+    backup_header.write_text("aaa")
+
+    assert list(check_header_backup_valid([], orig_header)) == [
+        "Header backup differs from local header"
+    ]
+
+    backup_header.write_text("abc")
+
+    assert list(check_header_backup_valid([], orig_header)) == []
+
+
 def test_check_integration_ok(monkeypatch, capsys):
     from fc.ceph.luks.manage import LuksDevice, LUKSKeyStoreManager
 
@@ -239,11 +266,13 @@ def test_check_integration_ok(monkeypatch, capsys):
         check_key_slots_exactly_1_and_0: OK
         check_512_bit_keys: OK
         check_pbkdf_is_argon2id: OK
+        check_header_backup_valid: OK
         Checking testdev2:
         check_cipher: OK
         check_key_slots_exactly_1_and_0: OK
         check_512_bit_keys: OK
         check_pbkdf_is_argon2id: OK
+        check_header_backup_valid: OK
         """
     )
     assert captured.err == ""
@@ -258,11 +287,13 @@ def test_check_integration_error(monkeypatch, capsys):
                 base_blockdev="/dev/mapper/foo",
                 name="testdev1",
                 mountpoint="/mnt/foo",
+                header="/mnt/foo.luks",
             ),
             LuksDevice(
                 base_blockdev="/dev/vgbar/holygrail",
                 name="testdev2",
                 mountpoint="/mnt/bar",
+                header="/mnt/bar.luks",
             ),
         ]
     )
@@ -287,6 +318,7 @@ def test_check_integration_error(monkeypatch, capsys):
         check_key_slots_exactly_1_and_0: keyslots: unexpected configuration ({0, 3})
         check_512_bit_keys: keysize: 256 bits does not match expected 512 bits
         check_pbkdf_is_argon2id: pbkdf: SHAKE382 does not match expected argon2id
+        check_header_backup_valid: Error checking header backup
         Checking testdev2:
         check_cipher: Unable to check cipher correctness, no `Cipher:` found in dump
         check_key_slots_exactly_1_and_0: keyslots: unexpected configuration (set())
@@ -295,6 +327,7 @@ def test_check_integration_error(monkeypatch, capsys):
         dump
         check_pbkdf_is_argon2id: Unable to check PBKDF correctness, no `PBKDF:` found in
         dump
+        check_header_backup_valid: Error checking header backup
         """
     )
     assert captured.err == ""