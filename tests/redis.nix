35d8f9e80fce52db845c24372c2e422a63d4ecdf
Maximilian Bosch <mb@flyingcircus.io>
2025-05-20 16:41:29 +0200

    services/redis: configure sensu checks & telegraf for each instance
    PL-133583, FC-41338, PL-133563
    
    The NixOS module allows to configure multiple Redis instances using
    `services.redis.servers."<name>"`. This adds monitoring for all of
    them.
    
    If there's an unreachable configure (i.e. no TCP socket & a Unix socket
    w/o read/write for its group), a warning like this will be issued when
    building the system:
    
        system-build-out               > evaluation warning: Cannot configure Telegraf metrics and Sensu checks for the following named redis servers:
        system-build-out               >
        system-build-out               >                     * gitlab2
        system-build-out               >
        system-build-out               >                     This means that no TCP socket is configured for connection and the Unix socket either doesn't
        system-build-out               >                     exist or cannot be accessed by its respective group.

--- tests/redis.nix
+++ tests/redis.nix
@@ -1,16 +1,22 @@
 import ./make-test-python.nix (
-  { ... }:
+  { pkgs, ... }:
   {
     name = "redis";
     nodes = {
       redis =
-        { ... }:
+        { pkgs, lib, ... }:
         {
           imports = [
             ../nixos
             ../nixos/roles
           ];
           flyingcircus.roles.redis.enable = true;
+
+          services.redis.servers.gitlab = {
+            enable = true;
+            unixSocketPerm = 770;
+            requirePassFile = "${pkgs.writeText "password" "hunter2"}";
+          };
 
           flyingcircus.enc.parameters = {
             resource_group = "test";
@@ -24,23 +30,95 @@ import ./make-test-python.nix (
             };
           };
 
+          # Taken from statshost-master test: needed to verify that
+          # telegraf is correctly configured.
+          flyingcircus.roles.statshost-master.enable = true;
+          flyingcircus.roles.statshost = {
+            hostName = "redis";
+            useSSL = false;
+          };
+
+          flyingcircus.encAddresses = [
+            {
+              name = "redis";
+              ip = "192.168.1.1";
+            }
+          ];
+          networking.extraHosts = ''
+            192.168.1.1 redis.fcio.net redis
+          '';
+
+          services.telegraf.enable = true;
+          services.telegraf.extraConfig.agent.interval = "1s";
+
+          users.users.s-test = {
+            isNormalUser = true;
+            extraGroups = [ "service" ];
+          };
+
+          services.prometheus.globalConfig.scrape_interval = "1s";
+
+          # Grafana is being enabled by statshost-master. This isn't needed for this
+          # test-case and spams the logs, so it's disabled here.
+          services.grafana.enable = lib.mkForce false;
         };
     };
 
-    testScript = ''
-      redis.wait_for_unit("redis.service")
-      cli = "redis-cli -a `< /etc/local/redis/password `"
-      redis.wait_until_succeeds(f"{cli} ping | grep PONG")
-      redis.succeed(f"{cli} set msg 'hello world'")
-      redis.succeed(f"{cli} get msg | grep 'hello world'")
+    testScript =
+      { nodes, ... }:
+      let
+        sensuChecks = nodes.redis.flyingcircus.services.sensu-client.checks;
+        api = "http://192.168.1.1:9090/api/v1";
+      in
+      ''
+        import json
 
-      # service user should be able to write the password file
-      redis.succeed('sudo -u redis touch /etc/local/redis/password')
+        redis.wait_for_unit("redis.service")
+        redis.wait_for_unit("redis-gitlab.service")
+        redis.wait_for_unit("prometheus.service")
+        redis.wait_for_unit("telegraf.service")
+        redis.wait_for_file("/run/telegraf/influx.sock")
 
-      # killing the redis process should trigger an automatic restart
-      redis.succeed("kill $(systemctl show redis.service --property MainPID | sed -e 's/MainPID=//')")
-      redis.wait_for_unit("redis.service")
-      redis.wait_until_succeeds(f"{cli} ping | grep PONG")
-    '';
+        cli = "redis-cli -a `< /etc/local/redis/password `"
+        redis.wait_until_succeeds(f"{cli} ping | grep PONG")
+        redis.succeed(f"{cli} set msg 'hello world'")
+        redis.succeed(f"{cli} get msg | grep 'hello world'")
+
+        cli_gitlab = "redis-cli -a hunter2 -s /run/redis-gitlab/redis.sock"
+        redis.wait_until_succeeds(f"{cli_gitlab} ping | grep PONG")
+        redis.wait_until_succeeds(f"{cli_gitlab} set msg 'hello world'")
+        redis.wait_until_succeeds(f"{cli_gitlab} get msg | grep 'hello world'")
+
+        with subtest("service user should be able to write the password file"):
+            redis.succeed('sudo -u redis touch /etc/local/redis/password')
+
+        with subtest("Sensu checks correctly configured"):
+            redis.succeed("${pkgs.writeShellScript "sensu-redis" sensuChecks.redis.command}")
+            redis.succeed("${pkgs.writeShellScript "sensu-redis-gitlab" sensuChecks.redis-gitlab.command}")
+
+        with subtest("Metrics for servers are scraped by telegraf"):
+            redis.wait_until_succeeds("test true = \"$(curl -s ${api}/query?query='redis_uptime' | jq '.data.result|length > 0')\"")
+
+            ret_val = json.loads(redis.succeed("""
+                curl -s ${api}/query?query='redis_uptime'
+            """))
+            print(ret_val)
+
+            assert ret_val['status'] == 'success'
+            data = ret_val['data']['result']
+            assert len(data) == 2
+
+            assert '/run/redis-gitlab/redis.sock' ==  [x['metric']['socket'] for x in data if 'socket' in x['metric']][0]
+            assert '127.0.0.1:6379' ==  [
+                x['metric']['server'] + ":" + x['metric']['port']
+                for x in data if 'server' in x['metric']
+            ][0]
+
+        with subtest("killing the redis process should trigger an automatic restart"):
+            redis.succeed("kill $(systemctl show redis.service --property MainPID | sed -e 's/MainPID=//')")
+            redis.succeed("sleep 5")
+            redis.wait_for_unit("redis.service")
+            redis.wait_until_succeeds(f"{cli} ping | grep PONG")
+      '';
   }
 )
