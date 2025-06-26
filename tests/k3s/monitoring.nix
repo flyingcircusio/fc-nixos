7684057e29cdb86efce81f628aecbe703279e194
Christian Theune <ct@flyingcircus.io>
2025-06-26 13:36:13 +0200

    tests: work around a v9fs bug on 6.12+

--- tests/k3s/monitoring.nix
+++ tests/k3s/monitoring.nix
@@ -55,6 +55,8 @@ import ../make-test-python.nix (
 
           virtualisation.memorySize = 2000;
           virtualisation.diskSize = lib.mkForce 3000;
+          virtualisation.useNixStoreImage = true; # PL-133821 - 6.12 crashes v9fs in some situations
+
           virtualisation.vlans = [
             1
             2
