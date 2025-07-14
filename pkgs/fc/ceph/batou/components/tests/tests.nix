72758f1d01fcad38b46c82cc37942100f2810fec
Christian Theune <ct@flyingcircus.io>
2025-07-14 10:19:00 +0200

    catch up with findings from fc.qemu

--- pkgs/fc/ceph/batou/components/tests/tests.nix
+++ pkgs/fc/ceph/batou/components/tests/tests.nix
@@ -14,11 +14,16 @@ in
 {
 
   environment.systemPackages =
+    # This is a horrible dance for two reasons:
+    # 1. pytest doesn't properly inherit the PATH environment from the propagatedBuildInputs.
+    #    We might want to reconsider using an additional buildPythonApplication with pytest
+    #    added to the primary dependencies (propagatedBuildInputs)
+    # 2. There's a bug(?) in stdenv.mkDerivation that causes external access to the
+    #    attribute's items to end up with their .dev outputs ... -_-
     let
       testPackages = (
         [ testPackage ]
-        ++ testPackage.propagatedBuildInputs
-        ++ testPackage.checkInputs
+        ++ (map (x: builtins.removeAttrs x [ "outputSpecified" ]) testPackage.propagatedBuildInputs)
         ++ testPackage.nativeCheckInputs
       );
       PYTHONPATH = testPackage.py.makePythonPath testPackages;
