5bb0cb4b5fc822465cfdc19f360b24a37f60f95b
Leona Maroni <lm@flyingcircus.io>
2025-07-21 14:03:40 +0200

    roles/rgw-location-proxy: init and add support for it in roles/ceph/rgw
    PL-128135
    
    TLS termination is done with nginx. The ACME challenge is DNS-01 since
    we usually have multiple RGW instances.
    
    Ceph RGW instances now have an additional script that accounts traffic to
    it.
    
    Co-Authored-By: Maximilian Bosch <mb@flyingcircus.io>
    (cherry picked from commit 8513eef2118103b7bf5d2a85f6e1c97d063b4866)

---
+++ nixos/roles/rgw-location-proxy.nix
@@ -0,0 +1,100 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (config) fclib;
+  cfg = config.flyingcircus.roles.rgw-location-proxy;
+  haproxyPort = toString 7475;
+in
+{
+  options.flyingcircus.roles.rgw-location-proxy = {
+    enable = lib.mkEnableOption "CEPH RGW location proxy";
+
+    domain = lib.mkOption {
+      type = lib.types.str;
+      default = lib.concatStringsSep "." (
+        [
+          "objects"
+          config.flyingcircus.location
+        ]
+        ++ lib.optionals (config.flyingcircus.enc.parameters.resource_group != "services") [
+          config.flyingcircus.enc.parameters.resource_group
+        ]
+        ++ [ "fcio.net" ]
+      );
+      defaultText = "objects.<location>.<resource_group>.fcio.net";
+      description = ''
+        Domain where the object gateway is publicly exposed.
+      '';
+    };
+
+    dns01CredFile = lib.mkOption {
+      type = lib.types.pathWith {
+        absolute = true;
+        inStore = false;
+      };
+      default = "/etc/local/nixos/rgw-dns01.env";
+      description = ''
+        Path to the file containing credentials for the DNS01 challenge. Must have the format
+        ```
+        PDNS_API_KEY=<api-key>
+        PDNS_API_URL=https://dns.flyingcircus.io/
+        PDNS_PROPAGATION_TIMEOUT=3610
+        ```
+      '';
+    };
+  };
+
+  config = (
+    lib.mkIf cfg.enable {
+      flyingcircus.services.nginx = {
+        enable = true;
+        virtualHosts.${cfg.domain} = lib.mkMerge [
+          {
+            locations."/".proxyPass = "http://[::1]:${haproxyPort}";
+            forceSSL = true;
+            enableACME = false;
+            useACMEHost = cfg.domain;
+          }
+        ];
+      };
+
+      security.acme.certs.${cfg.domain} = {
+        dnsProvider = "pdns";
+        credentialsFile = cfg.dns01CredFile;
+        webroot = lib.mkForce null;
+        group = "nginx";
+      };
+
+      flyingcircus.services.haproxy = {
+        enable = true;
+        enableStructuredConfig = true;
+
+        frontend = {
+          http-in = {
+            binds = [ "[::1]:${haproxyPort}" ];
+            default_backend = "s3";
+          };
+        };
+
+        backend = {
+          s3 = {
+            servers = map (
+              service:
+              let
+                name = builtins.head (lib.splitString "." service.address);
+                address = builtins.head (builtins.filter fclib.isIp4 service.ips);
+              in
+              "s3-${name} ${address}:7480 check inter 10s rise 2 fall 1 maxconn 1000"
+            ) (fclib.findServices "ceph_rgw-server");
+            extraConfig = ''
+              option httpchk GET /rgw-monitoring/probe
+            '';
+          };
+        };
+      };
+    }
+  );
+}
