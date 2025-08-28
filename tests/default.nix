db5e64bb4c2e1e79c3b447cd5872ffdce61e5b2c
Molly Miller <mm@flyingcircus.io>
2025-08-28 09:18:42 +0200

    open-webui: fix various typos in review feedback
    (cherry picked from commit dc3c73148564b91b8318326a528729e1ed79eeac)

--- tests/default.nix
+++ tests/default.nix
@@ -96,7 +96,7 @@ in
   opensearch = callTest ./opensearch.nix { };
   opensearch_dashboards = callTest ./opensearch_dashboards.nix { };
   openvpn = callTest ./openvpn.nix { };
-  open-webui = callTest ./open-webui { };
+  open-webui = callTest ./open-webui.nix { };
   percona80 = callTest ./mysql.nix { rolename = "percona80"; };
   percona84 = callTest ./mysql.nix { rolename = "percona84"; };
   physical-installer = callTest ./physical-installer.nix { inherit nixpkgs; };
