a6787c16b6e02097536953404e29e93b5700ad27
Oliver Schmidt <os@flyingcircus.io>
2025-10-02 18:17:42 +0200

    Reapply "Merge pull request #1834 from flyingcircusio/PL-134064-nullable-summary-properties"
    This reverts commit 3c90ae783e4886b08cdab0b19a3a2554db184942.

--- pkgs/fc/agent/src/fc/maintenance/activity/update.py
+++ pkgs/fc/agent/src/fc/maintenance/activity/update.py
@@ -321,7 +321,7 @@ class UpdateActivity(Activity):
         self.returncode = 0
 
     @property
-    def summary(self):
+    def summary(self) -> str:
         """
         A human-readable summary of what will be changed by this update.
         Includes possible reboots, significant unit state changes (start, stop,
@@ -350,9 +350,11 @@ class UpdateActivity(Activity):
         elif self.current_environment is not None:
             msg.append(f"Environment: {self.current_environment} (unchanged)")
 
-        current_build = nixos.get_fc_channel_build(
-            self.current_channel_url, self.log
-        )
+        current_build = None
+        if self.current_channel_url:
+            current_build = nixos.get_fc_channel_build(
+                self.current_channel_url, self.log
+            )
         if current_build:
             next_build = nixos.get_fc_channel_build(
                 self.next_channel_url, self.log
