adf7b5ad5b76b7122d07a78776eb81d2fd349770
Oliver Schmidt <os@flyingcircus.io>
2025-10-08 14:17:19 +0200

    statshost-global: allow-list `amd_rocm_smi` metrics
    these are issued by our AI inference servers

--- nixos/roles/statshost/global-metrics.nix
+++ nixos/roles/statshost/global-metrics.nix
@@ -10,6 +10,7 @@ let
   # Replaces flyingcircus.statshost.globalAllowedMetrics in role code.
   # This also includes the host metrics from telegraf defined in platform/monitoring.nix.
   globalAllowedMetrics = [
+    "amd_rocm_smi"
     "apache"
     "ceph"
     "conntrack"
