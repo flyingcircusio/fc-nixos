cffd83f2a6383926c683a136ee67ab522f39aca5
Oliver Schmidt <os@flyingcircus.io>
2025-10-09 14:22:35 +0200

    fc-ceph.batou: fix enc update to preserve provisioned data
    The enc data needs to preserve the initially provisioned data, e.g.
    information about network interfaces. Hence we cannot write a new enc
    file from scratch, but need to update it based on initial data.
    
    Fixes system rebuild after batou provisioning.

--- pkgs/fc/ceph/batou/components/ceph/component.py
+++ pkgs/fc/ceph/batou/components/ceph/component.py
@@ -4,7 +4,21 @@ from batou.lib.file import File
 
 class Ceph(Component):
     def configure(self):
-        self.provide("enc", {"roles": ["ceph_mon", "ceph_osd"]})
+        # keep constants in sync with init_cluster.py
+        self.provide(
+            "enc",
+            {
+                "roles": ["ceph_mon", "ceph_osd"],
+                "parameters": {
+                    "location": "test",
+                    "resource_group": "services",
+                    "secret_salt": "salt-for-${config.networking.hostName}-dhkasjy9",
+                    "secrets": {
+                        "ceph/admin_key": "AQBFJa9hAAAAABAAtdggM3mhVBAEYw3+Loehqw==",
+                    },
+                },
+            },
+        )
 
         self += File("init_cluster.py", is_template=False, mode=0o755)
         self |= (ceph_nix := File("/etc/local/nixos/ceph.nix"))
