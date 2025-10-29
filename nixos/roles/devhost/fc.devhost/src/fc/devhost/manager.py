838f93b375247fd45fa8addd9f63978894675cc8
Eli Kogan-Wang <elikowa@gmail.com>
2025-10-29 10:34:45 +0100

    devhost: change marker for qemu process detection
    (cherry picked from commit b21a2c81287054b0afe6a0800967405dea23d882)

--- nixos/roles/devhost/fc.devhost/src/fc/devhost/manager.py
+++ nixos/roles/devhost/fc.devhost/src/fc/devhost/manager.py
@@ -619,10 +619,9 @@ class Manager:
                 # pid file may contain trailing lines with garbage
                 for line in p:
                     proc = psutil.Process(int(line))
-                    # marker = "{name},process=kvm.{name}".format(name=self.name)
-                    marker = self.name  # i was unable to find the marker above in the cmdline of the qemu process
+                    marker = f" -name {self.name} "
                     # Do not use proc.name() here - it's only 16 bytes ...
-                    if marker not in proc.cmdline():
+                    if marker not in " ".join(proc.cmdline()):
                         break
                     return proc
         except (IOError, OSError, ValueError, psutil.NoSuchProcess):
