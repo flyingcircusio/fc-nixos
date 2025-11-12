1f1e80456deace7a4ca45937eff653594c2d7300
Leona Maroni <lm@flyingcircus.io>
2025-11-12 11:02:44 +0100

    fc.agent: fix slurm invocations after pyslurm 25.05 update
    PL-133856

--- pkgs/fc/agent/src/fc/util/tests/test_slurm.py
+++ pkgs/fc/agent/src/fc/util/tests/test_slurm.py
@@ -5,12 +5,32 @@ from textwrap import dedent
 from unittest.mock import MagicMock
 
 import pytest
-from pytest import fixture, raises
 
 from fc.util.logging import init_logging
 
+
+class Node:
+    def __init__(self, **kwargs):
+        self.__dict__.update(**kwargs)
+
+    def __eq__(self, other_node):
+        print(other_node.__dict__)
+        return self.__dict__ == other_node.__dict__
+
+    def __repr__(self):
+        return f"<Node({str(self.__dict__)})>"
+
+    @classmethod
+    def load(cls, name):
+        return cls(name=name)
+
+    def modify(self, *args):
+        pass
+
+# We cannot import pyslurm, as it just exits with error 1 when it has no slurm cluster
 pyslurm = type(sys)("pyslurm")
-pyslurm.node = MagicMock()
+pyslurm.Nodes = MagicMock()
+pyslurm.Node = Node
 pyslurm.get_controllers = MagicMock()
 pyslurm.NODE_STATE_DRAIN = 1
 pyslurm.NODE_STATE_DOWN = 2
@@ -26,11 +46,10 @@ pyslurm.version = type(sys)("pyslurm.version")
 pyslurm.version.__version__ = "24.11.0"
 sys.modules["pyslurm"] = pyslurm
 
-
 init_logging(verbose=True, syslog_identifier="slurm-test")
 
 import fc.util.slurm
-from fc.util.slurm import NodeStateError, NodeStateTimeout, drain
+from fc.util.slurm import NodeStateTimeout
 
 
 @pytest.mark.parametrize(
@@ -38,58 +57,49 @@ from fc.util.slurm import NodeStateError, NodeStateTimeout, drain
     ["IDLE+DRAIN", "ALLOCATED+DRAIN", "MIXED+DRAIN", "DOWN+DRAIN", "DOWN"],
 )
 @unittest.mock.patch("fc.util.slurm.get_node_info")
-@unittest.mock.patch("fc.util.slurm.update_nodes")
-def test_ready(update_nodes: MagicMock, get_node_info, state, logger):
-    get_node_info.return_value = {"name": "test20", "state": state}
+@unittest.mock.patch("fc.util.slurm.modify_node")
+def test_ready(modify_node: MagicMock, get_node_info, state, logger):
+    get_node_info.return_value = Node(name="test20", state=state)
     fc.util.slurm.ready(logger, "test20")
-    update_nodes.assert_called_once_with(
-        {
-            "node_names": "test20",
-            "node_state": pyslurm.NODE_RESUME,
-        }
-    )
+    modify_node.assert_called_once_with("test20", Node(state="RESUME"))
 
 
 @unittest.mock.patch("fc.util.slurm.get_node_info")
-@unittest.mock.patch("fc.util.slurm.update_nodes")
-def test_ready_noop(update_nodes: MagicMock, get_node_info, logger):
-    get_node_info.return_value = {"name": "test20", "state": "IDLE"}
+@unittest.mock.patch("fc.util.slurm.modify_node")
+def test_ready_noop(modify_node: MagicMock, get_node_info, logger):
+    get_node_info.return_value = Node(name="test20", state="IDLE")
     fc.util.slurm.ready(logger, "test20")
-    update_nodes.assert_not_called()
+    modify_node.assert_not_called()
 
 
 @unittest.mock.patch("fc.util.slurm.get_node_info")
-@unittest.mock.patch("fc.util.slurm.update_nodes")
-def test_ready_offline(update_nodes: MagicMock, get_node_info, logger):
-    get_node_info.return_value = {"name": "test20", "state": "IDLE*"}
+@unittest.mock.patch("fc.util.slurm.modify_node")
+def test_ready_offline(modify_node: MagicMock, get_node_info, logger):
+    get_node_info.return_value = Node(name="test20", state="IDLE*")
     fc.util.slurm.ready(logger, "test20")
-    update_nodes.assert_not_called()
+    modify_node.assert_not_called()
 
 
 @unittest.mock.patch("fc.util.slurm.get_node_info")
-@unittest.mock.patch("fc.util.slurm.update_nodes")
-def test_down(update_nodes: MagicMock, get_node_info, logger, monkeypatch):
-    get_node_info.return_value = {"name": "test20", "state": "IDLE+DRAIN"}
+@unittest.mock.patch("fc.util.slurm.modify_node")
+def test_down(modify_node: MagicMock, get_node_info, logger, monkeypatch):
+    get_node_info.return_value = Node(name="test20", state="IDLE+DRAIN")
     fc.util.slurm.down(logger, "test20", "test down")
-    update_nodes.assert_called_once_with(
-        {
-            "node_names": "test20",
-            "node_state": pyslurm.NODE_STATE_DOWN,
-            "reason": "test down",
-        }
+    modify_node.assert_called_once_with(
+        "test20", Node(state="DOWN", reason="test down")
     )
 
 
 @unittest.mock.patch("fc.util.slurm.get_node_info")
-@unittest.mock.patch("fc.util.slurm.update_nodes")
-def test_down_noop(update_nodes: MagicMock, get_node_info, logger, monkeypatch):
-    get_node_info.return_value = {"name": "test20", "state": "DOWN+DRAIN"}
+@unittest.mock.patch("fc.util.slurm.modify_node")
+def test_down_noop(modify_node: MagicMock, get_node_info, logger, monkeypatch):
+    get_node_info.return_value = Node(name="test20", state="DOWN+DRAIN")
     fc.util.slurm.down(logger, "test20", "test down noop")
-    update_nodes.assert_not_called()
+    modify_node.assert_not_called()
 
 
-@unittest.mock.patch("fc.util.slurm.update_nodes")
-def test_drain(update_nodes, logger, monkeypatch):
+@unittest.mock.patch("fc.util.slurm.modify_node")
+def test_drain(modify_node, logger, monkeypatch):
     iter_states = iter(
         [
             "ALLOCATED",
@@ -99,68 +109,53 @@ def test_drain(update_nodes, logger, monkeypatch):
     )
 
     def fake_get_node_info(node_name):
-        return {
-            "name": "test20",
-            "state": next(iter_states),
-        }
+        return Node(name="test20", state=next(iter_states))
 
     monkeypatch.setattr(fc.util.slurm, "get_node_info", fake_get_node_info)
-    drain(logger, "test20", 3, "test drain")
+    fc.util.slurm.drain(logger, "test20", 3, "test drain")
 
-    update_nodes.assert_called_once_with(
-        {
-            "node_names": "test20",
-            "node_state": pyslurm.NODE_STATE_DRAIN,
-            "reason": "test drain",
-        }
+    modify_node.assert_called_once_with(
+        "test20", Node(state="DRAIN", reason="test drain")
     )
 
 
 @unittest.mock.patch("fc.util.slurm.get_node_info")
-@unittest.mock.patch("fc.util.slurm.update_nodes")
+@unittest.mock.patch("fc.util.slurm.modify_node")
 def test_drain_noop_when_already_drained(
-    update_nodes: MagicMock, get_node_info, logger
+    modify_node: MagicMock, get_node_info, logger
 ):
-    get_node_info.return_value = {
-        "name": "test20",
-        "state": "IDLE+DRAIN",
-    }
-    drain(logger, "test20", 3, "test drain")
-
-    update_nodes.assert_not_called()
+    get_node_info.return_value = Node(name="test20", state="IDLE+DRAIN")
+    fc.util.slurm.drain(logger, "test20", 3, "test drain")
+    modify_node.assert_not_called()
 
 
 def test_drain_timeout(logger, monkeypatch):
     iter_states = chain(iter(["MIXED"]), repeat("MIXED+DRAIN"))
 
     def fake_get_node_info(node_name):
-        return {
-            "name": "test20",
-            "state": next(iter_states),
-        }
+        return Node(name="test20", state=next(iter_states))
 
     monkeypatch.setattr(fc.util.slurm, "get_node_info", fake_get_node_info)
-    with raises(NodeStateTimeout) as e:
-        drain(logger, "test20", 2, "test drain")
+    with pytest.raises(NodeStateTimeout) as e:
+        fc.util.slurm.drain(logger, "test20", 2, "test drain")
 
     assert e.value.remaining_node_states == {"test20": "MIXED+DRAIN"}
 
 
 def test_drain_many_noop(logger, monkeypatch):
     def fake_get_node_info(node_name):
-        return {"name": node_name, "state": "IDLE+DRAIN"}
+        return Node(name=node_name, state="IDLE+DRAIN")
 
     monkeypatch.setattr(fc.util.slurm, "get_node_info", fake_get_node_info)
-
     fc.util.slurm.drain_many(logger, ["test20", "test21"], 3, "test drain noop")
 
 
 def test_check_controller(logger):
-    pyslurm.node.return_value.get.return_value = {
-        "test20": {"state": "IDLE"},
-        "test21": {"state": "ALLOCATED"},
-        "test22": {"state": "MIXED"},
-    }
+    pyslurm.Nodes.load.return_value.items.return_value = {
+        "test20": Node(name="test20", state="IDLE"),
+        "test21": Node(name="test21", state="ALLOCATED"),
+        "test22": Node(name="test22", state="MIXED"),
+    }.items()
     res = fc.util.slurm.check_controller(logger, "test20")
     assert res.errors == []
     assert res.warnings == []
@@ -174,13 +169,13 @@ def test_check_controller(logger):
 
 
 def test_check_controller_warning(logger):
-    pyslurm.node.return_value.get.return_value = {
-        "test20": {"state": "IDLE"},
-        "test21": {"state": "ALLOCATED"},
-        "test22": {"state": "MIXED"},
-        "test23": {"state": "DOWN", "reason": "down"},
-        "test24": {"state": "DOWN*", "reason": "down unresp"},
-    }
+    pyslurm.Nodes.load.return_value.items.return_value = {
+        "test20": Node(name="test20", state="IDLE"),
+        "test21": Node(name="test21", state="ALLOCATED"),
+        "test22": Node(name="test22", state="MIXED"),
+        "test23": Node(name="test23", state="DOWN", reason="down"),
+        "test24": Node(name="test24", state="DOWN*", reason="down unresp")
+    }.items()
     res = fc.util.slurm.check_controller(logger, "test20")
     assert res.errors == []
     assert res.warnings == [
@@ -190,11 +185,11 @@ def test_check_controller_warning(logger):
 
 
 def test_check_controller_critical(logger):
-    pyslurm.node.return_value.get.return_value = {
-        "test22": {"state": "IDLE+DRAIN", "reason": "drain"},
-        "test23": {"state": "DOWN", "reason": "down"},
-        "test24": {"state": "DOWN*", "reason": "down unresp"},
-    }
+    pyslurm.Nodes.load.return_value.items.return_value = {
+        "test22": Node(name="test22", state="IDLE+DRAIN", reason="drain"),
+        "test23": Node(name="test23", state="DOWN", reason="down"),
+        "test24": Node(name="test24", state="DOWN*", reason="down unresp"),
+    }.items()
     res = fc.util.slurm.check_controller(logger, "test20")
     assert res.errors == [
         "All nodes cannot accept jobs: "
@@ -260,11 +255,8 @@ def test_drain_many(logger, monkeypatch):
     }
 
     def fake_get_node_info(node_name):
-        return {
-            "name": node_name,
-            "state": next(iter_states[node_name]),
-        }
-
+        return Node(name=node_name, state=next(iter_states[node_name]))
+    
     monkeypatch.setattr(fc.util.slurm, "get_node_info", fake_get_node_info)
     fc.util.slurm.drain_many(
         logger, list(iter_states.keys()), 3, "test drain many"
@@ -293,14 +285,11 @@ def test_drain_many_timeout(logger, log, monkeypatch):
     }
 
     def fake_get_node_info(node_name):
-        return {
-            "name": node_name,
-            "state": next(iter_states[node_name]),
-        }
+        return Node(name=node_name, state=next(iter_states[node_name]))
 
     monkeypatch.setattr(fc.util.slurm, "get_node_info", fake_get_node_info)
 
-    with raises(NodeStateTimeout) as e:
+    with pytest.raises(NodeStateTimeout) as e:
         fc.util.slurm.drain_many(
             logger, list(iter_states.keys()), 3, "test drain many timeout"
         )
@@ -355,11 +344,7 @@ def test_ready_many(logger, monkeypatch):
     # fails to ignore it because the node will never be in a ready state.
 
     def fake_get_node_info(node_name):
-        return {
-            "name": node_name,
-            "state": next(iter_states[node_name]),
-            "reason": "other" if node_name == "test25" else "test ready many",
-        }
+        return Node(name=node_name, state=next(iter_states[node_name]), reason="other" if node_name == "test25" else "test ready many")
 
     monkeypatch.setattr(fc.util.slurm, "get_node_info", fake_get_node_info)
     fc.util.slurm.ready_many(
@@ -388,15 +373,11 @@ def test_ready_many_timeout(logger, log, monkeypatch):
     }
 
     def fake_get_node_info(node_name):
-        return {
-            "name": node_name,
-            "state": next(iter_states[node_name]),
-            "reason": "test ready many timeout",
-        }
+        return Node(name=node_name, state=next(iter_states[node_name]), reason="test ready many timeout")
 
     monkeypatch.setattr(fc.util.slurm, "get_node_info", fake_get_node_info)
 
-    with raises(NodeStateTimeout) as e:
+    with pytest.raises(NodeStateTimeout) as e:
         fc.util.slurm.ready_many(
             logger,
             list(iter_states.keys()),
@@ -418,11 +399,22 @@ def test_ready_many_timeout(logger, log, monkeypatch):
 def test_fc_slurm_check_returns_fallback_warning_on_no_data(logger):
     from fc.util.checks import CheckResult
 
-    pyslurm.node.return_value.get.return_value = {
-        "test20": {"state": "IDLE"},
-        "test21": {"state": "ALLOCATED"},
-        "test22": {"state": "MIXED"},
-    }
+    pyslurm.Nodes.load.return_value.keys.return_value = [
+        "test20",
+        "test21",
+        "test22"
+    ]
+    pyslurm.Nodes.load.return_value.items.return_value = {
+        "test20": Node(name="test20", state="IDLE"),
+        "test21": Node(name="test21", state="ALLOCATED"),
+        "test22": Node(name="test22", state="MIXED")
+    }.items()
+
+    pyslurm.Nodes.load.return_value.get.side_effect = lambda node_name: {
+        "test20": Node(name="test20", state="IDLE"),
+        "test21": Node(name="test21", state="ALLOCATED"),
+        "test22": Node(name="test22", state="MIXED")
+    }[node_name]
     pyslurm.get_controllers.return_value = ["test20"]
 
     check_result = fc.util.slurm.check("logger", "somehost")
