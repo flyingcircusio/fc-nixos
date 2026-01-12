71b266576df548f014a5b16ef20b962d22ff7d34
Oliver Schmidt <os@flyingcircus.io>
2026-01-12 17:11:28 +0100

    fc.check-ceph: modernize (python version, pyproject)
    When moving away from using the rados and rbd python bindings, we are
    not bound to use the same ancient python version anymore as used by
    ceph.
    A pyproject.toml package structure simplifies building it with nix.
    
    (cherry picked from commit f1f47caa1d03be875e6ec23eecf6051dc165e4ea)

--- pkgs/fc/default.nix
+++ pkgs/fc/default.nix
@@ -24,9 +24,8 @@ rec {
 
   check-age = callPackage ./check-age { };
   check-bgp-sessions = callPackage ./check-bgp-sessions { };
-  check-ceph-nautilus = callPackage ./check-ceph/nautilus {
+  check-ceph-nautilus = pyPackages.callPackage ./check-ceph/nautilus {
     inherit (pkgs.ceph-nautilus) ceph-client;
-    python3Packages = pkgs.python38Packages;
   };
   check-haproxy = callPackage ./check-haproxy { };
   check-journal = callPackage ./check-journal.nix { };
