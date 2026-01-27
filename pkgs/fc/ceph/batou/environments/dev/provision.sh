fe6c0d9498d965e162325d8e1246214f9308d34e
Oliver Schmidt <os@flyingcircus.io>
2026-01-27 15:35:24 +0100

    fc-ceph/batou: fix provision path and flake resolving
    The top-level name of the repo working dir is not always called
    `fc-nixos`.

--- pkgs/fc/ceph/batou/environments/dev/provision.sh
+++ pkgs/fc/ceph/batou/environments/dev/provision.sh
@@ -1,4 +1,6 @@
-COPY ../../../../../../../fc-nixos /home/developer/
+COPY ../../../../../../ /home/developer/fc-nixos
 RUN chown developer: -R /home/developer/fc-nixos
 RUN rm -rf /home/developer/fc-nixos/channels
+# un-confuse nix flake references
+RUN rm -rf /home/developer/fc-nixos/.git
 RUN /home/developer/fc-nixos/dev-setup
