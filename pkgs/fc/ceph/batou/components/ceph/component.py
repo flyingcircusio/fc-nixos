32c94b9fb86712d95a75b89bc0d1eb50f7200ac9
Oliver Schmidt <os@flyingcircus.io>
2026-01-28 11:32:47 +0100

    fc-ceph/batou: fix ceph secret generation
    (cherry picked from commit 5509ba2c643a9f907c01e66099f49350eee96b98)

--- pkgs/fc/ceph/batou/components/ceph/component.py
+++ pkgs/fc/ceph/batou/components/ceph/component.py
@@ -5,6 +5,7 @@ from batou.lib.file import File
 class Ceph(Component):
     def configure(self):
         # keep constants in sync with init_cluster.py
+        # XXX: how does this relate to nixos/component.py?
         self.provide(
             "enc",
             {
@@ -12,7 +13,7 @@ class Ceph(Component):
                 "parameters": {
                     "location": "test",
                     "resource_group": "services",
-                    "secret_salt": "salt-for-${config.networking.hostName}-dhkasjy9",
+                    "secret_salt": f"salt-for-{self.host._name}-dhkasjy9",
                     "secrets": {
                         "ceph/admin_key": "AQBFJa9hAAAAABAAtdggM3mhVBAEYw3+Loehqw==",
                     },
