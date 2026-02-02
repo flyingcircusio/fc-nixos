6002b4154ce008a6226f532be26a10c1ca5525bf
Eli Kogan-Wang <elikowa@gmail.com>
2026-02-02 12:57:06 +0100

    Add embeddings reference json

--- nixos/roles/ai-model-server.nix
+++ nixos/roles/ai-model-server.nix
@@ -155,6 +155,9 @@ in
             lib.mapAttrsToList (n: v: v.name) enabledModels;
         };
 
+        flyingcircus.services.ai-model-server.skvaider.settings.embedding_verification_file =
+          lib.mkDefault pkgs.fc.skvaider.embeddings-reference-json;
+
         systemd.services.skvaider-inference = lib.mkIf cfg.skvaider-inference.enable {
           description = "Skvaider inference service";
           wantedBy = [ "multi-user.target" ];
