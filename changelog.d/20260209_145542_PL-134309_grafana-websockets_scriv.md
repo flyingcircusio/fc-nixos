791db71e5c119edd3523f96a46e29b5620aa31c7
Philipp Herzog <ph@flyingcircus.io>
2026-02-09 14:54:34 +0100

    [PL-134309] proxy websockets to grafana
    (cherry picked from commit 0f5c100f85aeda6285829c6eab72e434e6074dcd)

---
+++ changelog.d/20260209_145542_PL-134309_grafana-websockets_scriv.md
@@ -0,0 +1,5 @@
+### Impact
+
+### NixOS XX.XX platform
+
+- Enable Websocket connections between the Grafana frontend and backend for more efficient live updates (PL-134309)
--- nixos/roles/statshost/default.nix
+++ nixos/roles/statshost/default.nix
@@ -501,7 +501,10 @@ in
               auth_basic_user_file "/etc/local/htpasswd_fcio_users";
               proxy_pass http://${prometheusListenAddress};
             '';
-            "/grafana/".proxyPass = "http://127.0.0.1:3001/";
+            "/grafana/" = {
+              proxyPass = "http://127.0.0.1:3001/";
+              proxyWebsockets = true;
+            };
           };
         };
       };
