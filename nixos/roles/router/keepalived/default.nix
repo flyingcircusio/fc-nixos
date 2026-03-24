24cbae1b89c18f4cbf45dbb195e4eb21d406f8b1
Molly Miller <mm@flyingcircus.io>
2026-03-24 13:38:48 +0100

    router: make keepalived config customisable
    Also make the check scripts used by location keepalived configs
    overridable.
    
    PL-135248

--- nixos/roles/router/keepalived/default.nix
+++ nixos/roles/router/keepalived/default.nix
@@ -12,7 +12,7 @@ let
   inherit (config.networking) hostName;
   inherit (config.flyingcircus) location static;
   role = config.flyingcircus.roles.router;
-  locationConfig = readFile (./. + "/${location}.conf");
+  defaultLocationConfig = readFile (./. + "/${location}.conf");
   routerId = "${hostName}.gocept.net";
 
   checkDefaultRoute4 = pkgs.writeShellApplication {
@@ -52,7 +52,7 @@ let
       use_symlink_paths true
     }
 
-    ${locationConfig}
+    ${role.keepalivedConfig}
   '';
 
   requiredInterfaces = map (
@@ -63,81 +63,91 @@ let
   deviceDependencies = map (iface: "${iface}-netdev.service") requiredInterfaces;
 
 in
-lib.mkIf role.enable {
-
-  environment.etc."keepalived/check-default-route-v4".source =
-    "${checkDefaultRoute4}/bin/check-default-route-v4";
-  environment.etc."keepalived/check-default-route-v6".source =
-    "${checkDefaultRoute6}/bin/check-default-route-v6";
-  environment.etc."keepalived/check-zebra-liveness".source =
-    "${checkZebraLiveness}/bin/check-zebra-liveness";
-  environment.etc."keepalived/fc-keepalived".source = "${pkgs.fc.agent}/bin/fc-keepalived";
-  environment.etc."keepalived/keepalived.conf".source = keepalivedConf;
-
-  environment.systemPackages = with pkgs; [
-    keepalived
-    checkDefaultRoute4
-    checkDefaultRoute6
-  ];
-
-  flyingcircus.passwordlessSudoRules = [
-    {
-      commands = [
-        "${pkgs.fc.agent}/bin/fc-keepalived check"
-      ];
-      groups = [
-        "admins"
-        "sudo-srv"
-        "service"
-      ];
-    }
-  ];
-
-  services.keepalived = {
-    enable = true;
-    # Note: We override the keepalived config file in ExecStart below,
-    # using config options here has no effect.
-  };
-
-  systemd.services.keepalived = {
-    reloadIfChanged = true;
-    # Don't be confused by the name "restartTriggers", reload also uses it.
-    restartTriggers = [ keepalivedConf ];
-    # Ensure that keepalived is stopped *before* interfaces are
-    # stopped at shutdown.
-    after = deviceDependencies;
-    serviceConfig = {
-      Type = lib.mkOverride 90 "simple";
-      ExecStart = lib.mkOverride 90 (
-        "${pkgs.keepalived}/sbin/keepalived"
-        + " -f /etc/keepalived/keepalived.conf"
-        + " -D"
-        + " -p /run/keepalived.pid"
-        + " -n"
-      );
-      StateDirectory = "keepalived";
-      RuntimeDirectory = "keepalived";
+{
+  options.flyingcircus.roles.router = with lib; {
+    keepalivedConfig = mkOption {
+      type = types.lines;
+      description = "Keepalived configuration (will be appended to static global defaults)";
+      default = lib.optionalString (location != "standalone") defaultLocationConfig;
     };
   };
 
-  systemd.services.keepalived-reload = {
-    description = "Reload keepalived when required interfaces are changed";
-    after = addressDependencies ++ deviceDependencies;
-    wantedBy = deviceDependencies;
-    serviceConfig = {
-      Type = "oneshot";
-      Restart = "on-abnormal";
-      TimeoutSec = 120;
-      ExecCondition = "/run/current-system/systemd/bin/systemctl -q is-active keepalived.service";
-      ExecStart = "/run/current-system/systemd/bin/systemctl reload keepalived.service";
+  config = lib.mkIf role.enable {
+
+    environment.etc."keepalived/check-default-route-v4".source =
+      fclib.mkPlatform "${checkDefaultRoute4}/bin/check-default-route-v4";
+    environment.etc."keepalived/check-default-route-v6".source =
+      fclib.mkPlatform "${checkDefaultRoute6}/bin/check-default-route-v6";
+    environment.etc."keepalived/check-zebra-liveness".source =
+      fclib.mkPlatform "${checkZebraLiveness}/bin/check-zebra-liveness";
+    environment.etc."keepalived/fc-keepalived".source = "${pkgs.fc.agent}/bin/fc-keepalived";
+    environment.etc."keepalived/keepalived.conf".source = keepalivedConf;
+
+    environment.systemPackages = with pkgs; [
+      keepalived
+      checkDefaultRoute4
+      checkDefaultRoute6
+    ];
+
+    flyingcircus.passwordlessSudoRules = [
+      {
+        commands = [
+          "${pkgs.fc.agent}/bin/fc-keepalived check"
+        ];
+        groups = [
+          "admins"
+          "sudo-srv"
+          "service"
+        ];
+      }
+    ];
+
+    services.keepalived = {
+      enable = true;
+      # Note: We override the keepalived config file in ExecStart below,
+      # using config options here has no effect.
     };
+
+    systemd.services.keepalived = {
+      reloadIfChanged = true;
+      # Don't be confused by the name "restartTriggers", reload also uses it.
+      restartTriggers = [ keepalivedConf ];
+      # Ensure that keepalived is stopped *before* interfaces are
+      # stopped at shutdown.
+      after = deviceDependencies;
+      serviceConfig = {
+        Type = lib.mkOverride 90 "simple";
+        ExecStart = lib.mkOverride 90 (
+          "${pkgs.keepalived}/sbin/keepalived"
+          + " -f /etc/keepalived/keepalived.conf"
+          + " -D"
+          + " -p /run/keepalived.pid"
+          + " -n"
+        );
+        StateDirectory = "keepalived";
+        RuntimeDirectory = "keepalived";
+      };
+    };
+
+    systemd.services.keepalived-reload = {
+      description = "Reload keepalived when required interfaces are changed";
+      after = addressDependencies ++ deviceDependencies;
+      wantedBy = deviceDependencies;
+      serviceConfig = {
+        Type = "oneshot";
+        Restart = "on-abnormal";
+        TimeoutSec = 120;
+        ExecCondition = "/run/current-system/systemd/bin/systemctl -q is-active keepalived.service";
+        ExecStart = "/run/current-system/systemd/bin/systemctl reload keepalived.service";
+      };
+    };
+
+    networking.firewall.extraCommands = ''
+      ip6tables -A nixos-fw -p 112 -j nixos-fw-accept
+    '';
+
+    systemd.tmpfiles.rules = [
+      "d /etc/keepalived 0755 root root"
+    ];
   };
-
-  networking.firewall.extraCommands = ''
-    ip6tables -A nixos-fw -p 112 -j nixos-fw-accept
-  '';
-
-  systemd.tmpfiles.rules = [
-    "d /etc/keepalived 0755 root root"
-  ];
 }
