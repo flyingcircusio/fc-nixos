5c48a056ee3399490df72b4888125b19b4bda2d5
Oliver Schmidt <os@flyingcircus.io>
2026-03-30 12:59:19 +0200

    fc-ceph/api: fix auth, simplify runner infrastructure
    Many of the administral actions performed by the code require elevated
    cephx privileges, like creating new pools. Executing them as the
    `client.<hostname>` users with the privileges we grant clients is not
    possible. The easiest is to execute them as `client.admin`, which is the
    default anyway when not providing any user name.
    
    Refactors the existing code structure to use the newer
    fc.util.run infrastructure. This unifies call paths and provides
    additional logging insights for debugging.
    
    (cherry picked from commit 662e4bdbeb7f33ed4cd8a5b3023de394a711f66e)

--- pkgs/fc/ceph/src/fc/ceph/api/tests/cluster_test.py
+++ pkgs/fc/ceph/src/fc/ceph/api/tests/cluster_test.py
@@ -1,8 +1,7 @@
-import mock
 import pkg_resources
 import pytest
 
-from ..cluster import CephCmdError, Cluster
+from ..cluster import Cluster
 
 
 @pytest.fixture
@@ -16,43 +15,5 @@ class TestCluster(object):
     def test_default_pool_size(self, cluster):
         assert (2, 1) == cluster.default_pool_size()
 
-    @mock.patch("subprocess.Popen")
-    def test_generic_ceph_cmd_returns_stdout(self, popen, cluster):
-        p = popen.return_value
-        p.communicate.return_value = ("out", "err")
-        p.returncode = 0
-        assert ("out", "err") == cluster.generic_ceph_cmd(
-            ["ceph"], ["subcommand"]
-        )
-
-    @mock.patch("subprocess.Popen")
-    def test_generic_ceph_cmd_raises_on_failure(self, popen, cluster):
-        p = popen.return_value
-        p.communicate.return_value = ("", "")
-        p.returncode = 1
-        with pytest.raises(CephCmdError):
-            cluster.generic_ceph_cmd(["ceph"], ["subcommand"])
-
-    @mock.patch("subprocess.Popen")
-    def test_generic_ceph_cmd_accepts_failure(self, popen, cluster):
-        p = popen.return_value
-        p.communicate.return_value = ("", "")
-        p.returncode = 2
-        assert ("", "", 2) == cluster.generic_ceph_cmd(
-            ["ceph"], ["subcommand"], accept_failure=True
-        )
-
     def test_default_pg_num(self, cluster):
         assert 32 == cluster.default_pg_num()
-
-    def test_rbd_obeys_ceph_parameters(self):
-        cluster = Cluster("/path/to/ceph.conf")
-        setattr(cluster, "generic_ceph_cmd", lambda a, *args: a)
-        cluster.ceph_id = "host1"
-        assert [
-            "rbd",
-            "--id",
-            "host1",
-            "-c",
-            "/path/to/ceph.conf",
-        ] == cluster.rbd(["ls"])
