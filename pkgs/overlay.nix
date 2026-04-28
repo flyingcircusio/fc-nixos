993753a106b72575f699ff1791cf6318ff4e1dc6
Leona Maroni <lm@flyingcircus.io>
2026-04-28 17:38:47 +0200

    pkgs: remove not-used python38
    PL-134240

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -659,12 +659,6 @@ builtins.mapAttrs (_: patchPhps phpLogPermissionPatch) {
     ];
   });
 
-  python38 = builtins.trace "using 21.05 python38" (lib.dontRecurseIntoAttrs fc-nixos-21_05.python38);
-  python38Packages = builtins.trace "using 21.05 python38Packages" (
-    lib.dontRecurseIntoAttrs fc-nixos-21_05.python38Packages
-  );
-  py38_pytest_patterns = fc-nixos-21_05.py_pytest_patterns;
-
   qemu-ceph-nautilus = fc-nixos-21_05.qemu-ceph-nautilus;
   # pacific: let's also try a qemu version update from 6.0 to 10.1, because
   # pacific does not compile against old qemu
