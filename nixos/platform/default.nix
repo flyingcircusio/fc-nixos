a5d3e6d3c5e07f83e34a03d404503ff7c224f2bc
Leona Maroni <lm@flyingcircus.io>
2026-05-18 17:07:09 +0200

    platform: update openssh key exchange algorithms
    PL-135358

--- nixos/platform/default.nix
+++ nixos/platform/default.nix
@@ -449,6 +449,8 @@ in
         KbdInteractiveAuthentication = false;
         PasswordAuthentication = false;
         KexAlgorithms = [
+          "mlkem768x25519-sha256"
+          "sntrup761x25519-sha512"
           "sntrup761x25519-sha512@openssh.com"
           "curve25519-sha256"
           "curve25519-sha256@libssh.org"
