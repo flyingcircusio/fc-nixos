d564b36146953e1da426eaa0b07210d197e34e6a
Molly Miller <mm@flyingcircus.io>
2026-08-26 11:15:20 +0200

    platform: replace network interface setup with python script
    PL-132526

--- nixos/platform/network.nix
+++ nixos/platform/network.nix
@@ -606,137 +606,15 @@ in
               before = [ "network.target" ];
               stopIfChanged = false;
               path = [
-                pkgs.nettools
-                pkgs.ethtool
-                pkgs.procps
-                fclib.relaxedIp
-                pkgs.jq
-                pkgs.util-linux
+                pkgs.fc.netdev
               ];
-              script = ''
-                set -e
-
-                touch /var/lock/interface-rename.lock
-                exec 4</var/lock/interface-rename.lock
-
-                echo "Ensuring interface name '${iface.link}' ..."
-
-                echo "Acquiring interface renaming lock ..."
-                flock -x -w 60 4 || exit 1
-                echo "Got interface renaming lock ..."
-
-                # Look up the desired interface's current name by mac address
-                interface="$(ip -j -d link show | jq -r '.[] | select(.linkinfo?.info_kind? == null) | select(.address == "${iface.mac}") | .ifname')"
-
-                if [[ "$interface" == "" ]]; then
-                  echo "ERROR: Missing interface with MAC address '${iface.mac}'."
-                  echo "Found interfaces:"
-                  ip link show
-                  exit 1
-                fi
-
-                if [[ "$interface" != "${iface.link}" ]]; then
-                  echo "Interface is currently known as \`$interface\` ..."
-
-                  # Check whether our desired name is also already in use,
-                  # rename that one to a unique name.
-                  counter=0
-                  while ip l show dev ${iface.link} > /dev/null; do
-                    echo "'${iface.link}' is still being used, trying to clean up."
-                    # Down the other interface and rename it
-                    ip l set ${iface.link} down
-                    # Try, don't care if it fails. We check the success on the
-                    # next loop.
-                    ip l set ${iface.link} name eth$counter || true
-                    # The interface's name might not have been it's primary name
-                    # but an altname, try to delete that as well.
-                    # This looks funny, but we can use the altname to look up the
-                    # link to remove the altname ...
-                    # don't do this if the name is already gone
-                    ip l property del altname ${iface.link} dev ${iface.link} || true
-                    counter=$((counter+1))
-                  done
-
-                  echo "Performing rename from '$interface' to '${iface.link}'"
-                  ip l set $interface down
-                  ip l set $interface name ${iface.link}
-                fi
-
-                LINK_DRIVER=$(ethtool -i ${iface.link} | grep "driver: " | cut -d ':' -f 2 | sed -e 's/ //')
-                case $LINK_DRIVER in
-                    e1000|e1000e|igb|ixgbe|i40e)
-                        # Set adaptive interrupt moderation. This does increase
-                        # latency.
-                        echo "Enabling adaptive interrupt moderation ..."
-                        ethtool -C "${iface.link}" rx-usecs 1 || true
-                        # Larger buffers.
-                        echo "Setting ring buffer ..."
-                        ethtool -G "${iface.link}" rx 4096 tx 4096 || true
-                        # Large receive offload to reduce small packet CPU/interrupt impact.
-                        echo "Enabling large receive offload ..."
-                        ethtool -K "${iface.link}" lro on || true
-                        ;;
-                esac
-
-                # TODO: it'd be preferrable to manage this on a by-interface base
-                # and distinguish whether an interface is physical.
-                # Can this be done based on `config.flyingcircus.enc.parameters.interfaces.fe.policy`?
-                ${lib.optionalString (config.flyingcircus.networking.physicalHostNetworking) ''
-                  echo "Disabling flow control"
-                  ethtool -A ${iface.link} autoneg off rx off tx off || true
-                ''}
-
-                # Ensure MTU
-                ip l set ${iface.link} mtu ${toString iface.mtu}
-
-              ''
-
-              + (lib.optionalString (iface.externalLabel != null) ''
-                # Add long alternative names according to the external label
-                if ip l show dev ${quoteLabel iface.externalLabel} > /dev/null; then
-                  # XXX There is an edge case we don't cover here:
-                  # if the desired altname is already the primary name of an interface
-                  # the altname del will fail and this unit will not come up properly
-                  # but that means we notice it and will fix it then.
-                  ip l property del altname ${quoteLabel iface.externalLabel} dev ${quoteLabel iface.externalLabel}
-                fi
-                ip l property add altname ${quoteLabel iface.externalLabel} dev ${iface.link}
-
-              '')
-              + ''
-                echo "Releasing lock"
-                exec 4>&-
-              '';
-              preStop = ''
-                set -e
-
-                touch /var/lock/interface-rename.lock
-                exec 4</var/lock/interface-rename.lock
-
-                echo "Renaming ${iface.link} to neutral name ..."
-
-                echo "Acquiring interface renaming lock ..."
-                flock -x -w 60 4 || exit 1
-                echo "Got interface renaming lock ..."
-
-                # Shut down the interface so the systemd device unit goes away
-                # so we can perform proper renames.
-                ip l set ${iface.link} down
-
-                counter=0
-                while ip l show dev ${iface.link} > /dev/null; do
-                  ip l set ${iface.link} down
-                  # Try, don't care if it fails. We check the success on the
-                  # next loop.
-                  ip l set ${iface.link} name eth$counter || true
-                  # don't do this if the name is already gone
-                  ip l property del altname ${iface.link} dev ${iface.link} || true
-                  counter=$((counter+1))
-                done
-
-                echo "Releasing lock"
-                exec 4>&-
-              '';
+              environment.PYTHONUNBUFFERED = "1";
+              script =
+                "netdev start "
+                + (lib.optionalString config.flyingcircus.networking.physicalHostNetworking "-F ")
+                + (lib.optionalString (iface.externalLabel != null) "-l ${iface.externalLabel} ")
+                + "${iface.link} ${iface.mac} ${toString iface.mtu}";
+              preStop = "netdev stop ${iface.link}";
               serviceConfig = {
                 Type = "oneshot";
                 RemainAfterExit = true;
