18ba0af90296eb4df0f2297005431457187d1cdb
Leona Maroni <lm@flyingcircus.io>
2026-09-04 15:29:54 +0200

    test/kvm_host: fix with newer test driver
    PL-135540

--- tests/kvm_host_ceph-pacific.nix
+++ tests/kvm_host_ceph-pacific.nix
@@ -499,8 +499,8 @@ import ./make-test-python.nix (
           assert "I simplevm              running-ensure                 generation=0" in result, result
 
         ${lib.optionalString (!useFlakefinder) ''
-          host1.copy_from_vm('/tmp/coverage', './')
-          host1.copy_from_vm('/tmp/fc.qemu-report.xml', './')
+          host1.copy_from_machine('/tmp/coverage', './')
+          host1.copy_from_machine('/tmp/fc.qemu-report.xml', './')
         ''}
 
       '';
